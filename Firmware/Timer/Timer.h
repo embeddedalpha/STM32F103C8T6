3b23d82237e05e8a80ac4f5cfb002f91fe30ffeb
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-02-27 12:20:16 +0530

    Update Timer.h

--- Firmware/Timer/Timer.h
+++ Firmware/Timer/Timer.h
@@ -1,33 +1,35 @@
-#ifndef _TIMER_H_
-#define _TIMER_H_
+#ifndef _TIMER_
+#define _TIMER_
 
 #include "stm32f10x.h"
 
+#define TIMER_CLOCK_GENERAL 36000000
+#define TIMER_CLOCK_ADVANCED 72000000
+
+void Timer_Delay_Init(TIM_TypeDef *timer);
+
+//************************************************** uSECOND DELAY **************************************************
+void Timer_Delay_us(TIM_TypeDef *timer,float usSeconds);
 
 
-void Timer_Delay_us(float usSeconds);
-void Timer_Delay_ms(float msSeconds);
-void Timer_Delay_s(float Seconds);
+//************************************************** mSECOND DELAY **************************************************
+void Timer_Delay_ms(TIM_TypeDef *timer,float msSeconds);
 
-int Input_Trigger_channel_1(TIM_TypeDef *TIMER);
-int Input_Trigger_channel_2(TIM_TypeDef *TIMER);
-int Input_Trigger_channel_3(TIM_TypeDef *TIMER);
-int Input_Trigger_channel_4(TIM_TypeDef *TIMER);
+//************************************************** SECONDS DELAY **************************************************
+void Timer_Delay_s(TIM_TypeDef *timer,float Seconds);
 
+//************************************************** COUNTER MODE **************************************************
 
-//input capture
-//output capture
-//pwm 
-//one pulse
-//delay
+void Up_Counter(TIM_TypeDef *timer, uint16_t count);
+void Down_Counter(TIM_TypeDef *timer, uint16_t count);
+void CenterAligned_Counter(TIM_TypeDef *timer, uint16_t count);
 
+//************************************************** INPUT CAPTURE MODE **************************************************
 
-
-
-
-
-
-
+int Input_Capture_Channel_1(TIM_TypeDef *timer);
+int Input_Capture_Channel_2(TIM_TypeDef *timer);
+int Input_Capture_Channel_3(TIM_TypeDef *timer);
+int Input_Capture_Channel_4(TIM_TypeDef *timer);
 
 
 
