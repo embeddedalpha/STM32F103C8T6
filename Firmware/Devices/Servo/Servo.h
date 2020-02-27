b52de892f4013a923fdf856c81d02f38147f7b48
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-02-27 10:52:24 +0530

    Add files via upload

---
+++ Firmware/Devices/Servo/Servo.h
@@ -0,0 +1,71 @@
+#ifndef _SERVO_H_
+#define _SERVO_H_
+
+
+#include "stm32f10x.h"
+
+
+void Servo_1_Init(void);
+void Servo_1_Update(float angle);
+
+void Servo_2_Init(void);
+void Servo_2_Update(float angle);
+	
+void Servo_3_Init(void);
+void Servo_3_Update(float angle);
+
+void Servo_4_Init(void);
+void Servo_4_Update(float angle);
+	
+void Servo_5_Init(void);
+void Servo_5_Update(float angle);
+	
+void Servo_6_Init(void);
+void Servo_6_Update(float angle);
+	
+void Servo_7_Init(void);
+void Servo_7_Update(float angle);
+
+void Servo_8_Init(void);
+void Servo_8_Update(float angle);
+	
+void Servo_9_Init(void);
+void Servo_9_Update(float angle);
+
+void Servo_10_Init(void);
+void Servo_10_Update(float angle);
+	
+void Servo_11_Init(void);
+void Servo_11_Update(float angle);
+	
+void Servo_12_Init(void);
+void Servo_12_Update(float angle);
+	
+void Servo_13_Init(void);
+void Servo_13_Update(float angle);
+
+void Servo_14_Init(void);
+void Servo_14_Update(float angle);
+	
+void Servo_15_Init(void);
+void Servo_15_Update(float angle);
+
+void Servo_16_Init(void);
+void Servo_16_Update(float angle);
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
