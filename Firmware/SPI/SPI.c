5e72e046da3ac5f119c6d7fbade466c789f32789
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-03 23:24:45 +0530

    Update SPI.c

--- Firmware/SPI/SPI.c
+++ Firmware/SPI/SPI.c
@@ -1,29 +1,21 @@
 #include "SPI.h"
-
-
 /*
-MIT License
-
-Copyright (c) 2018 Embedded_Alpha
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
+*********************************************************************************************************************************
+@file:    SPI.c
+@author:  Kunal Salvi
+@version: V3.05.0
+@date:    29/2/2020
+@brief:   SPI Driver File. 
+					This file contains the functions declared in SPI.h file.
+					By calling various "Init" functions, the user can configure 
+					the SPI Hardware Peripheral in any of the permitted modes. 
+					
+@attention: This file aims at providing the user an abrstraction layer to the SPI peripheral
+						and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
+						In case of failure of code/product due to third party tampering, the author will not be
+						responsible. This code is published under MIT License.
+	
+*********************************************************************************************************************************	
 */
 
 /*
