6d5889495c7441df7bb40d85bf3da45b60095ade
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-04 16:29:44 +0530

    Update CAN.c

--- Firmware/CAN/CAN.c
+++ Firmware/CAN/CAN.c
@@ -1,114 +1,91 @@
+/*
+*********************************************************************************************************************************
+@file:    CAN.c
+@author:  Kunal Salvi
+@version: V3.05.0
+@date:    29/2/2020
+@brief:   CAN Source File. 
+This file contains the functions declared in CAN.h file.
+By calling various "Init" functions, the user can configure 
+the CAN Hardware Peripheral in any of the permitted modes. 
+					
+@attention: This file aims at providing the user an abrstraction layer to the CAN peripheral
+and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
+In case of failure of code/product due to third party tampering, the author will not be
+responsible. This code is published under MIT License.
+	
+*********************************************************************************************************************************	
+*/
+
 #include "CAN.h"
 
-
-void CAN_Sleep_Mode_Start(){
-
-	CAN1->MCR |= CAN_MCR_RESET;
-while((CAN1->MSR & CAN_MSR_SLAK) != 1){}
-}
-
-void CAN_Sleep_Mode_Stop(){
-
-	CAN1->MCR &= ~CAN_MCR_RESET;
-while((CAN1->MSR & CAN_MSR_SLAK) == 1){}
-}
-
-void CAN_Init_Mode_Start()
+int CAN_Init_Mode(int baudrate)
 {
-
-	CAN1->MCR |= CAN_MCR_INRQ;
-	while(!(CAN1->MSR & CAN_MSR_INAK)){}
-		RCC->APB1ENR |= RCC_APB1ENR_CAN1EN ;
-RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
-GPIOA -> CRH |= (0xB << 11) | (0xB << 12);
-	CAN1->BTR = 0x001c0008;		
-		
+    CAN -> MCR &= ~CAN_MCR_SLEEP; //RESET SLEEP BIT
+    while((CAN -> MSR & CAN_MSR_SLAK)){} //WAIT TILL SLAK IS RESET
+    CAN -> MCR |= CAN_MCR_INRQ;         //SET INRQ BIT 
+    while(!(CAN -> MSR & CAN_MSR_INAK)){}   //WAIT TILL INAK IS SET
+    
+    switch(baudrate)
+    {
+        case 1000: 
+        {
+           CAN -> BTR |= (1<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 1  |   TS1[] : 8   | TS2[] : 7
+            break;
+        };
+        
+        case 500: 
+        {
+            CAN -> BTR |= (3<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 3  |   TS1[] : 8   | TS2[] : 7
+            break;
+        };
+        
+        case 250: 
+        {
+            CAN -> BTR |= (7<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 7  |   TS1[] : 8   | TS2[] : 7
+            break;
+        };
+        
+        case 125: 
+        {
+            CAN -> BTR |= (15<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 15  |   TS1[] : 8   | TS2[] : 7
+            break;
+        };
+        
+        case 50: 
+        {
+            CAN -> BTR |= (39<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 39  |   TS1[] : 8   | TS2[] : 7
+            break;
+        };
+        
+        case 20: 
+        {
+            CAN -> BTR |= (99<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 99  |   TS1[] : 8   | TS2[] : 7
+            break;
+        }; 
+        
+        case 10: 
+        {
+            CAN -> BTR |= (199<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 199  |   TS1[] : 8   | TS2[] : 7
+            break;
+        };
+        
+        default: 
+        {
+            CAN -> BTR |= (1<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 1  |   TS1[] : 8   | TS2[] : 7
+            break;
+        };        
+    }
+    
+    CAN -> MCR |= CAN_MCR_AWUM; //AUTOMATIC WAKEUP FROM SLEEP MODE ON MESSAGE RECEPTION
+    
+    
 }
 
-void CAN_Init_Mode_Stop()
-{
-	CAN1->MCR &= ~CAN_MCR_INRQ;
-	while((CAN1->MSR & CAN_MSR_INAK) == 1){}	
-}
+int CAN_Normal_Mode()
+{}
 
-void CAN_Normal_Mode(){
+int CAN_Sleep_Mode()
+{}
 
-	CAN_Sleep_Mode_Stop();
-	CAN_Init_Mode_Stop();
-
-}
-
-void CAN_TX_Mailbox_0(uint32_t identifier, int length, int data[] ){
-	
-	//EMPTY STATE
-		while((CAN1->TSR & CAN_TSR_TME0) == 1){}
-	CAN1->sTxMailBox[0].TDTR |= (CAN_TDT0R_DLC | length);
-	CAN1->sTxMailBox[0].TIR &= ~CAN_TI0R_STID;
-	CAN1->sTxMailBox[0].TIR |= (identifier << 18);
-	CAN1->sTxMailBox[0].TDLR |= CAN_TDL0R_DATA0 << data[0];
-	CAN1->sTxMailBox[0].TDLR |= CAN_TDL0R_DATA1 << data[1];
-	CAN1->sTxMailBox[0].TDLR |= CAN_TDL0R_DATA2 << data[2];
-	CAN1->sTxMailBox[0].TDLR |= CAN_TDL0R_DATA3 << data[3];
-	CAN1->sTxMailBox[0].TDHR |= CAN_TDH0R_DATA4 << data[4];
-  CAN1->sTxMailBox[0].TDHR |= CAN_TDH0R_DATA5 << data[5];
-	CAN1->sTxMailBox[0].TDHR |= CAN_TDH0R_DATA6 << data[6];
-	CAN1->sTxMailBox[0].TDHR |= CAN_TDH0R_DATA7 << data[7];
-	CAN1->sTxMailBox[0].TIR |= CAN_TI0R_TXRQ;
-	
-			//PENDING STATE
-	while((CAN1->TSR & CAN_TSR_RQCP0) != 0){}
-	while((CAN1->TSR & CAN_TSR_TXOK0) != 0){}
-	while((CAN1->TSR & CAN_TSR_TME0) != 0){}
-		
-
-}
-
-void CAN_TX_Mailbox_1(uint32_t identifier, int length, int data[] ){
-	
-	//EMPTY STATE
-		while((CAN1->TSR & CAN_TSR_TME1) == 1){}
-	CAN1->sTxMailBox[1].TDTR |= (CAN_TDT0R_DLC | length);
-	CAN1->sTxMailBox[1].TIR &= ~CAN_TI0R_STID;
-	CAN1->sTxMailBox[1].TIR |= (identifier << 18);
-	CAN1->sTxMailBox[1].TDLR |= CAN_TDL0R_DATA0 << data[0];
-	CAN1->sTxMailBox[1].TDLR |= CAN_TDL0R_DATA1 << data[1];
-	CAN1->sTxMailBox[1].TDLR |= CAN_TDL0R_DATA2 << data[2];
-	CAN1->sTxMailBox[1].TDLR |= CAN_TDL0R_DATA3 << data[3];
-	CAN1->sTxMailBox[1].TDHR |= CAN_TDH0R_DATA4 << data[4];
-  CAN1->sTxMailBox[1].TDHR |= CAN_TDH0R_DATA5 << data[5];
-	CAN1->sTxMailBox[1].TDHR |= CAN_TDH0R_DATA6 << data[6];
-	CAN1->sTxMailBox[1].TDHR |= CAN_TDH0R_DATA7 << data[7];
-	CAN1->sTxMailBox[1].TIR |= CAN_TI0R_TXRQ;
-	
-			//PENDING STATE
-	while((CAN1->TSR & CAN_TSR_RQCP1) != 0){}
-	while((CAN1->TSR & CAN_TSR_TXOK1) != 0){}
-	while((CAN1->TSR & CAN_TSR_TME1) != 0){}
-		
-
-}
-
-void CAN_TX_Mailbox_2(uint32_t identifier, int length, int data[] ){
-	
-	//EMPTY STATE
-		while((CAN1->TSR & CAN_TSR_TME1) == 1){}
-	CAN1->sTxMailBox[2].TDTR |= (CAN_TDT0R_DLC | length);
-	CAN1->sTxMailBox[2].TIR &= ~CAN_TI0R_STID;
-	CAN1->sTxMailBox[2].TIR |= (identifier << 18);
-	CAN1->sTxMailBox[2].TDLR |= CAN_TDL0R_DATA0 << data[0];
-	CAN1->sTxMailBox[2].TDLR |= CAN_TDL0R_DATA1 << data[1];
-	CAN1->sTxMailBox[2].TDLR |= CAN_TDL0R_DATA2 << data[2];
-	CAN1->sTxMailBox[2].TDLR |= CAN_TDL0R_DATA3 << data[3];
-	CAN1->sTxMailBox[2].TDHR |= CAN_TDH0R_DATA4 << data[4];
-  CAN1->sTxMailBox[2].TDHR |= CAN_TDH0R_DATA5 << data[5];
-	CAN1->sTxMailBox[2].TDHR |= CAN_TDH0R_DATA6 << data[6];
-	CAN1->sTxMailBox[2].TDHR |= CAN_TDH0R_DATA7 << data[7];
-	CAN1->sTxMailBox[2].TIR |= CAN_TI0R_TXRQ;
-	
-			//PENDING STATE
-	while((CAN1->TSR & CAN_TSR_RQCP2) != 0){}
-	while((CAN1->TSR & CAN_TSR_TXOK2) != 0){}
-	while((CAN1->TSR & CAN_TSR_TME2) != 0){}
-		
-}
 
