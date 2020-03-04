9f32d9f750970e025b8fc5029e35751640a79498
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-04 15:35:31 +0530

    Update CAN.h

--- Firmware/CAN/CAN.h
+++ Firmware/CAN/CAN.h
@@ -5,14 +5,14 @@
 @version: V3.05.0
 @date:    29/2/2020
 @brief:   CAN Header File. 
-		  This file contains the prototype of functions in CAN.c file.
-		  By calling various "Init" functions, the user can configure 
-		  the CAN Hardware Peripheral in any of the permitted modes. 
+		    This file contains the prototype of functions in CAN.c file.
+		    By calling various "Init" functions, the user can configure 
+		    the CAN Hardware Peripheral in any of the permitted modes. 
 					
 @attention: This file aims at providing the user an abrstraction layer to the CAN peripheral
-			and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
-			In case of failure of code/product due to third party tampering, the author will not be
-			responsible. This code is published under MIT License.
+			    and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
+			    In case of failure of code/product due to third party tampering, the author will not be
+			    responsible. This code is published under MIT License.
 	
 *********************************************************************************************************************************	
 */
