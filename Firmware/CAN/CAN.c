672a450a1273812596bee2b88b6381990f073bc8
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-06 19:27:00 +0530

    Update CAN.c

--- Firmware/CAN/CAN.c
+++ Firmware/CAN/CAN.c
@@ -26,10 +26,10 @@ responsible. This code is published under MIT License.
 
 int CAN_Initialization_Mode()
 {
-    CAN -> MCR &= ~CAN_MCR_SLEEP; //RESET SLEEP BIT
+    CAN1 -> MCR &= ~CAN_MCR_SLEEP; //RESET SLEEP BIT
     while((CAN -> MSR & CAN_MSR_SLAK)){} //WAIT TILL SLAK IS RESET
-    CAN -> MCR |= CAN_MCR_INRQ;         //SET INRQ BIT 
-    while(!(CAN -> MSR & CAN_MSR_INAK)){}   //WAIT TILL INAK IS SET
+    CAN1 -> MCR |= CAN_MCR_INRQ;         //SET INRQ BIT 
+    while(!(CAN1 -> MSR & CAN_MSR_INAK)){}   //WAIT TILL INAK IS SET
     return 1;
 }
 //
@@ -42,10 +42,10 @@ int CAN_Initialization_Mode()
 
 int CAN_Normal_Mode()
 {
-    CAN->MCR &= ~CAN_MCR_INRQL;  //CLEAR INRQ BIT
-    while((CAN->MSR & CAN_MSR_INAK)); //WAIT TILL INAK BIT IS RESET
-    CAN->MCR &= ~CAN_MCR_SLEEP;
-    while((CAN->MSR & CAN_MSR_SLAK)); //WAIT TILL SLAK BIT IS RESET
+    CAN1->MCR &= ~CAN_MCR_INRQL;  //CLEAR INRQ BIT
+    while((CAN1->MSR & CAN_MSR_INAK)); //WAIT TILL INAK BIT IS RESET
+    CAN1->MCR &= ~CAN_MCR_SLEEP;
+    while((CAN1->MSR & CAN_MSR_SLAK)); //WAIT TILL SLAK BIT IS RESET
     return 1;
 }
 //
@@ -58,9 +58,9 @@ int CAN_Normal_Mode()
 
 int CAN_Sleep_Mode()
 {
-    CAN -> MCR |= CAN_MCR_AWUM; //AUTOMATIC WAKEUP FROM SLEEP MODE ON MESSAGE RECEPTION
-    CAN -> MCR |= CAN_MCR_SLEEP; //ENTER SLEEP MODE
-    while(!(CAN -> MSR & CAN_MSR_SLAK)){} //WAIT TILL ACKNOWLEDGEMENT IS RECEIVED
+    CAN1 -> MCR |= CAN_MCR_AWUM; //AUTOMATIC WAKEUP FROM SLEEP MODE ON MESSAGE RECEPTION
+    CAN1 -> MCR |= CAN_MCR_SLEEP; //ENTER SLEEP MODE
+    while(!(CAN1 -> MSR & CAN_MSR_SLAK)){} //WAIT TILL ACKNOWLEDGEMENT IS RECEIVED
 }
 //
 
@@ -84,55 +84,55 @@ void CAN_Bitrate(int baudrate)
     {
         case 1000: 
         {
-           CAN -> BTR |= (1<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 1  |   TS1[] : 8   | TS2[] : 7
+           CAN1 -> BTR |= (1<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 1  |   TS1[] : 8   | TS2[] : 7
             break;
         };
         
         case 500: 
         {
-            CAN -> BTR |= (3<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 3  |   TS1[] : 8   | TS2[] : 7
+            CAN1 -> BTR |= (3<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 3  |   TS1[] : 8   | TS2[] : 7
             break;
         };
         
         case 250: 
         {
-            CAN -> BTR |= (7<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 7  |   TS1[] : 8   | TS2[] : 7
+            CAN1 -> BTR |= (7<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 7  |   TS1[] : 8   | TS2[] : 7
             break;
         };
         
         case 125: 
         {
-            CAN -> BTR |= (15<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 15  |   TS1[] : 8   | TS2[] : 7
+            CAN1 -> BTR |= (15<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 15  |   TS1[] : 8   | TS2[] : 7
             break;
         };
         
         case 100: 
         {
-            CAN -> BTR |= (19<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 39  |   TS1[] : 8   | TS2[] : 7
+            CAN1 -> BTR |= (19<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 39  |   TS1[] : 8   | TS2[] : 7
             break;
         };
         
         case 50: 
         {
-            CAN -> BTR |= (39<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 39  |   TS1[] : 8   | TS2[] : 7
+            CAN1 -> BTR |= (39<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 39  |   TS1[] : 8   | TS2[] : 7
             break;
         };
         
         case 20: 
         {
-            CAN -> BTR |= (99<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 99  |   TS1[] : 8   | TS2[] : 7
+            CAN1 -> BTR |= (99<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 99  |   TS1[] : 8   | TS2[] : 7
             break;
         }; 
         
         case 10: 
         {
-            CAN -> BTR |= (199<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 199  |   TS1[] : 8   | TS2[] : 7
+            CAN1 -> BTR |= (199<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 199  |   TS1[] : 8   | TS2[] : 7
             break;
         };
         
         default: 
         {
-            CAN -> BTR |= (1<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 1  |   TS1[] : 8   | TS2[] : 7
+            CAN1 -> BTR |= (1<< 0) | (8 << 16) | (7 << 20) | (1 << 24); //PRESCALLER : 1  |   TS1[] : 8   | TS2[] : 7
             break;
         };        
     
@@ -157,21 +157,189 @@ void CAN_Test_Mode_Setup(int mode)
     {
         case 0:
         {   
-            CAN->BTR |= CAN_BTR_SLIM;   //SET SLIM BIT 
+            CAN1->BTR |= CAN_BTR_SLIM;   //SET SLIM BIT 
             break;
         };
         
         case 1:
         {   
-            CAN->BTR |= CAN_BTR_LBKM;  //SET LBKM BIT
+            CAN1->BTR |= CAN_BTR_LBKM;  //SET LBKM BIT
             break;
         };
         
         case 2:
         {   
-            CAN->BTR |= CAN_BTR_LBKM | CAN_BTR_SLIM;  //SET LBKM  AND SLIM BITS
+            CAN1->BTR |= CAN_BTR_LBKM | CAN_BTR_SLIM;  //SET LBKM  AND SLIM BITS
             break;
         };        
     }
 }
 //
+
+/***************************************** CAN Trasnmmit Data *************************************************************************
+//@function: The function sets up the bitrate for CAN bus
+//@param: 
+//       mailbox_no               0     ->    Mailbox 0     
+//                                1     ->    Mailbox 1
+//                                2     ->    Mailbox 2
+//       standard_id              Any 11 bit id
+//       extended_id              Any 18 bit id
+//       dlc                      from 0 to 8
+//       data[]                   Array of data to be transmitted
+//       priority                 0     ->    Message driven by id of message
+//                                1     ->    Message driven by request order
+//@return: Returns 1 when data is transmitted.
+********************************************************************************************************************************/
+
+int CAN_Transmit_Data_Frame(int mailbox_no, int standard_id, int extended_id, int dlc, int data[], int priority)
+{
+    int i = 0;
+   switch(mailbox_no)
+   {
+       case 0 : //mailbox 0
+       {
+           while(!(CAN1 -> TSR & CAN_TSR_TME0)){}
+           CAN1 -> TSR |= CAN_TSR_RQCP0;
+           if(extended_id != 0)
+           {
+               CAN1 -> TI0R |= CAN_TI0R_IDE;
+               CAN1 -> TI0R |= (standard_id << 21) | (extended_id << 3);
+           }
+           else
+           {
+               CAN1 -> TI0R &= ~CAN_TI0R_IDE;
+               CAN1 -> TI0R |= (standard_id << 21);
+           }
+           
+           CAN1 -> TDT0R |= dlc << 0;
+           
+           for(i = 0; i < dlc; i++)
+           {
+               if(i <= 3)
+               {
+                   CAN1 -> TDL0R |= data[i] << (8 * i);
+               }
+               else
+               {
+                   CAN1 -> TDL0R |= data[i] << (8 * (i-4));
+               }
+           }
+           
+           CAN1 -> TI0R |= CAN_TI0R_TXRQ;
+           
+           if(priority == 0)
+           {
+               CAN1 -> MCR &= ~CAN_MCR_TXFP;
+           }
+           if(priority == 1)
+           {
+               CAN1 -> MCR |=  CAN_MCR_TXFP;
+           }
+           
+           while(!(CAN1 -> TSR & CAN_TSR_TXOK0)){}
+           while(!(CAN1 -> TSR & CAN_TSR_RQCP0)){}
+           
+           
+           break;
+       };
+       
+       case 1 : //mailbox 1
+       {
+           while(!(CAN1 -> TSR & CAN_TSR_TME1)){}
+           CAN1 -> TSR |= CAN_TSR_RQCP1;
+           if(extended_id != 0)
+           {
+               CAN1 -> TI1R |= CAN_TI1R_IDE;
+               CAN1 -> TI1R |= (standard_id << 21) | (extended_id << 3);
+           }
+           else
+           {
+               CAN1 -> TI1R &= ~CAN_TI1R_IDE;
+               CAN1 -> TI1R |= (standard_id << 21);
+           }
+           
+           CAN1 -> TDT1R |= dlc << 0;
+           
+           for(i = 0; i < dlc; i++)
+           {
+               if(i <= 3)
+               {
+                   CAN1 -> TDL1R |= data[i] << (8 * i);
+               }
+               else
+               {
+                   CAN1 -> TDL1R |= data[i] << (8 * (i-4));
+               }
+           }
+           
+           CAN1 -> TI1R |= CAN_TI1R_TXRQ;
+           
+           if(priority == 0)
+           {
+               CAN1 -> MCR &= ~CAN_MCR_TXFP;
+           }
+           if(priority == 1)
+           {
+               CAN1 -> MCR |=  CAN_MCR_TXFP;
+           }
+           
+           while(!(CAN1 -> TSR & CAN_TSR_TXOK1)){}
+           while(!(CAN1 -> TSR & CAN_TSR_RQCP1)){}
+           
+           
+           break;
+       }; 
+       
+       case 2 : //mailbox 2
+       {
+           while(!(CAN1 -> TSR & CAN_TSR_TME2)){}
+           CAN1 -> TSR |= CAN_TSR_RQCP2;
+           if(extended_id != 0)
+           {
+               CAN1 -> TI2R |= CAN_TI2R_IDE;
+               CAN1 -> TI2R |= (standard_id << 21) | (extended_id << 3);
+           }
+           else
+           {
+               CAN1 -> TI2R &= ~CAN_TI2R_IDE;
+               CAN1 -> TI2R |= (standard_id << 21);
+           }
+           
+           CAN1 -> TDT2R |= dlc << 0;
+           
+           for(i = 0; i < dlc; i++)
+           {
+               if(i <= 3)
+               {
+                   CAN1 -> TDL2R |= data[i] << (8 * i);
+               }
+               else
+               {
+                   CAN1 -> TDH2R |= data[i] << (8 * (i-4));
+               }
+           }
+           
+           CAN1 -> TI2R |= CAN_TI2R_TXRQ;
+           
+           if(priority == 0)
+           {
+               CAN1 -> MCR &= ~CAN_MCR_TXFP;
+           }
+           if(priority == 1)
+           {
+               CAN1 -> MCR |=  CAN_MCR_TXFP;
+           }
+           
+           while(!(CAN1 -> TSR & CAN_TSR_TXOK2)){}
+           while(!(CAN1 -> TSR & CAN_TSR_RQCP2)){}
+           
+           
+           break;
+       };
+   }
+   
+   return 1;
+    
+}
+//
+
