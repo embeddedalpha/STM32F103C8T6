763bef83a7a03514854cff23a4e81cb22c61eb1f
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-07 11:51:22 +0530

    Update CAN.h

--- Firmware/CAN/CAN.h
+++ Firmware/CAN/CAN.h
@@ -129,4 +129,34 @@ int CAN_Transmit_Data_Frame(int mailbox_no, int standard_id, int extended_id, in
 
 int CAN_Buffer_0_Transmission_Errors(void);
 
+
+
+/***************************************** CAN Buffer 1 Transmission Errors *************************************************************************
+//@function: The function sets up the bitrate for CAN bus
+//@param: none
+//@return: Returns 1 when data is lost in arbitration 
+//         Return  2 when data is lost in transmission
+//         Return  3 when data is lost in both arbitration and transmission.
+*****************************************************************************************************************************************************/
+
+
+int CAN_Buffer_1_Transmission_Errors(void);
+
+
+
+
+/***************************************** CAN Buffer 2 Transmission Errors *************************************************************************
+//@function: The function sets up the bitrate for CAN bus
+//@param: none
+//@return: Returns 1 when data is lost in arbitration 
+//         Return  2 when data is lost in transmission
+//         Return  3 when data is lost in both arbitration and transmission.
+*****************************************************************************************************************************************************/
+
+
+int CAN_Buffer_2_Transmission_Errors(void);
+
+
+
+
 #endif
