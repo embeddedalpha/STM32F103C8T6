545e292628b2c73697c95b82878a9f82d7a07fcc
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-07 12:24:56 +0530

    Update CAN.h

--- Firmware/CAN/CAN.h
+++ Firmware/CAN/CAN.h
@@ -4,17 +4,17 @@
 @author:  Kunal Salvi
 @version: V3.05.0
 @date:    29/2/2020
-@brief:   CAN Header File. 
+@brief:   CAN Header File.
           This file contains the prototype of functions in CAN.c file.
-          By calling various "Init" functions, the user can configure 
-          the CAN Hardware Peripheral in any of the permitted modes. 
-					
+          By calling various "Init" functions, the user can configure
+          the CAN Hardware Peripheral in any of the permitted modes.
+
 @attention: This file aims at providing the user an abrstraction layer to the CAN peripheral
             and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
             In case of failure of code/product due to third party tampering, the author will not be
             responsible. This code is published under MIT License.
-	
-*********************************************************************************************************************************	
+
+*********************************************************************************************************************************
 */
 
 
@@ -26,15 +26,15 @@
 struct CAN_Standard_Frame{
     uint16_t id;
     uint8_t  data[8];
-    
-}
+
+};
 //
 
 struct CAN_Extended_Frame{
-    uint16_ id;
-    uint32 extended_id;
+    uint16_t id;
+    uint32_t extended_id;
     uint8_t data[8];
-}
+};
 //
 
 
@@ -49,7 +49,7 @@ struct CAN_Extended_Frame{
 #define CAN_BitRate_10_kbps     10
 
 /***************************************** Initialization Mode *****************************************************************
-//@function: The function sets up the CAN controller in Initialization mode.
+//@brief: The function sets up the CAN controller in Initialization mode.
 //@param: none
 //@return: Returns 1 if Initialization is successful
 ********************************************************************************************************************************/
@@ -57,7 +57,7 @@ struct CAN_Extended_Frame{
 int CAN_Initialization_Mode();
 
 /***************************************** Normal Mode *************************************************************************
-//@function: The function sets up the CAN controller in Normal mode.
+//@brief: The function sets up the CAN controller in Normal mode.
 //@param: none
 //@return: Returns 1 if Normalization is successful
 ********************************************************************************************************************************/
@@ -65,7 +65,7 @@ int CAN_Initialization_Mode();
 int CAN_Normal_Mode();
 
 /***************************************** Sleep Mode *************************************************************************
-//@function: The function sets up the CAN controller in Sleep mode.
+//@brief: The function sets up the CAN controller in Sleep mode.
 //@param: none
 //@return: Returns 1 if controller is in sleep mode
 ********************************************************************************************************************************/
@@ -73,8 +73,8 @@ int CAN_Normal_Mode();
 int CAN_Sleep_Mode();
 
 /***************************************** CAN Bitrate *************************************************************************
-//@function: The function sets up the bitrate for CAN bus
-//@param: 
+//@brief: The function sets up the bitrate for CAN bus
+//@param:
 //       baudrate           CAN_BitRate_1000_kbps   1000     [Defined in CAN.h]
 //                          CAN_BitRate_500_kbps    500
 //                          CAN_BitRate_250_kbps    250
@@ -89,9 +89,9 @@ int CAN_Sleep_Mode();
 void CAN_Bitrate(int baudrate);
 
 /***************************************** CAN Test Mode *************************************************************************
-//@function: The function sets up the bitrate for CAN bus
-//@param: 
-//       mode               0     ->    Silent Mode     
+//@brief: The function sets up the bitrate for CAN bus
+//@param:
+//       mode               0     ->    Silent Mode
 //                          1     ->    Loop Back Mode
 //                          2     ->    Silent Loop Back Mode
 //@return: none
@@ -100,10 +100,11 @@ void CAN_Bitrate(int baudrate);
 void CAN_Test_Mode_Setup(int mode);
 
 
+
 /***************************************** CAN Trasnmmit Data *************************************************************************
-//@function: The function transmitts data onto the CAN bus by filling the appropriate buffers
-//@param: 
-//       mailbox_no               0     ->    Mailbox 0     
+//@brief: The function transmitts data onto the CAN bus by filling the appropriate buffers
+//@param:
+//       mailbox_no               0     ->    Mailbox 0
 //                                1     ->    Mailbox 1
 //                                2     ->    Mailbox 2
 //       standard_id              Any 11 bit id
@@ -119,26 +120,24 @@ int CAN_Transmit_Data_Frame(int mailbox_no, int standard_id, int extended_id, in
 
 
 /***************************************** CAN Buffer 0 Transmission Errors *************************************************************************
-//@function: The function sets up the bitrate for CAN bus
+//@brief: The function sets up the bitrate for CAN bus
 //@param: none
-//@return: Returns 1 when data is lost in arbitration 
+//@return: Returns 1 when data is lost in arbitration
 //         Return  2 when data is lost in transmission
 //         Return  3 when data is lost in both arbitration and transmission.
 *****************************************************************************************************************************************************/
-
 
 int CAN_Buffer_0_Transmission_Errors(void);
 
 
 
 /***************************************** CAN Buffer 1 Transmission Errors *************************************************************************
-//@function: The function sets up the bitrate for CAN bus
+//@brief: The function sets up the bitrate for CAN bus
 //@param: none
-//@return: Returns 1 when data is lost in arbitration 
+//@return: Returns 1 when data is lost in arbitration
 //         Return  2 when data is lost in transmission
 //         Return  3 when data is lost in both arbitration and transmission.
 *****************************************************************************************************************************************************/
-
 
 int CAN_Buffer_1_Transmission_Errors(void);
 
@@ -146,13 +145,12 @@ int CAN_Buffer_1_Transmission_Errors(void);
 
 
 /***************************************** CAN Buffer 2 Transmission Errors *************************************************************************
-//@function: The function sets up the bitrate for CAN bus
+//@brief: The function sets up the bitrate for CAN bus
 //@param: none
-//@return: Returns 1 when data is lost in arbitration 
+//@return: Returns 1 when data is lost in arbitration
 //         Return  2 when data is lost in transmission
 //         Return  3 when data is lost in both arbitration and transmission.
 *****************************************************************************************************************************************************/
-
 
 int CAN_Buffer_2_Transmission_Errors(void);
 
