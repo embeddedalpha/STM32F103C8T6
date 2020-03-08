aaa6213c1fdb1431c91a32b23c564f665a03b37f
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-08 14:00:28 +0530

    Update LIN.c

--- Firmware/LIN/LIN.c
+++ Firmware/LIN/LIN.c
@@ -1,18 +1,4 @@
-/**********************************************************************************************************************************
-@file:    LIN.h
-@author:  Kunal Salvi
-@version: V3.05.0
-@date:    29/2/2020
-@brief:   LIN Header File.
-          This file contains the prototype of functions declared in LIN.h file.
-          This is the LIN protocol driver file that any application can use.
 
-@attention: This file aims at providing the user an abrstraction layer to the LIN peripheral
-            and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
-            In case of failure of code/product due to third party tampering, the author will not be
-            responsible. This code is published under MIT License.
-
-**********************************************************************************************************************************/
 
 #include "LIN.h"
 
