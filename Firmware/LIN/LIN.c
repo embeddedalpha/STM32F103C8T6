0589b2e129d937cee24ead02ce6861772f71cd03
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-08 13:53:24 +0530

    Update LIN.c

--- Firmware/LIN/LIN.c
+++ Firmware/LIN/LIN.c
@@ -1,17 +1,38 @@
-#ifndef _LIN_H_
-#define _LIN_H_
-
-#include "stm32f10x.h"
-
-USART_TypeDef *LIN;
+#include "LIN.h"
 
 
 
-int LIN_Init(void);
-int LIN_Send_Break(void);
-int LIN_Transmit_Data(void);
-int LIN_Receive_Data(void);
+int LIN_Init(void)
+{
 
+if(LIN == USART1)
+{
+LIN -> BRR = (234 << 4 |6 << 0 );
+}
+else
+{
+LIN -> BRR = (117 << 4 | 3 << 0 );
+}
+LIN ->CR2 &= ~(USART_CR2_CLKEN | USART_CR2_STOP);
+LIN -> CR3 &= ~(USART_CR3_SCEN | USART_CR3_HDSEL | USART_CR3_IREN);
+LIN -> CR1 &= ~(USART_CR1_M);
+LIN -> CR2 |= USART_CR2_STOP;
+LIN ->CR2 |= USART_CR2_LINEN;
 
+return 1;
+}
 
-#endif
+int LIN_Send_Break(void)
+{
+LIN -> CR1 |= USART_CR1_SBK;
+}
+
+int LIN_Transmit_Data(void)
+{
+LIN -> CR1 |= USART_CR1_TE;
+LIN_Send_Break();
+
+}
+
+int LIN_Receive_Data(void)
+{}
