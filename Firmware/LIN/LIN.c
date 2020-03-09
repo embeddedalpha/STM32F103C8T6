0d2b0ba1afebf82aeb416f81ee0cab682e2d1fe9
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-09 13:28:05 +0530

    Update LIN.c

--- Firmware/LIN/LIN.c
+++ Firmware/LIN/LIN.c
@@ -17,7 +17,7 @@
 
 #include "LIN.h"
 
-int LIN_Init(void)
+void LIN_Setup(void)
 {
 
 if(LIN == USART1)
@@ -32,22 +32,41 @@ LIN ->CR2 &= ~(USART_CR2_CLKEN | USART_CR2_STOP);
 LIN -> CR3 &= ~(USART_CR3_SCEN | USART_CR3_HDSEL | USART_CR3_IREN);
 LIN -> CR1 &= ~(USART_CR1_M);
 LIN -> CR2 |= USART_CR2_STOP;
-LIN ->CR2 |= USART_CR2_LINEN;
 
-return 1;
 }
+
+int LIN_Init(void)
+{
+	LIN ->CR2 |= USART_CR2_LINEN;
+	return 1;
+}
+
 
 int LIN_Send_Break(void)
 {
 LIN -> CR1 |= USART_CR1_SBK;
+
 }
 
-int LIN_Transmit_Data(void)
+int LIN_Transmit_Data(uint8_t LIN_SLAVE_ID)
 {
 LIN -> CR1 |= USART_CR1_TE;
 LIN_Send_Break();
-
+LIN -> DR = 0x55;
+while(!(LIN->SR & USART_SR_TXE));
+while(!(LIN->SR & USART_SR_TC));
+uint8_t P0 = (LIN_SLAVE_ID &(1 << 0)) ^ (LIN_SLAVE_ID &(1 << 1)) ^ (LIN_SLAVE_ID &(1 << 2)) ^ (LIN_SLAVE_ID(1 << 4)) ;
+uint8_t P1 = !((LIN_SLAVE_ID &(1 << 1)) ^ (LIN_SLAVE_ID &(1 << 3)) ^ (LIN_SLAVE_ID &(1 << 4)) ^ (LIN_SLAVE_ID(1 << 5)));
+uint_t PID = (P0 << 6) | (P1 << 7) | LIN_SLAVE_ID;
+LIN -> DR = PID;
+while(!(LIN->SR & USART_SR_TXE));
+while(!(LIN->SR & USART_SR_TC));
 }
 
 int LIN_Receive_Data(void)
-{}
+{
+LIN -> CR2 |= UASRT_CR2_LBDL;
+LIN -> CR1 |= USART_CR1_RE;
+while(!(LIN->SR & USART_SR_LBD));
+
+}
