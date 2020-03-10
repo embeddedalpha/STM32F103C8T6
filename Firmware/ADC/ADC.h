00cc9011e0d0a917bee9185454bcda9529ba7d49
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-10 19:55:10 +0530

    Update ADC.h

--- Firmware/ADC/ADC.h
+++ Firmware/ADC/ADC.h
@@ -22,6 +22,40 @@
 
 #include "stm32f10x.h"
 
+/***************************************** Initialization Mode *****************************************************************
+//@brief: The function sets up the ADC in Initialization mode.
+//@param:
+//      ADC                ADC1 or ADC2
+//      conversion_mode    0  ->  Single Conversion Mode
+//                         1  ->  Continuous  Conversion Mode
+//      data_alignment     0  ->  Right Alignment
+//                         1  ->  Left Alignment
+//      DMA_Enable         0  ->  DMA disabled
+//                         1  ->  DMA enabled
+//@return: Returns 1 if Initialization is successful
+********************************************************************************************************************************/
+
+void ADC_Init(ADC_TypeDef *ADC , int conversion_mode, int data_alignment, int DMA_Enable);
+
+/***************************************** Calibration Mode *****************************************************************
+//@brief: The function calibrates ADC
+//@param:
+//      ADC                ADC1 or ADC2
+//@return: Returns 1 if Initialization is successful
+********************************************************************************************************************************/
+
+int ADC_Calibrate(ADC_TypeDef *ADC);
+
+/***************************************** Channel Initialization Mode *****************************************************************
+//@brief: The function calibrates ADC
+//@param:
+//       channel_type           0  ->  Regular mode
+//                              1  ->  Injected mode
+//@return: none
+********************************************************************************************************************************/
+
+void ADC_Channel_0_Init(int channel_type, int sequence);
+
 
 
 #endif
