7c5b11170f4a8792a5aa5088bf4689139f779a3c
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-10 18:02:57 +0530

    Add files via upload

---
+++ Firmware/ADC/ADC.h
@@ -0,0 +1,27 @@
+/**********************************************************************************************************************************
+@file:    ADC.H
+@author:  Kunal Salvi
+@version: V3.05.0
+@date:    29/2/2020
+@brief:   ADC Header File.
+          This file contains the prototype of functions in ADC.c file.
+          By calling various functions, the user can configure
+          the ADC Hardware Peripheral.
+
+@attention: This file aims at providing the user an abrstraction layer to the CAN peripheral
+            and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
+            In case of failure of code/product due to third party tampering, the author will not be
+            responsible. This code is published under MIT License.
+
+*********************************************************************************************************************************
+*/
+
+#ifndef _ADC_H_
+#define _ADC_H_
+
+
+#include "stm32f10x.h"
+
+
+
+#endif
