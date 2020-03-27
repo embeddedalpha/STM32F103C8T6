e84cd70faba2f1575d885740be54b0f5d5996302
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-03-27 18:33:24 +0530

    Update I2C.c

--- Firmware/I2C/I2C.c
+++ Firmware/I2C/I2C.c
@@ -1,70 +1,143 @@
+/**********************************************************************************************************************************
+@file:    I2C.c
+@author:  Kunal Salvi
+@version: V3.05.0
+@date:    27/3/2020
+@brief:   I2C Source File.
+          This file contains the functions declared in I2C.h file.
+          By calling various  functions, the user can configure
+          the I2C Hardware Peripheral in any of the permitted modes.
+
+@attention: This file aims at providing the user an abstraction layer to the I2C peripheral
+            and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
+            In case of failure of code/product due to third party tampering, the author will not be
+            responsible. This code is published under MIT License.
+
+**********************************************************************************************************************************/
+
 #include "I2C.h"
 
 
+/***************************************** Initialization Mode *****************************************************************
+//@brief: The function sets up the I2C controller in Master mode.
+//@param: none
+//@return: none
+********************************************************************************************************************************/
 
+void I2C_Master_Init(void)
+{
 
-void I2C_Init(void)
+	if(I2C == I2C1)
 	{
+		RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+		GPIOB->CRL |=  (ALT_OPEN_DRAIN_OUTPUT << (4*4)) |  (ALT_OPEN_DRAIN_OUTPUT << (4*3)); //PB7 PB6
+	}
+	else
+	{
+		RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
+		GPIOB->CRH |=  (ALT_OPEN_DRAIN_OUTPUT << (4*2)) |  (ALT_OPEN_DRAIN_OUTPUT << (4*3)); //PB10 PB11
+	}
 
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
-	GPIOB->CRL |= GPIO_CRL_CNF6_1 | GPIO_CRL_MODE6_1 | GPIO_CRL_MODE6_0; //PB6 SCLK
-	GPIOB->CRL |= GPIO_CRL_CNF7_1 | GPIO_CRL_MODE7_1 | GPIO_CRL_MODE7_0; //PB7 SDA
-
-	I2C1->CR2 = 30;
-	I2C1->CCR = 0x802D;
-	I2C1->TRISE = 0x5;
-	I2C1->CR1 |= I2C_CR1_PE;
-	I2C1->CR1 |= I2C_CR1_START;
-	READ_REG(I2C1->SR1);
-
-
+	I2C -> CR2 |= (uint16_t)30; //30MHz PCLK1
+	I2C -> CCR |= I2C_CCR_FS | I2C_CCR_DUTY | 0x0003;
+	I2C -> TRISE = (uint16_t)10;
+	I2C -> CR1 |= I2C_CR1_PE;
 }
+//******************************************************************************************************************************
 
-void I2C_Start(void){
-I2C1->CR1 |= I2C_CR1_START;
-READ_REG(I2C1->SR1);
+
+
+
+/***************************************** Generate Start Condition *****************************************************************
+//@brief: The function generates a start condition.
+//@param: none
+//@return: none
+********************************************************************************************************************************/
+
+void I2C_Master_Start(){
+I2C->CR1 |= I2C_CR1_START;
+while(!(I2C->SR1 & I2C_SR1_SB)){}
 }
+//*******************************************************************************************************************************
 
-void I2C_Send_Address(int address){
 
-	address &= ~(1 << 0);
-	I2C1->DR = (uint8_t)address;
-	READ_REG(I2C1->SR1);
-	READ_REG(I2C1->SR2);
-	CLEAR_BIT(I2C1->SR1,I2C_SR1_ADDR);
 
-}
-void I2C_TX_Data(int data){
+/***************************************** Write Address *****************************************************************
+//@brief: The function sends device address as write command.
+//@param:
+//        data          device address
+//@return: none
+********************************************************************************************************************************/
 
-	READ_REG(I2C1->SR1);
-	I2C1->DR =  data;
-	while(!(I2C1->CR1 & I2C_CR1_ACK)){}
-  READ_REG(I2C1->SR1);
-	READ_REG(I2C1->SR2);
-}
-
-void I2C_Rx_Address(int address){
-
-	address &= ~(1 << 1);
-	I2C1->DR = (uint8_t)address;
-	READ_REG(I2C1->SR1);
-	READ_REG(I2C1->SR2);
-	CLEAR_BIT(I2C1->SR1,I2C_SR1_ADDR);
-
-}
-
-int I2C_RX_Data(void)
+void I2C_Master_Address_Write(uint8_t data)
 {
-  int data;
-	while(!(I2C1->CR1 & I2C_CR1_ACK)){}
-	data = I2C1->DR;
-	READ_REG(I2C1->SR1);
-	READ_REG(I2C1->SR2);	
-	return data;
+	I2C -> DR = (uint8_t)(data << 1) | i2c_device_write;
+while(!(I2C->SR1 & I2C_SR1_ADDR)){}
 }
 
 
-void I2C_Stop(void)
+
+
+/***************************************** Read Address *****************************************************************
+//@brief: The function sends device address as read command.
+//@param:
+//        data          device address
+//@return: none
+********************************************************************************************************************************/
+
+void I2C_Master_Address_Read(uint8_t data)
 {
-	I2C1->CR1 |= I2C_CR1_STOP;
+I2C -> DR = (uint8_t)(data << 1) | i2c_device_read;
+while(!(I2C->SR1 & I2C_SR1_ADDR)){}
 }
+//*******************************************************************************************************************************
+
+
+
+
+/***************************************** Send Data *****************************************************************
+//@brief: The function sends data onto the I2C bus.
+//@param:
+//        data          data to be sent
+//@return: none
+********************************************************************************************************************************/
+
+void I2C_Master_Send_Data( uint8_t data)
+{
+	while(!(I2C->SR1 & I2C_SR1_TXE)){}
+	I2C -> DR = (uint8_t)data;
+}
+//*******************************************************************************************************************************
+
+
+
+/***************************************** Read Data *****************************************************************
+//@brief: The function receives data from the I2C bus.
+//@param: none
+//@return: data from the device
+********************************************************************************************************************************/
+
+int I2C_Master_Rx_Data(void)
+{
+	uint8_t data = 0;
+	data = I2C->DR;
+	while(!(I2C->SR1 & I2C_SR1_RXNE)){}
+return data;
+}
+//*******************************************************************************************************************************
+
+
+
+/***************************************** Stops Transfer  *****************************************************************
+//@brief: The function terminates all transfers.
+//@param: none
+//@return: none
+********************************************************************************************************************************/
+
+void I2C_Master_Stop(void)
+{
+	while(!(I2C->SR1 & I2C_SR1_TXE)){}
+	while(!(I2C->SR1 & I2C_SR1_BTF)){}
+	I2C->CR1 |= I2C_CR1_STOP;
+}
+
