d7bc91e8e7514dc606d90d4ca2e0bd1c565ad73c
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-03 21:22:19 +0530

    Update CC1101.c

--- Firmware/Devices/CC1101/CC1101.c
+++ Firmware/Devices/CC1101/CC1101.c
@@ -1,1 +1,21 @@
 
+#include "CC1101.h"
+
+
+void CC1101_Write_Strobe(uint8_t strobe)
+{
+
+
+}
+void CC1101_Write_Reg(uint8_t addr, uint8_t *buffer, uint8_t count)
+{
+
+}
+void CC1101_Read_Status(uint8_t strobe)
+{
+
+}
+void CC1101_Read_Reg(uint8_t addr,uint8_t *buffer, uint8_t count)
+{
+
+}
