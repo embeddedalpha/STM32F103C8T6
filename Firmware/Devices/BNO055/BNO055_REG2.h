a579373cacc078a800f0b8d7367c19b9291e012d
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-10 23:51:57 +0530

    Update BNO055_REG2.h

--- Firmware/Devices/BNO055/BNO055_REG2.h
+++ Firmware/Devices/BNO055/BNO055_REG2.h
@@ -1,1 +1,37 @@
+#ifndef _BNO055_REG_2_H_
+#define _BNO055_REG_2_H_
+
+#define GYR_AM_SET	0x1F
+#define GYR_AM_THRES 0x1E
+
+#define GYR_DUR_Z	0x1d
+#define GYR_DUR_Y	0x1b
+#define GYR_DUR_X	0x19
+
+#define GYR_HR_Z_SET	0x1c
+#define GYR_HR_Y_SET	0x1a
+#define GYR_HR_X_SET	0x18
+
+#define GYR_INT_SETING	0x17
+#define ACC_NM_SET		0x16
+#define ACC_NM_THRE		0x15
+#define ACC_HG_THRES	0x14
+#define ACC_HG_DURATION 0x13
+#define ACC_INT_SETTING 0x12
+#define ACC_AM_THRES	0x11
+#define INT_EN			0x10
+#define INT_MSK			0x0F
+#define GYR_SLEEP_CONFIG	0x0E
+#define ACC_SLEEP_CONFIG	0x0C
+#define GYR_CONFIG_1	0x0B
+#define GYR_Config_0	0x0A
+#define MAG_Config		0x09
+#define ACC_Config		0x08
+#define PAGE_ID			0x07
+
+
+
+
+
+#endif
 
