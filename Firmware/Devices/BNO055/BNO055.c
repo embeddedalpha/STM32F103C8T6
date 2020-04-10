dd1db79682859f7b8e31903271908f5bbf9331dd
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-11 00:35:06 +0530

    Create BNO055.c

---
+++ Firmware/Devices/BNO055/BNO055.c
@@ -0,0 +1,24 @@
+#include "BNO055.h"
+
+
+//accelerometer config
+
+void BNO055_Init()
+{
+I2C_Master_Init();
+}
+
+void Accelerometer_CONFIG(uint8_t G_Range, uint8_t Bandwidth, uint8_t OpMode)
+{
+
+}
+
+void Gyroscope_CONFIG(uint8_t G_Range, uint8_t Bandwidth, uint8_t OpMode)
+{
+
+}
+
+void Magnetometer_CONFIG(uint8_t DataRate, uint8_t OpMode, uint8_t PowerMode)
+{
+
+}
