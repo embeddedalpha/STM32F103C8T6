d1542fa5003c9f00fb8cb132b4f440ae06f5a83f
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-19 20:40:06 +0530

    Update SPI.c

--- Firmware/SPI/SPI.c
+++ Firmware/SPI/SPI.c
@@ -5,14 +5,14 @@
 @version: V3.05.0
 @date:    29/2/2020
 @brief:   SPI Driver File.
-	  This file contains the functions declared in SPI.h file.
-          By calling various "Init" functions, the user can configure
-	  the SPI Hardware Peripheral in any of the permitted modes.
+		  This file contains the functions declared in SPI.h file.
+		  By calling various "Init" functions, the user can configure
+		  the SPI Hardware Peripheral in any of the permitted modes.
 
 @attention: This file aims at providing the user an abrstraction layer to the SPI peripheral
-	    and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
-	    In case of failure of code/product due to third party tampering, the author will not be
-	    responsible. This code is published under MIT License.
+			and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
+			In case of failure of code/product due to third party tampering, the author will not be
+			responsible. This code is published under MIT License.
 
 *********************************************************************************************************************************
 */
@@ -22,7 +22,7 @@
 /******************************************     Master Configuration     ******************************************/
 
 
-void SPI_Master_Config(SPI_TypeDef *SPI, struct SPI_Master_Parameters SPI_M)
+void SPI_Master_Config(struct SPI_Master_Parameters SPI_M)
 {
 SPI->CR1 |= SPI_M.Baudrate << 3;
 SPI->CR1 |= SPI_M.CPOL << 1;
@@ -38,24 +38,24 @@ SPI->CR2 |= SPI_M.RxDMA << 0;
 }
 
 
-void SPI_Master_IRQ_Config(SPI_TypeDef *SPI, struct SPI_Master_IRQ_Parameters SPIM_I)
+void SPI_Master_IRQ_Config(struct SPI_Master_IRQ_Parameters SPIM_I)
 {
 SPI->CR2 |= (SPIM_I.TX_Interrupt) << 7;
 SPI->CR2 |= (SPIM_I.RX_Interrupt) << 6;
 SPI->CR2 |= (SPIM_I.Error_Interrupt) << 5;
 }
 
-void SPI_Master_Enable(SPI_TypeDef *SPI)
+void SPI_Master_Enable(void)
 {
 	SPI -> CR1 |= SPI_CR1_SPE;
 }
 
-void SPI_Master_TX(SPI_TypeDef *SPI, int data)
+void SPI_Master_TX(int data)
 {
 	SPI -> DR = data;
 }
 
-int SPI_Master_RX(SPI_TypeDef *SPI)
+int SPI_Master_RX(void)
 {
 	return SPI->DR;
 }
@@ -63,7 +63,7 @@ int SPI_Master_RX(SPI_TypeDef *SPI)
 void SPI_NSS_Pin_Setup(void)
 {
 
-	if(pin < 8)
+	if(NSS_Pin < 8)
 	{
 		PORT ->CRL |= ALT_PUSH_PULL_OUTPUT << NSS_Pin;
 	}
@@ -84,7 +84,7 @@ void SPI_NSS_HIGH(void)
 }
 /******************************************     Slave Configuration     ******************************************/
 
-void SPI_Slave_Config(SPI_TypeDef *SPI, struct SPI_Slave_Parameters SPI_S)
+void SPI_Slave_Config(struct SPI_Slave_Parameters SPI_S)
 {
 SPI->CR1 |= SPI_S.Baudrate << 3;
 SPI->CR1 |= SPI_S.CPOL << 1;
@@ -100,25 +100,25 @@ SPI->CR2 |= SPI_S.RxDMA << 0;
 }
 
 
-void SPI_Slave_IRQ_Config(SPI_TypeDef *SPI, struct SPI_Slave_IRQ_Parameters SPIS_I)
+void SPI_Slave_IRQ_Config(struct SPI_Slave_IRQ_Parameters SPIS_I)
 {
 	SPI->CR2 |= (SPIS_I.TX_Interrupt) << 7;
 	SPI->CR2 |= (SPIS_I.RX_Interrupt) << 6;
 	SPI->CR2 |= (SPIS_I.Error_Interrupt) << 5;
 }
 
-void SPI_Slave_TX(SPI_TypeDef *SPI, int data)
+void SPI_Slave_TX(int data)
 {
 	SPI -> DR = data;
 }
 
-int SPI_Slave_RX(SPI_TypeDef *SPI)
+int SPI_Slave_RX(void)
 {
 	return SPI->DR;
 }
 
 
-void SPI_Slave_Enable(SPI_TypeDef *SPI)
+void SPI_Slave_Enable(void)
 {
 	SPI -> CR1 |= SPI_CR1_SPE;
 }
