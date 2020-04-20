e4f588c6bf62dd6ec64bc423a5d8c0fa50119102
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-20 10:39:32 +0530

    Update W25Q128.c

--- Firmware/Devices/W25Q128 Flash/W25Q128.c
+++ Firmware/Devices/W25Q128 Flash/W25Q128.c
@@ -1,11 +1,123 @@
 #include "W25Q128.h"
 
+
+
+
+
+void W25Q128_Init()
+{
+	W25Q128.Baudrate = 8;
+	W25Q128.CPHA = 1;
+	W25Q128.CPOL = 1;
+	W25Q128.DataFormat = Bit_8;
+	W25Q128.LSBorMSB = LSB;
+	W25Q128.BiDirectional_Mode = Duplex;
+	SPI_Master_Config(W25Q128);
+	SPI_Master_Enable();
+
+}
+
+/***********************************************************************************/
+
+
+void W25Q128_Write_Status_Register(uint8_t reg, uint8_t data)
+{
+	//Send Write Enable
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(Write_Enable);
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(reg);
+	SPI_Master_TX(data);
+	SPI_NSS_HIGH();
+}
+
+void W25Q128_Page_Write(int data[256], int length, uint32_t address)
+{
+	uint8_t status, busy, wel;
+	uint8_t A23_16 = (0x00FF0000 & address) >> 16;
+	uint8_t A15_08 = (0x0000FF00 & address) >> 8;
+	uint8_t A07_00 = (0x00000000 & address) >> 0;
+	//Send Write Enable
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(Write_Enable);
+	SPI_NSS_HIGH();
+	//Check Status Register for WEL Bit
+	SPI_NSS_LOW();
+	SPI_Master_TX(Read_Status_Register_1);
+	do{
+			 status = W25Q128_Read_Status_Register_1();
+			 busy = 0x02 & status;
+		}while( wel != 0);
+	SPI_NSS_HIGH();
+	//Start Page Program
+	SPI_NSS_LOW();
+	SPI_Master_TX(Page_Program);
+	SPI_Master_TX(A23_16);
+	SPI_Master_TX(A15_08);
+	SPI_Master_TX(A07_00);
+	for(int i = 0 ; i <= length; i++)
+	{
+		SPI_Master_TX(data[i]);
+	}
+	SPI_NSS_HIGH();
+	//Check Status Register for BUSY Bit
+	do{
+		 status = W25Q128_Read_Status_Register_1();
+		 busy = 0x01 & status;
+	}while( busy != 0);
+}
+
+
+void W25Q128_Write(int data[256], int length, uint32_t address)
+{
+	uint8_t status, busy, wel;
+	uint8_t A23_16 = (0x00FF0000 & address) >> 16;
+	uint8_t A15_08 = (0x0000FF00 & address) >> 8;
+	uint8_t A07_00 = (0x000000FF & address) >> 0;
+	//Send Write Enable
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(Write_Enable);
+	SPI_NSS_HIGH();
+	//Check Status Register for WEL Bit
+	SPI_NSS_LOW();
+	SPI_Master_TX(Read_Status_Register_1);
+	do{
+				status = W25Q128_Read_Status_Register_1();
+				busy = 0x02 & status;
+	   }while( wel != 0);
+	SPI_NSS_HIGH();
+	//Start Page Program
+	SPI_NSS_LOW();
+	SPI_Master_TX(Page_Program);
+	SPI_Master_TX(A23_16);
+	SPI_Master_TX(A15_08);
+	SPI_Master_TX(A07_00);
+	for(int i = 0 ; i <= length; i++)
+	{
+		SPI_Master_TX(data[i]);
+	}
+	SPI_NSS_HIGH();
+	//Check Status Register for BUSY Bit
+	do{
+		 status = W25Q128_Read_Status_Register_1();
+		 busy = 0x01 & status;
+	}while( busy != 0);
+}
+
+
+/***********************************************************************************/
+
 uint8_t W25Q128_Read_Status_Register_1(void)
 {
 	SPI_NSS_HIGH();
 	SPI_NSS_LOW();
 	SPI_Master_TX(Read_Status_Register_1);
 	uint8_t status = SPI_Master_RX();
+	SPI_NSS_HIGH();
 	return status;
 }
 
@@ -15,6 +127,7 @@ uint8_t W25Q128_Read_Status_Register_2(void)
 	SPI_NSS_LOW();
 	SPI_Master_TX(Read_Status_Register_2);
 	uint8_t status = SPI_Master_RX();
+	SPI_NSS_HIGH();
 	return status;
 }
 
@@ -24,9 +137,11 @@ uint8_t W25Q128_Read_Status_Register_3(void)
 	SPI_NSS_LOW();
 	SPI_Master_TX(Read_Status_Register_3);
 	uint8_t status = SPI_Master_RX();
+	SPI_NSS_HIGH();
 	return status;
 }
 
+/**********************************************************************************/
 
 
 void W25Q128_Chip_Erase(void)
@@ -67,6 +182,8 @@ void W25Q128_Erase_4KB_Sector(uint32_t address)
 		 busy = 0x01 & status;
 	}while( busy != 0);
 }
+
+
 
 void W25Q128_Erase_32KB_Block(uint32_t address)
 {
@@ -114,58 +231,49 @@ void W25Q128_Erase_64KB_Block(uint32_t address)
 	}while( busy != 0);
 }
 
+/***********************************************************************************/
 
-void W25Q128_Write(int data[256], int length, uint32_t address)
+
+void W25Q128_Reset_Device(void)
 {
-	uint8_t A23_16 = (0x00FF0000 & address) >> 16;
-	uint8_t A15_08 = (0x0000FF00 & address) >> 8;
-	uint8_t A07_00 = (0x000000FF & address) >> 0;
+	uint8_t status_1, status_2,busy, sus;
+	do{
+		 status_1 = W25Q128_Read_Status_Register_1();
+		 status_2 = W25Q128_Read_Status_Register_2();
+		 busy = 0x01 & status_1;
+		 sus = 0x80 & status_2;
+	}while( (busy != 0) & (sus != 0));
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(Enable_Reset);
+	SPI_Master_TX(Reset_Device);
+	SPI_NSS_HIGH();
+
+
+}
+
+void W25Q128_Global_Block_Sector_Lock(void)
+{
 	SPI_NSS_HIGH();
 	SPI_NSS_LOW();
 	SPI_Master_TX(Write_Enable);
 	SPI_NSS_HIGH();
 	SPI_NSS_LOW();
-	SPI_Master_TX(Page_Program);
-	SPI_Master_TX(A23_16);
-	SPI_Master_TX(A15_08);
-	SPI_Master_TX(A07_00);
-	for(int i = 0 ; i <= length; i++)
-	{
-		SPI_Master_TX(data[i]);
-	}
+	SPI_Master_TX(Global_Block_Lock);
 	SPI_NSS_HIGH();
+
 }
 
-void W25Q128_Page_Write(int data[256], int length, uint32_t address)
+
+void W25Q128_Global_Block_Sector_Unlock(void)
 {
-	uint8_t A23_16 = (0x00FF0000 & address) >> 16;
-	uint8_t A15_08 = (0x0000FF00 & address) >> 8;
-	uint8_t A07_00 = (0x00000000 & address) >> 0;
 	SPI_NSS_HIGH();
 	SPI_NSS_LOW();
 	SPI_Master_TX(Write_Enable);
 	SPI_NSS_HIGH();
 	SPI_NSS_LOW();
-	SPI_Master_TX(Page_Program);
-	SPI_Master_TX(A23_16);
-	SPI_Master_TX(A15_08);
-	SPI_Master_TX(A07_00);
-	for(int i = 0 ; i <= length; i++)
-	{
-		SPI_Master_TX(data[i]);
-	}
+	SPI_Master_TX(Global_Block_Unlock);
 	SPI_NSS_HIGH();
-}
-
-void W25Q128_Init()
-{
-	W25Q128.Baudrate = 8;
-	W25Q128.CPHA = 1;
-	W25Q128.CPOL = 1;
-	W25Q128.DataFormat = Bit_8;
-	W25Q128.LSBorMSB = LSB;
-	W25Q128.BiDirectional_Mode = Duplex;
-	SPI_Master_Config(W25Q128);
-	SPI_Master_Enable();
 
 }
+
