723be7d4619cbc5abfb5c417683e60fc96ebf29e
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-21 00:12:24 +0530

    Update MCP2515_CAN_Controller.h

--- Firmware/Devices/MCP_CAN_Controller/MCP2515_CAN_Controller.h
+++ Firmware/Devices/MCP_CAN_Controller/MCP2515_CAN_Controller.h
@@ -1,103 +1,147 @@
-#ifndef _MCP2515_
-#define _MCP2515_
+#ifndef _MCP2512_CAN_Controller_H_
+#define _MCP2512_CAN_Controller_H_
 
-#include "stm32f10x.h"
 #include "SPI.h"
+#include "board.h"
 
-static SPI_TypeDef *spi;
-
-struct MCP_TX_DataStruct
-{
-uint16_t standard_id;
-uint32_t extended_id;
-uint16_t dlc;
-uint16_t data0, data1, data2, data3, 
-         data4, data5, data6, data6;
-         
-};
-
-struct MCP_RX_DataStruct
-{
-uint16_t standard_id;
-uint32_t extended_id;
-uint16_t dlc;
-uint16_t data0, data1, data2, data3, 
-         data4, data5, data6, data6;
-         
-};
+struct SPI_Master_Parameters MCP2512;
 
 
-uint8_t MCP_Read_Instruction(uint8_t address);
-void MCP_Write_Byte_Instruction(uint8_t address, uint8_t data);
-void MCP_Load_TX_Buff_1();
-void MCP_Bit_Modify_Instruction(uint8_t address, uint8_t mask_byte, uint8_t data_byte);
-void MCP_Transmit_Buffer_0(uint8_t data);
-uint16_t MCP_Read_Status_Instruction(void);
+#define RXF0SIDH  0x00
+#define RXF0SIDL  0x01
+#define RXF0EID8  0x02
+#define RXF0EID0  0x03
+#define RXF1SIDH  0x04
+#define RXF1SIDL  0x05
+#define RXF1EID8  0x06
+#define RXF1EID0  0x07
+#define RXF2SIDH  0x08
+#define RXF2SIDL  0x09
+#define RXF2EID8  0x0A
+#define RXF2EID0  0x0B
+#define BFPCTRL   0x0C
+#define TXRTSCTRL 0x0D
+#define CANSTAT   0x0E
+#define CANCTRL   0x0F
 
+#define RXF3SIDH  0x10
+#define RXF3SIDL  0x11
+#define RXF3EID8  0x12
+#define RXF3EID0  0x13
+#define RXF4SIDH  0x14
+#define RXF4SIDL  0x15
+#define RXF4EID8  0x16
+#define RXF4EID0  0x17
+#define RXF5SIDH  0x18
+#define RXF5SIDL  0x19
+#define RXF5EID8  0x1A
+#define RXF5EID0  0x1B
+#define TEC       0x1C
+#define REC       0x1D
+#define CANSTAT   0x1E
+#define CANCTRL   0x1F
 
+#define RXM0SIDH  0x20
+#define RXM0SIDL  0x21
+#define RXM0EID8  0x22
+#define RXM0EID0  0x23
+#define RXM1SIDH  0x24
+#define RXM1SIDL  0x25
+#define RXM1EID8  0x26
+#define RXM1EID0  0x27
+#define CNF3      0x28
+#define CNF2      0x29
+#define CNF1      0x2A
+#define CANINTE   0x2B
+#define CANINTF   0x2C
+#define EFLG      0x2D
+#define CANSTAT   0x2E
+#define CANCTRL   0x2F
 
+#define TXB0CTRL  0x30
+#define TXB0SIDH  0x31
+#define TXB0SIDL  0x32
+#define TXB0EID8  0x33
+#define TXB0EID0  0x34
+#define TXB0DLC   0x35
+#define TXB0D0    0x36
+#define TXB0D1    0x37
+#define TXB0D2    0x38
+#define TXB0D3    0x39
+#define TXB0D4    0x3A
+#define TXB0D5    0x3B
+#define TXB0D6    0x3C
+#define TXB0D7    0x3D
+#define CANSTAT   0x3E
+#define CANCTRL   0x3F
 
-// SPI Instruction Set
-#define MCP_RESET 					0xC0 //Reset internal registers
-#define MCP_READ						0x03 //Read data from the register at selected address
-#define MCP_READ_RX_BUFFER_0	0x90 //When reading a receive buffer, reduces the overhead of a normal READ
-#define MCP_READ_RX_BUFFER_1	0x90
-#define MCP_READ_RX_BUFFER_2	0x90
-#define MCP_READ_RX_BUFFER_3	0x90
-#define MCP_WRITE						0x02 //Writes data to the register beginning at the selected address
-#define MCP_LOAD_TX_BUFFER  0x40 //When loading a transmit buffer, reduces the overhead of a normal WRITE
-#define MCP_RTS							0x80 //Instructs controller to begin message transmission sequence for any of the transmit buffers
-#define MCP_READ_STATUS			0xA0 //Quick polling command that reads several status bits for transmit and receive functions.
-#define MCP_RX_STATU				0xB0 //Quick polling command that indicates filter match and message type  of received message.
-#define MCP_BIT_MODIFY			0x05 //Allows the user to set or clear individual bits in a particular register. 
+#define TXB1CTRL  0x40
+#define TXB1SIDH  0x41
+#define TXB1SIDL  0x42
+#define TXB1EID8  0x43
+#define TXB1EID0  0x44
+#define TXB1DLC   0x45
+#define TXB1D0    0x46
+#define TXB1D1    0x47
+#define TXB1D2    0x48
+#define TXB1D3    0x49
+#define TXB1D4    0x4A
+#define TXB1D5    0x4B
+#define TXB1D6    0x4C
+#define TXB1D7    0x4D
+#define CANSTAT   0x4E
+#define CANCTRL   0x4F
 
+#define TXB2CTRL  0x50
+#define TXB2SIDH  0x51
+#define TXB2SIDL  0x52
+#define TXB2EID8  0x53
+#define TXB2EID0  0x54
+#define TXB2DLC   0x55
+#define TXB2D0    0x56
+#define TXB2D1    0x57
+#define TXB2D2    0x58
+#define TXB2D3    0x59
+#define TXB2D4    0x5A
+#define TXB2D5    0x5B
+#define TXB2D6    0x5C
+#define TXB2D7    0x5D
+#define CANSTAT   0x5E
+#define CANCTRL   0x5F
 
-//Transmit Buffer Register Set
-#define TXB_1_CTRL	0x30
-#define TXB_2_CTRL	0x40
-#define TXB_3_CTRL	0x50
-#define TXRTSCTRL		0x0D
-#define TXB_1_SIDH	0x31
-#define TXB_2_SIDH	0x41
-#define TXB_3_SIDH	0x51
-#define TXB_1_SIDL	0x32
-#define TXB_2_SIDL	0x42
-#define TXB_3_SIDL	0x52
-#define TXB_1_EID8	0x33
-#define TXB_2_EID8	0x43
-#define TXB_3_EID8	0x53
-#define TXB_1_EID0	0x34
-#define TXB_2_EID0	0x44
-#define TXB_3_EID0	0x54
-#define TXB_1_DLC		0x35
-#define TXB_2_DLC		0x45
-#define TXB_3_DLC		0x55
-#define TXB_1_D_1		0x36
-#define TXB_1_D_2		0x37
-#define TXB_1_D_3		0x38
-#define TXB_1_D_4		0x39
-#define TXB_1_D_5		0x3a
-#define TXB_1_D_6		0x3b
-#define TXB_1_D_7		0x3c
-#define TXB_1_D_8		0x3d
-#define TXB_2_D_1		0x46
-#define TXB_2_D_2		0x47
-#define TXB_2_D_3		0x48
-#define TXB_2_D_4		0x49
-#define TXB_2_D_5		0x4a
-#define TXB_2_D_6		0x4b
-#define TXB_2_D_7		0x4c
-#define TXB_2_D_8		0x4d
-#define TXB_3_D_1		0x56
-#define TXB_3_D_2		0x57
-#define TXB_3_D_3		0x58
-#define TXB_3_D_4		0x59
-#define TXB_3_D_5		0x5a
-#define TXB_3_D_6		0x5b
-#define TXB_3_D_7		0x5c
-#define TXB_3_D_8		0x5d
+#define RXB0CTRL  0x60
+#define RXB0SIDH  0x61
+#define RXB0SIDL  0x62
+#define RXB0EID8  0x63
+#define RXB0EID0  0x64
+#define RXB0DLC   0x65
+#define RXB0D0    0x66
+#define RXB0D1    0x67
+#define RXB0D2    0x68
+#define RXB0D3    0x69
+#define RXB0D4    0x6A
+#define RXB0D5    0x6B
+#define RXB0D6    0x6C
+#define RXB0D7    0x6D
+#define CANSTAT   0x6E
+#define CANCTRL   0x6F
 
-
+#define RXB1CTRL  0x70
+#define RXB1SIDH  0x71
+#define RXB1SIDL  0x72
+#define RXB1EID8  0x73
+#define RXB1EID0  0x74
+#define RXB1DLC   0x75
+#define RXB1D0    0x76
+#define RXB1D1    0x77
+#define RXB1D2    0x78
+#define RXB1D3    0x79
+#define RXB1D4    0x7A
+#define RXB1D5    0x7B
+#define RXB1D6    0x7C
+#define RXB1D7    0x7D
+#define CANSTAT   0x7E
+#define CANCTRL   0x7F
 
 
 #endif
