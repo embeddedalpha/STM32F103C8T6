9f059a79214645e8b7bf031c3422fa40add7ec66
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-21 00:53:12 +0530

    Update MCP2515_CAN_Controller.c

--- Firmware/Devices/MCP_CAN_Controller/MCP2515_CAN_Controller.c
+++ Firmware/Devices/MCP_CAN_Controller/MCP2515_CAN_Controller.c
@@ -1,25 +1,102 @@
-#include "MCP2515_CAN_Controller.h"
+#include "MCP2512_CAN_Controller.h"
 
 
-//READ INSTRUCTION
 
-int MCP_Read(uint8_t address)
+
+
+void MCP2512_Init(void)
 {
-	//CS high
-	//CS low
-	//send read instruction
-	//send address 
-	//send 000 to receive data
-	//CS low
-	//return rx_buffer
+	MCP2512.Baudrate = 8;
+	MCP2512.CPHA = 1;
+	MCP2512.CPOL = 1;
+	MCP2512.DataFormat = Bit_8;
+	MCP2512.LSBorMSB = MSB;
+	MCP2512.BiDirectional_Mode = Duplex;
+	SPI_Master_Config(MCP2512);
+	SPI_Master_Enable();
+	NSS_Pin = 4;
+	SPI_NSS_Pin_Setup();
 }
 
-void MCP_Read_RX0_Buffer(struct &rxstruct)
-{	
-	//CS high
-	//cs low
-	
+uint8_t MCP2512_Read(uint8_t address)
+{
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(MCP2512_Read_INST);
+	SPI_Master_TX(address);
+	uint8_t data = SPI_Master_RX();
+	SPI_NSS_HIGH();
+	return data;
+}
+
+uint8_t MCP2512_Read_RX_Buffer(uint8_t buffer)
+{
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(buffer);
+	uint8_t data = SPI_Master_RX();
+	SPI_NSS_HIGH();
+	return data;
+}
+
+void MCP2512_Write(uint8_t address, uint8_t data)
+{
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(MCP2512_Write_INST);
+	SPI_Master_TX(address);
+	SPI_Master_TX(data);
+	SPI_NSS_HIGH();
+}
+
+
+void MCP2512_Load_TX_Buffer(uint8_t buffer, uint8_t data)
+{
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(buffer);
+	SPI_Master_TX(data);
+	SPI_NSS_HIGH();
 
 }
 
+void MCP2512_Request_To_Send(uint8_t RTS_Buffer)
+{
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(RTS_Buffer);
+	SPI_NSS_HIGH();
+}
+
+void MCP2512_Bit_Modify(uint8_t rts_buffer,uint8_t address, uint8_t mask, uint8_t data)
+{
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(rts_buffer);
+	SPI_Master_TX(address);
+	SPI_Master_TX(mask);
+	SPI_Master_TX(data);
+	SPI_NSS_HIGH();
+}
+
+
+uint8_t MCP2512_Read_Status(void)
+{
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(MCP2512_Read_Status_INST);
+	uint8_t data = SPI_Master_RX();
+	SPI_NSS_HIGH();
+	return data;
+}
+
+uint8_t MCP2512_Rx_Status(void)
+{
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(MCP2512_Rx_Status_INST);
+	uint8_t data = SPI_Master_RX();
+	SPI_NSS_HIGH();
+	return data;
+}
 
