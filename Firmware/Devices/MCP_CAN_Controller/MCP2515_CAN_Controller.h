687c82254014e312aec223b4659980a399842495
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-21 00:53:42 +0530

    Update MCP2515_CAN_Controller.h

--- Firmware/Devices/MCP_CAN_Controller/MCP2515_CAN_Controller.h
+++ Firmware/Devices/MCP_CAN_Controller/MCP2515_CAN_Controller.h
@@ -6,25 +6,25 @@
 
 struct SPI_Master_Parameters MCP2512;
 
-#define MCP2512_Reset                      0xC0
-#define MCP2512_Read                       0x02
-#define MCP2512_Read_Rx_Buffer_0_RXB0SIDH  0x90
-#define MCP2512_Read_Rx_Buffer_0_RXB0D0    0x91
-#define MCP2512_Read_Rx_Buffer_1_RXB1SIDH  0x92
-#define MCP2512_Read_Rx_Buffer_1_RXB1D0    0x93
-#define MCP2512_Write                      0x02
-#define MCP2512_Load_Tx_Buffer_0_TXB0SIDH  0x40
-#define MCP2512_Load_Tx_Buffer_0_TXB0D0    0x41
-#define MCP2512_Load_Tx_Buffer_1_TXB1SIDH  0x42
-#define MCP2512_Load_Tx_Buffer_1_TXB1D0    0x43
-#define MCP2512_Load_Tx_Buffer_2_TXB2SIDH  0x44
-#define MCP2512_Load_Tx_Buffer_2_TXB2D0    0x45
-#define MCP2512_RTS_TXB0                   0x81
-#define MCP2512_RTS_TXB1                   0x82
-#define MCP2512_RTS_TXB2                   0x84
-#define MCP2512_Read_Status                0xA0
-#define MCP2512_Rx_Status                  0xB0
-#define MCP2512_Bit_Modify                 0x05
+#define MCP2512_Reset_INST                       0xC0
+#define MCP2512_Read_INST                        0x02
+#define MCP2512_Read_Rx_Buffer_0_RXB0SIDH        0x90
+#define MCP2512_Read_Rx_Buffer_0_RXB0D0          0x91
+#define MCP2512_Read_Rx_Buffer_1_RXB1SIDH        0x92
+#define MCP2512_Read_Rx_Buffer_1_RXB1D0          0x93
+#define MCP2512_Write_INST                       0x02
+#define MCP2512_Load_Tx_Buffer_0_TXB0SIDH_INST   0x40
+#define MCP2512_Load_Tx_Buffer_0_TXB0D0_INST     0x41
+#define MCP2512_Load_Tx_Buffer_1_TXB1SIDH_INST   0x42
+#define MCP2512_Load_Tx_Buffer_1_TXB1D0_INST     0x43
+#define MCP2512_Load_Tx_Buffer_2_TXB2SIDH_INST   0x44
+#define MCP2512_Load_Tx_Buffer_2_TXB2D0_INST     0x45
+#define MCP2512_RTS_TXB0_INST                    0x81
+#define MCP2512_RTS_TXB1_INST                    0x82
+#define MCP2512_RTS_TXB2_INST                    0x84
+#define MCP2512_Read_Status_INST                 0xA0
+#define MCP2512_Rx_Status_INST                   0xB0
+#define MCP2512_Bit_Modify_INST                  0x05
 
 #define RXF0SIDH  0x00
 #define RXF0SIDL  0x01
@@ -162,5 +162,15 @@ struct SPI_Master_Parameters MCP2512;
 #define CANSTAT   0x7E
 #define CANCTRL   0x7F
 
+
+void MCP2512_Init(void);
+uint8_t MCP2512_Read(uint8_t address);
+uint8_t MCP2512_Read_RX_Buffer(uint8_t buffer);
+void MCP2512_Write(uint8_t address, uint8_t data);
+void MCP2512_Load_TX_Buffer(uint8_t buffer, uint8_t data);
+void MCP2512_Request_To_Send(uint8_t RTS_Buffer);
+void MCP2512_Bit_Modify(uint8_t rts_buffer,uint8_t address, uint8_t mask, uint8_t data);
+uint8_t MCP2512_Read_Status(void);
+uint8_t MCP2512_Rx_Status(void);
 
 #endif
