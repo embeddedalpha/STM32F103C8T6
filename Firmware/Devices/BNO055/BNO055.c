6b2d54eca20303cde2b405415631f7ff5e899fe2
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-24 13:30:54 +0530

    Update BNO055.c

--- Firmware/Devices/BNO055/BNO055.c
+++ Firmware/Devices/BNO055/BNO055.c
@@ -8,6 +8,30 @@ void BNO055_Init()
 I2C_Master_Init();
 }
 
+void BNO055_Write(uint8_t reg, uint8_t data)
+{
+	I2C_Master_Start();
+	I2C_Master_Address_Write(BNO055_Address);
+	I2C_Master_Send_Data(reg);
+	I2C_Master_Send_Data(data);
+	I2C_Master_Stop();
+}
+
+
+void BNO055_Read(uint8_t reg, uint8_t bytes)
+{
+	I2C_Master_Start();
+	I2C_Master_Address_Write(BNO055_Address);
+	I2C_Master_Start();
+	for(int i = 0; i < bytes; i++)
+	{
+		BNO055_Buffer[i] = I2C_Master_Rx_Data();
+		I2C_Gen_Ack();
+	}
+	I2C_Master_Stop();
+}
+
+
 void Accelerometer_CONFIG(uint8_t G_Range, uint8_t Bandwidth, uint8_t OpMode)
 {
 
