20a33823c8aad25e5486e842f09f56de8b602b52
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-24 14:00:52 +0530

    Update BNO055.h

--- Firmware/Devices/BNO055/BNO055.h
+++ Firmware/Devices/BNO055/BNO055.h
@@ -3,7 +3,6 @@
 
 #include "BNO055_REG1.h"
 #include "BNO055_REG2.h"
-
 #include "i2c.h"
 
 #define BNO055_Address	0x28
@@ -14,16 +13,35 @@
 
 
 
+uint8_t BNO055_Buffer[10];
+
+uint16_t BNO055_QUATERNION_Buffer[3];
+uint16_t BNO055_GRAVITY_VECTOR_Buffer[3];
+uint16_t BNO055_LINEAR_ACCL_Buffer[3];
+uint16_t BNO055_EULER_ANGLE_Buffer[3];
+uint16_t BNO055_COMPASS_DATA_Buffer[3];
+uint16_t BNO055_IMU_DATA_Buffer[3];
+uint16_t BNO055_M4G_DATA_Buffer[3];
+uint16_t BNO055_NDOF_DATA_Buffer[3];
+
+uint16_t BNO055_GYRO_DATA_Buffer[3];
+uint16_t BNO055_ACCL_DATA_Buffer[3];
+uint16_t BNO055_MAG_DATA_Buffer[3];
+
+
 void BNO055_Init();
 
-void Accelerometer_CONFIG(uint8_t G_Range, uint8_t Bandwidth, uint8_t OpMode);
+void BNO055_GET_ACC_DATA(void);
 
-void Gyroscope_CONFIG(uint8_t G_Range, uint8_t Bandwidth, uint8_t OpMode);
+void BNO055_GET_GYR_DATA(void);
 
-void Magnetometer_CONFIG(uint8_t DataRate, uint8_t OpMode, uint8_t PowerMode);
+void BNO055_GET_MAG_DATA(void);
+
 
 void BNO055_Write(uint8_t reg, uint8_t data);
 
 void BNO055_Read(uint8_t reg, uint8_t bytes);
+
+void BNO055_Power_Mode(uint8_t mode);
 
 #endif
