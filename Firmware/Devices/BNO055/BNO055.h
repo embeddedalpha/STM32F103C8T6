9423b04ac81e781c57a7eb009dc448ad1c4cd7b7
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-29 20:45:50 +0530

    Update BNO055.h

--- Firmware/Devices/BNO055/BNO055.h
+++ Firmware/Devices/BNO055/BNO055.h
@@ -15,33 +15,35 @@
 
 uint8_t BNO055_Buffer[10];
 
-uint16_t BNO055_QUATERNION_Buffer[3];
-uint16_t BNO055_GRAVITY_VECTOR_Buffer[3];
-uint16_t BNO055_LINEAR_ACCL_Buffer[3];
-uint16_t BNO055_EULER_ANGLE_Buffer[3];
-uint16_t BNO055_COMPASS_DATA_Buffer[3];
-uint16_t BNO055_IMU_DATA_Buffer[3];
-uint16_t BNO055_M4G_DATA_Buffer[3];
-uint16_t BNO055_NDOF_DATA_Buffer[3];
+float BNO055_QUATERNION_Buffer[4];
+float BNO055_GRAVITY_VECTOR_Buffer[3];
+float BNO055_LINEAR_ACCL_Buffer[3];
+float BNO055_EULER_ANGLE_Buffer[3];
+float BNO055_COMPASS_DATA_Buffer[3];
+float BNO055_IMU_DATA_Buffer[3];
+float BNO055_M4G_DATA_Buffer[3];
+float BNO055_NDOF_DATA_Buffer[3];
 
-uint16_t BNO055_GYRO_DATA_Buffer[3];
-uint16_t BNO055_ACCL_DATA_Buffer[3];
-uint16_t BNO055_MAG_DATA_Buffer[3];
+float BNO055_GYRO_DATA_Buffer[3];
+float BNO055_ACCL_DATA_Buffer[3];
+float BNO055_MAG_DATA_Buffer[3];
 
 
 void BNO055_Init();
+void BNO055_Write(uint8_t reg, uint8_t data);
+void BNO055_Read(uint8_t reg, uint8_t bytes);
+void BNO055_Power_Mode(uint8_t mode);
 
-void BNO055_GET_ACC_DATA(void);
 
-void BNO055_GET_GYR_DATA(void);
-
+void BNO055_GET_Quaternion_DATA(void);
+void BNO055_GET_Euler_DATA(void);
+void BNO055_GET_Gravity_Vector_DATA(void);
+void BNO055_GET_Linear_Acceleration_DATA(void);
+void BNO055_GET_Compass_DATA(void);
+void BNO055_GET_GYRO_DATA(void);
+void BNO055_GET_ACCL_DATA(void);
 void BNO055_GET_MAG_DATA(void);
 
 
-void BNO055_Write(uint8_t reg, uint8_t data);
-
-void BNO055_Read(uint8_t reg, uint8_t bytes);
-
-void BNO055_Power_Mode(uint8_t mode);
 
 #endif
