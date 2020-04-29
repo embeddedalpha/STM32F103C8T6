2b3607157c7955492811f022ef311a2728e7f950
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-04-29 20:58:22 +0530

    Update BNO055.c

--- Firmware/Devices/BNO055/BNO055.c
+++ Firmware/Devices/BNO055/BNO055.c
@@ -1,7 +1,7 @@
 #include "BNO055.h"
 
 
-//accelerometer config
+
 
 void BNO055_Init()
 {
@@ -41,10 +41,10 @@ void BNO055_GET_Quaternion_DATA(void)
 
 	BNO055_Read(QUA_DATA_W_LSB,8);
 
-	QUA_W = (BNO055_Buffer[0] << 8) | BNO055_Buffer[1];
-	QUA_X = (BNO055_Buffer[2] << 8) | BNO055_Buffer[3];
-	QUA_Y = (BNO055_Buffer[4] << 8) | BNO055_Buffer[5];
-	QUA_Z = (BNO055_Buffer[6] << 8) | BNO055_Buffer[7];
+	QUA_W = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
+	QUA_X = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
+	QUA_Y = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
+	QUA_Z = (BNO055_Buffer[7] << 8) | BNO055_Buffer[6];
 
 	BNO055_QUATERNION_Buffer[0] = (float)(QUA_W / 16384.0f);
 	BNO055_QUATERNION_Buffer[1] = (float)(QUA_X / 16384.0f);
@@ -67,9 +67,9 @@ void BNO055_GET_Euler_DATA(void)
 
 	BNO055_Read(EUL_Heading_LSB,6);
 
-	RAW = (BNO055_Buffer[0] << 8) | BNO055_Buffer[1];
-	PITCH = (BNO055_Buffer[2] << 8) | BNO055_Buffer[3];
-	ROLL = (BNO055_Buffer[4] << 8) | BNO055_Buffer[5];
+	RAW = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
+	PITCH = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
+	ROLL = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
 
 
 	BNO055_EULER_ANGLE_Buffer[0] = (float)(RAW / 16.0f);
@@ -92,9 +92,9 @@ void BNO055_GET_Gravity_Vector_DATA(void)
 
 	BNO055_Read(GRV_Data_X_LSB,6);
 
-	GRV_X = (BNO055_Buffer[0] << 8) | BNO055_Buffer[1];
-	GRV_Y = (BNO055_Buffer[2] << 8) | BNO055_Buffer[3];
-	GRV_Z = (BNO055_Buffer[4] << 8) | BNO055_Buffer[5];
+	GRV_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
+	GRV_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
+	GRV_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
 
 
 	BNO055_GRAVITY_VECTOR_Buffer[0] = (float)(GRV_X / 100.0f);
@@ -117,9 +117,9 @@ void BNO055_GET_Linear_Acceleration_DATA(void)
 
 	BNO055_Read(GRV_Data_X_LSB,6);
 
-	LIN_X = (BNO055_Buffer[0] << 8) | BNO055_Buffer[1];
-	LIN_Y = (BNO055_Buffer[2] << 8) | BNO055_Buffer[3];
-	LIN_Z = (BNO055_Buffer[4] << 8) | BNO055_Buffer[5];
+	LIN_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
+	LIN_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
+	LIN_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
 
 
 	BNO055_LINEAR_ACCL_Buffer[0] = (float)(LIN_X / 100.0f);
@@ -142,9 +142,9 @@ void BNO055_GET_Compass_DATA(void)
 
 	BNO055_Read(GRV_Data_X_LSB,6);
 
-	LIN_X = (BNO055_Buffer[0] << 8) | BNO055_Buffer[1];
-	LIN_Y = (BNO055_Buffer[2] << 8) | BNO055_Buffer[3];
-	LIN_Z = (BNO055_Buffer[4] << 8) | BNO055_Buffer[5];
+	LIN_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
+	LIN_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
+	LIN_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
 
 
 	BNO055_LINEAR_ACCL_Buffer[0] = (float)(LIN_X / 100.0f);
@@ -167,9 +167,9 @@ void BNO055_GET_GYRO_DATA(void)
 
 	BNO055_Read(GYRO_DATA_X_LSB,6);
 
-	GYRO_X = (BNO055_Buffer[0] << 8) | BNO055_Buffer[1];
-	GYRO_Y = (BNO055_Buffer[2] << 8) | BNO055_Buffer[3];
-	GYRO_Z = (BNO055_Buffer[4] << 8) | BNO055_Buffer[5];
+	GYRO_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
+	GYRO_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
+	GYRO_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
 
 	BNO055_GYRO_DATA_Buffer[0] = (float)(GYRO_X / 16.0f);
 	BNO055_GYRO_DATA_Buffer[1] = (float)(GYRO_Y / 16.0f);
@@ -187,9 +187,9 @@ void BNO055_GET_ACCL_DATA(void)
 
 	BNO055_Read(ACC_DATA_X_LSB,6);
 
-	ACCL_X = (BNO055_Buffer[0] << 8) | BNO055_Buffer[1];
-	ACCL_Y = (BNO055_Buffer[2] << 8) | BNO055_Buffer[3];
-	ACCL_Z = (BNO055_Buffer[4] << 8) | BNO055_Buffer[5];
+	ACCL_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
+	ACCL_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
+	ACCL_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
 
 	BNO055_ACCL_DATA_Buffer[0] = (float)(ACCL_X / 0.01f);
 	BNO055_ACCL_DATA_Buffer[1] = (float)(ACCL_Y / 0.01f);
@@ -207,9 +207,9 @@ void BNO055_GET_MAG_DATA(void)
 
 	BNO055_Read(MAG_DATA_X_LSB,6);
 
-	MAG_X = (BNO055_Buffer[0] << 8) | BNO055_Buffer[1];
-	MAG_Y = (BNO055_Buffer[2] << 8) | BNO055_Buffer[3];
-	MAG_Z = (BNO055_Buffer[4] << 8) | BNO055_Buffer[5];
+	MAG_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
+	MAG_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
+	MAG_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
 
 	BNO055_MAG_DATA_Buffer[0] = (float)(MAG_X / 16.0f);
 	BNO055_MAG_DATA_Buffer[1] = (float)(MAG_Y / 16.0f);
