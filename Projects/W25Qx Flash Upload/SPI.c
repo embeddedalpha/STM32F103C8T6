a7ca074b0d8e38ca34dbadd07e2a94794ffc12bb
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-05-21 22:37:53 +0530

    Add files via upload

---
+++ Projects/W25Qx Flash Upload/SPI.c
@@ -0,0 +1,135 @@
+/*
+*********************************************************************************************************************************
+@file:    SPI.c
+@author:  Kunal Salvi
+@version: V3.05.0
+@date:    29/2/2020
+@brief:   SPI Driver File.
+		  This file contains the functions declared in SPI.h file.
+		  By calling various "Init" functions, the user can configure
+		  the SPI Hardware Peripheral in any of the permitted modes.
+@attention: This file aims at providing the user an abstraction layer to the SPI peripheral
+			and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
+			In case of failure of code/product due to third party tampering, the author will not be
+			responsible. This code is published under MIT License.
+*********************************************************************************************************************************
+*/
+#include "SPI.h"
+
+
+/******************************************     Master Configuration     ******************************************/
+
+
+void SPI_Master_Config(SPI_TypeDef *SPI,struct SPI_Master_Parameters SPI_M)
+{
+
+SPI ->CR1 |= SPI_CR1_MSTR;
+SPI->CR1 |= SPI_M.Baudrate << 3;
+SPI->CR1 |= SPI_M.CPOL << 1;
+SPI->CR1 |= SPI_M.CPHA << 0;
+SPI->CR1 |= SPI_M.DataFormat << 11;
+SPI->CR1 |= SPI_M.LSBorMSB << 7;
+SPI->CR1 |= SPI_M.CRC_Enable << 13;
+SPI->CR1 |= SPI_M.BiDirectional_Mode << 15;
+SPI->CR1 |= SPI_M.TxorRX << 14;
+SPI->CR2 |= SPI_M.TxDMA << 1;
+SPI->CR2 |= SPI_M.RxDMA << 0;
+SPI->CR1 |= SPI_M.Slave_Management << 9;
+SPI->CR1 |= SPI_CR1_SSI;
+SPI->CRCPR |= SPI_M.CRC_Polynomial;
+SPI->CR1 |= SPI_M.CRC_Enable << 13;
+}
+
+void SPI_Master_Enable(SPI_TypeDef *SPI)
+{
+	SPI->CR1 |= SPI_CR1_SPE;
+}
+
+void SPI_Master_IRQ_Config(struct SPI_Master_IRQ_Parameters SPIM_I)
+{
+SPI->CR2 |= (SPIM_I.TX_Interrupt) << 7;
+SPI->CR2 |= (SPIM_I.RX_Interrupt) << 6;
+SPI->CR2 |= (SPIM_I.Error_Interrupt) << 5;
+}
+
+void SPI_Master_Enable(void)
+{
+
+	SPI -> CR1 |= SPI_CR1_SPE;
+}
+
+void SPI_Master_TX(SPI_TypeDef *SPI,int data)
+{
+
+	while(!(SPI -> SR & SPI_SR_TXE));
+	SPI -> DR = data;
+}
+
+int SPI_Master_RX(void)
+{
+	while((SPI -> SR & SPI_SR_RXNE));
+	return SPI->DR;
+}
+
+void SPI_NSS_Pin_Setup(void)
+{
+
+	if(NSS_Pin < 8)
+	{
+		PORT ->CRL |= ALT_PUSH_PULL_OUTPUT << NSS_Pin;
+	}
+	else
+	{
+		PORT->CRH |= ALT_PUSH_PULL_OUTPUT << NSS_Pin;
+	}
+}
+
+void SPI_NSS_LOW(void)
+{
+	PORT ->BSRR |= (16 + NSS_Pin);
+}
+
+void SPI_NSS_HIGH(void)
+{
+	PORT ->BSRR |= NSS_Pin;
+}
+/******************************************     Slave Configuration     ******************************************/
+
+void SPI_Slave_Config(struct SPI_Slave_Parameters SPI_S)
+{
+SPI->CR1 |= SPI_S.Baudrate << 3;
+SPI->CR1 |= SPI_S.CPOL << 1;
+SPI->CR1 |= SPI_S.CPHA << 0;
+SPI->CR1 |= SPI_S.DataFormat << 11;
+SPI->CR1 |= SPI_S.LSBorMSB << 7;
+SPI->CR1 |= SPI_S.CRC_Enable << 13;
+SPI->CR1 |= SPI_S.BiDirectional_Mode << 15;
+SPI->CR1 |= SPI_S.TxorRX << 14;
+SPI -> CR1 &= ~SPI_CR1_MSTR;
+SPI->CR2 |= SPI_S.TxDMA << 1;
+SPI->CR2 |= SPI_S.RxDMA << 0;
+}
+
+
+void SPI_Slave_IRQ_Config(struct SPI_Slave_IRQ_Parameters SPIS_I)
+{
+	SPI->CR2 |= (SPIS_I.TX_Interrupt) << 7;
+	SPI->CR2 |= (SPIS_I.RX_Interrupt) << 6;
+	SPI->CR2 |= (SPIS_I.Error_Interrupt) << 5;
+}
+
+void SPI_Slave_TX(int data)
+{
+	SPI -> DR = data;
+}
+
+int SPI_Slave_RX(void)
+{
+	return SPI->DR;
+}
+
+
+void SPI_Slave_Enable(void)
+{
+	SPI -> CR1 |= SPI_CR1_SPE;
+}
---
+++ Projects/W25Qx Flash Upload/SPI.h
@@ -0,0 +1,102 @@
+#ifndef _SPI_H_
+#define _SPI_H_
+
+#include "stm32f10x.h"
+#include "board.h"
+
+GPIO_TypeDef *PORT;
+
+extern int NSS_Pin;
+
+#define LSB 0
+#define MSB 1
+#define Bit_8 0
+#define Bit_16 1
+#define Simplex 1
+#define Duplex 0
+#define Tx 1
+#define Rx 0
+#define Enable 1
+#define Disable 0
+#define Hardware_Managed 0
+#define Software_Managed 1
+
+
+struct  SPI_Master_Parameters
+{
+	uint8_t CPOL ;
+	uint8_t CPHA ;
+	uint8_t Baudrate;
+	uint8_t LSBorMSB ;
+	uint8_t DataFormat ;
+	uint8_t CRC_Enable ;
+	uint8_t BiDirectional_Mode ;
+	uint8_t TxorRX ;
+	uint8_t TxDMA ;
+	uint8_t RxDMA ;
+	uint8_t Slave_Management;
+	uint16_t CRC_Polynomial;
+	uint8_t CRC_Enable;
+};
+
+struct SPI_Master_IRQ_Parameters
+{
+	uint8_t TX_Interrupt ;
+	uint8_t RX_Interrupt ;
+	uint8_t Error_Interrupt  ;
+
+};
+
+struct  SPI_Slave_Parameters
+{
+	uint8_t CPOL ;
+	uint8_t CPHA ;
+	uint8_t Baudrate ;
+	uint8_t LSBorMSB ;
+	uint8_t DataFormat ;
+	uint8_t CRC_Enable ;
+	uint8_t BiDirectional_Mode ;
+	uint8_t TxorRX ;
+	uint8_t TxDMA ;
+	uint8_t RxDMA ;
+};
+
+struct SPI_Slave_IRQ_Parameters
+{
+	uint8_t TX_Interrupt ;
+	uint8_t RX_Interrupt ;
+	uint8_t Error_Interrupt;
+
+};
+
+
+void SPI_Master_Config(SPI_TypeDef *SPI, struct SPI_Master_Parameters SPI_M);
+void SPI_Master_IRQ_Config(SPI_TypeDef *SPI, struct SPI_Master_IRQ_Parameters SPIM_I);
+void SPI_Master_TX(SPI_TypeDef *SPI,int data);
+int SPI_Master_RX();
+void SPI_Master_Enable(SPI_TypeDef *SPI);
+
+void SPI_NSS_Pin_Setup(void);
+void SPI_NSS_LOW(void);
+void SPI_NSS_HIGH(void);
+
+void SPI_Slave_Init(struct SPI_Slave_Parameters SPI_S);
+void SPI_Slave_IRQ_Config(struct SPI_Slave_IRQ_Parameters SPIS_I);
+void SPI_Slave_Enable(void);
+void SPI_Slave_TX(int data);
+int SPI_Slave_RX(void);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
