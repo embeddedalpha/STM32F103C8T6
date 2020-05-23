a11e56b3d82ffee20deccaaee1945f6537b81535
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-05-23 13:31:14 +0530

    Update PID_Example.c

--- Firmware/PID/PID_Example.c
+++ Firmware/PID/PID_Example.c
@@ -1,1 +1,37 @@
+#include "PID.h"
+#include "stm32f10x.h"
+
+float Test(float inp)
+{
+	int output;
+	return output;
+}
+
+int main(void)
+{
+
+	PID_Controller roll;
+	roll.KD = 0.1f;
+	roll.KI = 0.01f;
+	roll.KP = 5.0f;
+	roll.Time_constant = 0.01f;
+	roll.sample_time = 0.01f;
+	roll.maxLimit = 100;
+	roll.minLimit = 0;
+
+	PID_Init(&roll);
+
+float setpoint = 50.0f;
+
+for(float t = 0.0f; t < roll.sample_time; t += roll.sample_time)
+{
+	float measurement = Test(roll.out);
+	PID_Update(&roll,setpoint,measurement);
+}
+
+
+    while(1)
+    {
+    }
+}
 
