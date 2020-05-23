040386fd52eebb9a80f8bc47dfeea9a230263e18
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-05-23 13:30:21 +0530

    Update PID.h

--- Firmware/PID/PID.h
+++ Firmware/PID/PID.h
@@ -1,1 +1,28 @@
 
+#ifndef _PID_H_
+#define _PID_H_
+
+
+
+typedef struct{
+	float KP;
+	float KI;
+	float KD;
+	float Time_constant;
+	float minLimit;
+	float maxLimit;
+	float sample_time;
+	float integrator, prevError, differentiator, prevMeasurement;
+	float out;
+
+}PID_Controller;
+
+
+void PID_Init(PID_Controller *pid);
+float PID_Update(PID_Controller *pid, float setpoint, float measurement);
+
+
+
+
+
+#endif
