36449901480ad56c692d8aff568a2d6e840d8b77
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-05-26 22:10:01 +0530

    Update OLED_SPI.h

--- Firmware/Devices/OLED_SSD1306/OLED_SPI.h
+++ Firmware/Devices/OLED_SSD1306/OLED_SPI.h
@@ -1,1 +1,28 @@
+#ifndef _OLED_SPI_H_
+#define _OLED_SPI_H_
+
+
+
+#include "stm32f10x.h"
+#include "SPI.h"
+#include "board.h"
+
+
+#define Vertical_and_Right_Horizontal_Scroll 0
+#define Vertical_and_Left_Horizontal_Scroll 1
+
+
+static int length;
+static int width;
+
+//PA7          MOSI
+//PA5          SCK1
+//PA4          NSS
+//PA3          D/C
+//PA2          RESET
+
+
+
+
+#endif
 
