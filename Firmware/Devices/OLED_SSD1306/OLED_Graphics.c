7ce29ad6bcc28bc3fac6263a4963e19d70c2d8b1
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-05-26 22:34:58 +0530

    Update OLED_Graphics.c

--- Firmware/Devices/OLED_SSD1306/OLED_Graphics.c
+++ Firmware/Devices/OLED_SSD1306/OLED_Graphics.c
@@ -1,1 +1,22 @@
+#include "OLED_Graphics.h"
+
+
+
+
+
+void OLED_Draw_Line(int x1, int x2, int y1, int y2, int thickness)
+{
+
+
+}
+
+void OLED_Draw_Rectangle(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4, int thickess, int fill_or_nofill)
+{
+
+}
+
+void OLED_Draw_Circle(int x1, int y1, int radius, int thickness, int fill_or_nofill)
+{
+
+}
 
