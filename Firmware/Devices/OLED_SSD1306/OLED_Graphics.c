19a82e0d63cc0a5f93db5513457e1181fea1bf5b
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-05-27 21:12:32 +0530

    Update OLED_Graphics.c

--- Firmware/Devices/OLED_SSD1306/OLED_Graphics.c
+++ Firmware/Devices/OLED_SSD1306/OLED_Graphics.c
@@ -1,22 +1,34 @@
-#include "OLED_Graphics.h"
+#ifndef _OLED_GRAPHICS_H_
+#define _OLED_GRAPHICS_H_
 
 
 
 
 
-void OLED_Draw_Line(int x1, int x2, int y1, int y2, int thickness)
-{
+#include "OLED_SPI.h"
+
+static int graphics_buffer[length*width];
 
 
-}
+void OLED_Draw_Line(int x1, int x2, int y1, int y2, int thickness);
 
-void OLED_Draw_Rectangle(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4, int thickess, int fill_or_nofill)
-{
+void OLED_Draw_Rectangle(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4, int thickess, int fill_or_nofill);
 
-}
+void OLED_Draw_Circle(int x1, int y1, int radius, int thickness, int fill_or_nofill);
 
-void OLED_Draw_Circle(int x1, int y1, int radius, int thickness, int fill_or_nofill)
-{
+void OLED_Fill_Screen(void);
 
-}
+void OLED_Clear_Screen(void);
 
+
+
+
+
+
+
+
+
+
+
+
+#endif
