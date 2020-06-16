921b9d78bc5059d915aa7bb6fb49c3b4b171dcad
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-06-16 17:23:33 +0530

    Update Timer.c

--- Firmware/Timer/Timer.c
+++ Firmware/Timer/Timer.c
@@ -664,6 +664,90 @@ timer -> CR1 |= TIM_CR1_CEN;
 }
 //
 
-//************************************************** Timer Input XOR MODE *****************************************************
+//************************************************** INPUT PWM MODE *****************************************************
 
+uint32_t Timer_Input_PWM_Capture_Channel_1(TIM_TypeDef *TIM)
+{
+	if(TIM == TIM1)
+	{
+		RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
 
+	}
+	if(TIM == TIM2)
+	{
+		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	}
+	if(TIM == TIM3)
+	{
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	}
+	if(TIM == TIM4)
+	{
+		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	}
+	TIM -> CCMR1 |= TIM_CCMR1_CC1S_0; //IC1 mapped to T1
+	TIM -> CCMR1 |= TIM_CCMR1_CC2S_1; //IC2 mapped to T1
+
+	TIM -> CCMR1 |= (TIM_CCMR1_IC1F_0 | TIM_CCMR1_IC1F_1);
+	TIM -> CCMR1 |= (TIM_CCMR1_IC2F_0 | TIM_CCMR1_IC2F_1);
+
+	TIM -> CCER &= ~TIM_CCER_CC1P; //non inverted
+	TIM -> CCER |= TIM_CCER_CC2P; //inverted
+
+	TIM -> SMCR |= TIM_SMCR_TS_2 | TIM_SMCR_TS_0; //trigger select
+
+	TIM -> SMCR |= TIM_SMCR_SMS_2;
+
+	TIM -> CCMR1 &= ~TIM_CCMR1_IC1PSC;
+	TIM -> CCMR1 &= ~TIM_CCMR1_IC2PSC;
+
+	TIM -> CCER |= TIM_CCER_CC1E;
+	TIM -> CCER |= TIM_CCER_CC2E;
+
+	TIM -> CR1 |= TIM_CR1_CEN;
+
+	return (TIM->CCR1 << 16 | TIM->CCR2);
+}
+
+uint32_t Timer_Input_PWM_Capture_Channel_2(TIM_TypeDef *TIM)
+{
+	if(TIM == TIM1)
+	{
+		RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+
+	}
+	if(TIM == TIM2)
+	{
+		RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	}
+	if(TIM == TIM3)
+	{
+		RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	}
+	if(TIM == TIM4)
+	{
+		RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	}
+	TIM -> CCMR2 |= TIM_CCMR2_CC3S_0; //IC1 mapped to T1
+	TIM -> CCMR2 |= TIM_CCMR2_CC4S_1; //IC2 mapped to T1
+
+	TIM -> CCMR2 |= (TIM_CCMR2_IC3F_0 | TIM_CCMR2_IC3F_1);
+	TIM -> CCMR2 |= (TIM_CCMR2_IC4F_0 | TIM_CCMR2_IC4F_1);
+
+	TIM -> CCER &= ~TIM_CCER_CC3P; //non inverted
+	TIM -> CCER |= TIM_CCER_CC4P; //inverted
+
+	TIM -> SMCR |= TIM_SMCR_TS_2 | TIM_SMCR_TS_0; //trigger select
+
+	TIM -> SMCR |= TIM_SMCR_SMS_2;
+
+	TIM -> CCMR2 &= ~TIM_CCMR2_IC3PSC;
+	TIM -> CCMR2 &= ~TIM_CCMR2_IC4PSC;
+
+	TIM -> CCER |= TIM_CCER_CC3E;
+	TIM -> CCER |= TIM_CCER_CC4E;
+
+	TIM -> CR1 |= TIM_CR1_CEN;
+
+	return (TIM->CCR3 << 16 | TIM->CCR4);
+}
