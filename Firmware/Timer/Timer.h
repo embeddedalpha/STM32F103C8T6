6ffc25306afbf35e1b92e7a007a0a6cc7067e44d
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-06-22 20:53:57 +0530

    Update Timer.h

--- Firmware/Timer/Timer.h
+++ Firmware/Timer/Timer.h
@@ -5,6 +5,7 @@
 
 #include "stm32f10x.h"
 
+#define frequency 10000
 #define TIMER_CLOCK_GENERAL 36000000
 #define TIMER_CLOCK_ADVANCED 72000000
 
@@ -27,31 +28,26 @@ uint16_t Timer_Input_Capture_Channel_4(TIM_TypeDef *TIM);
 uint32_t Timer_Input_PWM_Capture_Channel_1(TIM_TypeDef *TIM);
 uint32_t Timer_Input_PWM_Capture_Channel_2(TIM_TypeDef *TIM);
 
-void Output_Compare_Channel_1(TIM_Typedef *timer, uint16_t compare_value_1, uint16_t compare_Value_2);
-void Output_Compare_Channel_2(TIM_Typedef *timer, uint16_t compare_value_1, uint16_t compare_Value_2);
-void Output_Compare_Channel_3(TIM_Typedef *timer, uint16_t compare_value_1, uint16_t compare_Value_2);
-void Output_Compare_Channel_4(TIM_Typedef *timer, uint16_t compare_value_1, uint16_t compare_Value_2);
+void Output_Compare_Channel_1(TIM_TypeDef *timer, uint16_t compare_value_1, uint16_t compare_Value_2);
+void Output_Compare_Channel_2(TIM_TypeDef *timer, uint16_t compare_value_1, uint16_t compare_Value_2);
+void Output_Compare_Channel_3(TIM_TypeDef *timer, uint16_t compare_value_1, uint16_t compare_Value_2);
+void Output_Compare_Channel_4(TIM_TypeDef *timer, uint16_t compare_value_1, uint16_t compare_Value_2);
 
-void PWM_Channel_1_Init(TIM_Typedef *timer, uint16_t frequency);
-void PWM_Channel_2_Init(TIM_Typedef *timer, uint16_t frequency);
-void PWM_Channel_3_Init(TIM_Typedef *timer, uint16_t frequency);
-void PWM_Channel_4_Init(TIM_Typedef *timer, uint16_t frequency);
+void PWM_Channel_1_Init(TIM_TypeDef *timer, uint16_t freq);
+void PWM_Channel_2_Init(TIM_TypeDef *timer, uint16_t freq);
+void PWM_Channel_3_Init(TIM_TypeDef *timer, uint16_t freq);
+void PWM_Channel_4_Init(TIM_TypeDef *timer, uint16_t freq);
 
-void PWM_Channel_1_Update(TIM_Typedef *timer, uint16_t duty_cycle);
-void PWM_Channel_2_Update(TIM_Typedef *timer, uint16_t duty_cycle);
-void PWM_Channel_3_Update(TIM_Typedef *timer, uint16_t duty_cycle);
-void PWM_Channel_4_Update(TIM_Typedef *timer, uint16_t duty_cycle);
+void PWM_Channel_1_Update(TIM_TypeDef *timer, uint16_t duty_cycle);
+void PWM_Channel_2_Update(TIM_TypeDef *timer, uint16_t duty_cycle);
+void PWM_Channel_3_Update(TIM_TypeDef *timer, uint16_t duty_cycle);
+void PWM_Channel_4_Update(TIM_TypeDef *timer, uint16_t duty_cycle);
 
-void One_Pulse_Mode_Channel_1(TIM_Typedef *timer, uint16_t t_delay);
-void One_Pulse_Mode_Channel_2(TIM_Typedef *timer, uint16_t t_delay);
-
-
+void One_Pulse_Mode_Channel_1(TIM_TypeDef *timer, uint16_t t_delay);
+void One_Pulse_Mode_Channel_2(TIM_TypeDef *timer, uint16_t t_delay);
 
 
-
-
-
-
+void Timer_Hall_Sensor(TIM_TypeDef *TIM, uint16_t* hall);
 
 
 #endif
