01225965d653d39cfe33890fa95c157f796deb57
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-08-25 23:57:55 +0530

    Update ILI9341.h

--- Firmware/Devices/ILI9341 Display/ILI9341.h
+++ Firmware/Devices/ILI9341 Display/ILI9341.h
@@ -1,1 +1,29 @@
+#ifndef  _ILI9341_H_
+#define  _ILI9341_H_
+
+#include "stm32f10x.h"
+#include "SPI.h"
+
+//	D/CX -> 1 data & 0 command
+
+// 4 wire 8 - bit data serial IM[3:0] 1110
+
+
+
+//https://cdn-shop.adafruit.com/datasheets/ILI9341.pdf
+
+
+void ILI9341_Display_Init();
+
+void ILI9341_NOP(void);
+
+
+
+
+
+
+
+
+
+#endif
 
