99213ff55984b18fbc5bfb7c2758b662f1daaa14
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-08-27 23:35:01 +0530

    Update ILI9341.h

--- Firmware/Devices/ILI9341 Display/ILI9341.h
+++ Firmware/Devices/ILI9341 Display/ILI9341.h
@@ -13,11 +13,32 @@
 //https://cdn-shop.adafruit.com/datasheets/ILI9341.pdf
 
 
+#define Display_Width  360
+#define Display_Height 240
+
+
+
+/*
+ * USER ASSESSIBLE DATA
+ */
+
 void ILI9341_Display_Init();
 
-void ILI9341_NOP(void);
+void ILI9341_Display_Inversion_OFF(void);
+void ILI9341_Display_Inversion_ON(void);
 
+void ILI9341_Display_OFF(void);
+void ILI9341_Display_ON(void);
 
+void ILI9341_Reset_Low(void);
+void ILI9341_Reset_High(void);
+
+void ILI9341_Define_Window(uint16_t X1, uint16_t Y1, uint16_t W, uint16_t H);
+void ILI9341_Draw_Pixel(uint16_t x, uint16_t y, uint16_t* color);
+void ILI9341_Write_Display_Brightness(uint8_t brightness);
+
+void ILI9341_Display_Inversion_ON(void);
+void ILI9341_Display_Inversion_OFF(void);
 
 
 
@@ -26,4 +47,3 @@ void ILI9341_NOP(void);
 
 
 #endif
-
