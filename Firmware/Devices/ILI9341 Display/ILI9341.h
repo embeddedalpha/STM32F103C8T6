1999f017fa4e6f6e774205b4b7bbb0a1feb3388f
Embedded_Alpha <kunalsalvi63@gmail.com>
2020-08-28 16:23:21 +0530

    Update ILI9341.h

--- Firmware/Devices/ILI9341 Display/ILI9341.h
+++ Firmware/Devices/ILI9341 Display/ILI9341.h
@@ -23,7 +23,6 @@
  */
 
 void ILI9341_Display_Init();
-
 void ILI9341_Display_Inversion_OFF(void);
 void ILI9341_Display_Inversion_ON(void);
 
@@ -32,13 +31,28 @@ void ILI9341_Display_ON(void);
 
 void ILI9341_Reset_Low(void);
 void ILI9341_Reset_High(void);
-
-void ILI9341_Define_Window(uint16_t X1, uint16_t Y1, uint16_t W, uint16_t H);
-void ILI9341_Draw_Pixel(uint16_t x, uint16_t y, uint16_t* color);
-void ILI9341_Write_Display_Brightness(uint8_t brightness);
-
 void ILI9341_Display_Inversion_ON(void);
 void ILI9341_Display_Inversion_OFF(void);
+void ILI9341_Define_Window(uint16_t X1, uint16_t Y1, uint16_t W, uint16_t H);
+uint32_t ILI9341_Set_Cursor(uint16_t x, uint16_t y );
+void ILI9341_Get_Cursor(void);
+void ILI9341_Draw_Pixel(uint16_t x, uint16_t y, uint16_t* color);
+void ILI9341_Write_Display_Brightness(uint8_t brightness);
+void ILI9341_Draw_Line(uint16_t x1, uint16_t x2, uint16_t y1, uint16_t y2, uint16_t color );
+void ILI9341_Draw_Rectangle(uint16_t x1, uint16_t x2, uint16_t y1, uint16_t y2, uint16_t color);
+void ILI9341_Draw_Rectangle_Filled(uint16_t x1, uint16_t x2, uint16_t y1, uint16_t y2, uint16_t color);
+void ILI9341_Draw_Circle(uint16_t x, uint16_t y, uint16_t radius, uint16_t color );
+void ILI9341_Draw_Circle_Filled(uint16_t x, uint16_t y, uint16_t radius, uint16_t color );
+
+
+void ILI9341_Print_Characters(char *c, uint16_t color);
+void ILI9341_Print_Numbers(uint16_t *n,uint8_t font, uint16_t color);
+
+void ILI9341_Draw_Image(uint16_t x, uint16_t y, uint16_t *data);
+void ILI9341_Vertical_Scroll(uint16_t x, uint16_t y, uint16_t BottomFixedArea );
+void ILI9341_Horizontal_Scroll(uint16_t x, uint16_t y, uint16_t color);
+void ILI9341_Tearing_Effect();
+void ILI9341_Paint_Screen(uint16_t color);
 
 
 
