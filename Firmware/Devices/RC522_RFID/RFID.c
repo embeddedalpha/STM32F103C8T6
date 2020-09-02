341c69abe4ce89965154c1406ec93607eee30834
Axion Engineering <kunalsalvi63@gmail.com>
2020-09-02 20:39:25 +0530

    Update RFID.c

--- Firmware/Devices/RC522_RFID/RFID.c
+++ Firmware/Devices/RC522_RFID/RFID.c
@@ -1,10 +1,13 @@
 #include "RFID.h"
 
-void RFID_SPI_Init(void)
+void MFRC522_Send_Data(SPI_TypeDef *SPI, uint8_t address, uint8_t *data, int len)
 {
+	SPI_NSS_HIGH();
+	SPI_NSS_LOW();
+	SPI_Master_TX(SPI,address << 1); //SEND ADDRESS (WRITE)
+	for(int i =0; i< len; i++)
+	{
+		SPI_Master_TX(SPI,data);
+	}
 
-	SPI_Master_Init(SPI1,0,0,8,1,8);
-	SPI1_Pin_Config_PortA();
-
-}
-	
+}	
