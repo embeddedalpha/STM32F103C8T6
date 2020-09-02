83f35857d06a11b35696b5fedbc9f541e295ad24
Axion Engineering <kunalsalvi63@gmail.com>
2020-09-02 20:33:04 +0530

    Update RFID.h

--- Firmware/Devices/RC522_RFID/RFID.h
+++ Firmware/Devices/RC522_RFID/RFID.h
@@ -3,95 +3,125 @@
 
 
 #include "stm32f10x.h"
+#include "board.h"
 #include "SPI.h"
 
-int buffer_1[64];
-/*
-PIN 1 -> VCC
-PIN 2 -> RST
-PIN 3 -> GND
-PIN 4 -> IRQ
-PIN 5 -> MISO / SCL / TX
-PIN 6 -> MOSI
-PIN 7 -> SCK
-PIN 8 -> RX / SDA / SS
-*/
+//1. VCC
+//2. RST
+//3. GND
+//4. IRQ
+//5. MISO
+//6. MOSI
+//7. SCK
+//8. SS
+
+struct Command_and_Status
+{
+	uint8_t		CommandReg				= 0x01 ;	// starts and stops command execution
+	uint8_t		ComIEnReg				= 0x02 ;	// enable and disable interrupt request control bits
+	uint8_t		DivIEnReg				= 0x03 ;	// enable and disable interrupt request control bits
+	uint8_t		ComIrqReg				= 0x04 ;	// interrupt request bits
+	uint8_t		DivIrqReg				= 0x05 ;	// interrupt request bits
+	uint8_t		ErrorReg				= 0x06 ;	// error bits showing the error status of the last command executed
+	uint8_t		Status1Reg				= 0x07 ;	// communication status bits
+	uint8_t		Status2Reg				= 0x08 ;	// receiver and transmitter status bits
+	uint8_t		FIFODataReg				= 0x09 ;	// input and output of 64 byte FIFO buffer
+	uint8_t		FIFOLevelReg			= 0x0A ;	// number of bytes stored in the FIFO buffer
+	uint8_t		WaterLevelReg			= 0x0B ;	// level for FIFO underflow and overflow warning
+	uint8_t		ControlReg				= 0x0C ;	// miscellaneous control registers
+	uint8_t		BitFramingReg			= 0x0D ;	// adjustments for bit-oriented frames
+	uint8_t		CollReg					= 0x0E ;	//bit position of the first bit-collision detected on the RF interface
+}Command_and_Status;
+
+
+struct Command
+{
+	uint8_t		ModeReg 				= 0x11;		//defines general modes for transmitting and receiving
+	uint8_t		TxModeReg  				= 0x11;		//defines transmission data rate and framing
+	uint8_t		RxModeReg   			= 0x11;		//defines reception data rate and framing
+	uint8_t		TxControlReg 			= 0x11;		//controls the logical behavior of the antenna driver pins TX1 and TX2
+	uint8_t		TxASKReg 				= 0x11;		//controls the setting of the transmission modulation Table 63 on page 51
+	uint8_t		TxSelReg 				= 0x11;		//selects the internal sources for the antenna driver Table 65 on page 51
+	uint8_t		RxSelReg 				= 0x11;		//selects internal receiver settings Table 67 on page 52
+	uint8_t		RxThresholdReg 			= 0x11;		//selects thresholds for the bit decoder Table 69 on page 53
+	uint8_t		DemodReg 				= 0x11;		//defines demodulator settings Table 71 on page 53
+	uint8_t		MfTxReg 				= 0x11;		//controls some MIFARE communication transmit parameters Table 77 on page 55
+	uint8_t		MfRxReg 				= 0x11;		//controls some MIFARE communication receive parameters Table 79 on page 55
+	uint8_t		SerialSpeedReg			= 0x11;		//selects the speed of the serial UART interface
+}Command;
 
 
 
-//Commands
-#define	Idle 0b0000
-#define Mem  0b0001
-#define Generate_RandomID 0b0010
-#define CalcCRC 0b0011
-#define Transmit 0b0100
-#define NoCmdChange 0b0111
-#define Transceive 0b1100
-#define MFAuthent 0b1110
-#define SoftReset 0b1111
+struct Configuration
+{
+	uint8_t		CRCResultRegH			= 0x21 ;	// shows the MSB and LSB values of the CRC calculation
+	uint8_t		CRCResultRegL			= 0x22 ;
+	uint8_t		ModWidthReg				= 0x24 ;	// controls the ModWidth setting?
+	uint8_t		RFCfgReg				= 0x26 ;	// configures the receiver gain
+	uint8_t		GsNReg					= 0x27 ;	// selects the conductance of the antenna driver pins TX1 and TX2 for modulation
+	uint8_t		CWGsPReg				= 0x28 ;	// defines the conductance of the p-driver output during periods of no modulation
+	uint8_t		ModGsPReg				= 0x29 ;	// defines the conductance of the p-driver output during periods of modulation
+	uint8_t		TModeReg				= 0x2A ;	// defines settings for the internal timer
+	uint8_t		TPrescalerReg			= 0x2B ;	// the lower 8 bits of the TPrescaler value. The 4 high bits are in TModeReg.
+	uint8_t		TReloadRegH				= 0x2C ;	// defines the 16-bit timer reload value
+	uint8_t		TReloadRegL				= 0x2D ;
+	uint8_t		TCounterValueRegH		= 0x2E ;	// shows the 16-bit timer value
+	uint8_t		TCounterValueRegL		= 0x2F ;
+}Configuration;
 
 
-#define Read_Register  0x80
-#define Write_Register 0x00
+struct Test_Register
+{
+	uint8_t		TestSel1Reg				= 0x31 ;	// general test signal configuration
+	uint8_t		TestSel2Reg				= 0x32 ;	// general test signal configuration
+	uint8_t		TestPinEnReg			= 0x33 ;	// enables pin output driver on pins D1 to D7
+	uint8_t		TestPinValueReg			= 0x34 ;	// defines the values for D1 to D7 when it is used as an I/O bus
+	uint8_t		TestBusReg				= 0x35 ;	// shows the status of the internal test bus
+	uint8_t		AutoTestReg				= 0x36 ;	// controls the digital self-test
+	uint8_t		VersionReg				= 0x37 ;	// shows the software version
+	uint8_t		AnalogTestReg			= 0x38 ;	// controls the pins AUX1 and AUX2
+	uint8_t		TestDAC1Reg				= 0x39 ;	// defines the test value for TestDAC1
+	uint8_t		TestDAC2Reg				= 0x3A ;	// defines the test value for TestDAC2
+	uint8_t		TestADCReg				= 0x3B ;	// shows the value of ADC I and Q channels
+}Test_Register;
 
-//Page 0
-#define CommandReg 0x01 << 1
-#define ComlEnReg	 0x02 << 1
-#define DivlEnReg  0x03 << 1
-#define ComIrqReg  0x04 << 1
-#define DivIrqReg	 0x05 << 1
-#define ErrorReg   0x06 << 1
-#define Status1Reg 0x07 << 1
-#define Status2Reg 0x08 << 1
-#define FIFODataReg 0x09 << 1
-#define FIFOLevelReg 0x0a << 1
-#define WaterLevelReg 0x0b << 1
-#define ControlReg  0x0c << 1
-#define BitFramingReg 0x0d << 1
-#define CollReg     0x0e << 1
 
-//Page 1
-#define ModeReg 	0x11 << 1
-#define TxModeReg 0x12 << 1
-#define RxModeReg 0x13 << 1
-#define TxControlReg 0x14 << 1
-#define TxASKReg	0x15 << 1
-#define TxSelReg 0x16 << 1
-#define RxSelReg 0x17 << 1
-#define RxThresholdReg 0x18 << 1
-#define DemodReg 0x19 << 1
-#define MfTxReg 0x1c << 1
-#define MfRxReg 0x1d << 1
-#define SerialSpeedReg 0x1f << 1
+struct General_Command {
+	uint8_t 	Idle 					= 0b0000; 	//no action, cancels current command execution
+	uint8_t		Mem  					= 0b0001; 	//stores 25 bytes into the internal buffer
+	uint8_t		Generate_RandomID 		= 0b0010; 	//generates a 10-byte random ID number
+	uint8_t		CalcCRC 				= 0b0011; 	//activates the CRC co-processor or performs a self test
+	uint8_t		Transmit 				= 0b0100;	//transmits data from the FIFO buffer
+	uint8_t		NoCmdChange 			= 0b0111; 	//no command change, can be used to modify the CommandReg register bits without affecting the command, for example, the PowerDown bit
+	uint8_t		Receive  				= 0b1000; 	//activates the receiver circuits
+	uint8_t		Transceive 				= 0b1100; 	//transmits data from FIFO buffer to antenna and automatically activates the receiver after transmission
+	uint8_t		MFAuthent 				= 0b1110; 	//performs the MIFARE standard authentication as a reader
+	uint8_t		SoftReset 				= 0b1111; 	//resets the MRFC522
+}GeneralCommand;
 
-//Page 2
-#define CRCResultReg_1 0x21 << 1
-#define CRCResultReg_2 0x22 << 1
-#define ModWidthReg 0x24 << 1
-#define RFCfgReg 0x26 << 1
-#define GsNReg 0x27 << 1
-#define CWGsPReg 0x28 << 1
-#define ModGsPReg 0x29 << 1
-#define TModeReg 0x2a << 1
-#define TPrescalerReg 0x2b << 1
-#define TReloadReg_1 0x2c << 1
-#define TReloadReg_2 0x2d << 1
-#define TCounterValReg_1 0x2e << 1
-#define TCounterValReg_2 0x2f << 1
+#define PowerDown								1<<4
+#define Analog_Receiver_Off						1<<5
+#define Analog_Receiver_ON						0<<5
 
-//Page 3
-#define TestSel1Reg 0x31 << 1
-#define TestSel2Reg 0x32 << 1
-#define TestPinEnReg 0x33 << 1
-#define TestPinValueReg 0x34 << 1
-#define TestBusReg 0x35 << 1
-#define AutoTestReg 0x36 << 1
-#define VersionReg 0x37 << 1
-#define AnalogTestReg 0x38 << 1
-#define TestDAC1Reg 0x39 << 1
-#define TestDAC2Reg 0x3a << 1
-#define TestADCReg 0x3b << 1
 
+#define Allow_Transmitter_Interrupt_Request		1<<6
+#define Deny_Transmitter_Interrupt_Request		0<<6
+#define Allow_Receiver_Interrupt_Request		1<<5
+#define Deny_Receiver_Interrupt_Request			0<<5
+#define Allow_Idle_Interrupt_Request			1<<4
+#define Deny_Idle_Interrupt_Request				0<<4
+#define Allow_High_Alert_Interrupt_Request		1<<3
+#define Deny_High_Alert_Interrupt_Request		0<<3
+#define Allow_Low_Alert_Interrupt_Request		1<<2
+#define Deny_Low_Alert_Interrupt_Request		0<<2
+#define Allow_Error_Interrupt_Request			1<<1
+#define Deny_Error_Interrupt_Request			0<<1
+#define Allow_Timer_Interrupt_Request			1<<0
+#define Deny_Timer_Interrupt_Request			0<<0
+
+#define IRQpin_isCMOS							1<<7
+#define Allow_MFIN_Active_Interrupt_Request		1<<4
+#define Allow_CRC_Interrupt_Request				1<<2
 
 
 
