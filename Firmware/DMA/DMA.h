56c4c3449747f34bb86d118b6e61ed3f458fd699
Axion Engineering <kunalsalvi63@gmail.com>
2020-09-18 23:53:00 +0530

    Update DMA.h

--- Firmware/DMA/DMA.h
+++ Firmware/DMA/DMA.h
@@ -3,24 +3,29 @@
 
 
 #include "stm32f10x.h"
+#include <stdbool.h>
 
 
 
 void DMA_Memory_2_Memory(uint32_t destination, uint32_t source, uint8_t direction, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+
 void USART1_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void USART1_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void USART2_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void USART2_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void USART3_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void USART3_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+
 void I2C1_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void I2C1_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void I2C2_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void I2C2_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+
 void SPI1_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void SPI1_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void SPI2_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 void SPI2_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+
 void ADC1_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 
 
