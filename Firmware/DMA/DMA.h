c3cbe58fcbe7917f833c9c79b38815cb3109e6f6
Axion Engineering <kunalsalvi63@gmail.com>
2020-09-19 16:47:09 +0530

    Update DMA.h

--- Firmware/DMA/DMA.h
+++ Firmware/DMA/DMA.h
@@ -10,30 +10,30 @@
 #define DMA_Priority_Medium    1
 #define DMA_Priority_Low       0
 
+void DMA_Memory_2_Memory(uint32_t destination, uint32_t source, uint8_t direction, uint16_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
 
+void USART1_TX_DMA_Init(uint32_t source_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
+void USART1_RX_DMA_Init(uint32_t destination_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
 
-void DMA_Memory_2_Memory(uint32_t destination, uint32_t source, uint8_t direction, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+void USART2_TX_DMA_Init(uint32_t source_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
+void USART2_RX_DMA_Init(uint32_t destination_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
 
-void USART1_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void USART1_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void USART2_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void USART2_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void USART3_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void USART3_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+void USART3_TX_DMA_Init(uint32_t source_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
+void USART3_RX_DMA_Init(uint32_t destination_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
 
-void I2C1_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void I2C1_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void I2C2_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void I2C2_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+void I2C1_TX_DMA_Init(uint32_t source_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
+void I2C1_RX_DMA_Init(uint32_t destination_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
 
-void SPI1_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void SPI1_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void SPI2_TX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
-void SPI2_RX_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+void I2C2_TX_DMA_Init(uint32_t source_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
+void I2C2_RX_DMA_Init(uint32_t destination_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
 
-void ADC1_DMA(uint32_t desitnation, uint8_t data_buffer_size, uint8_t memory_size, uint8_t peripheral_size, uint8_t priority);
+void SPI1_TX_DMA_Init(uint32_t source_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
+void SPI1_RX_DMA_Init(uint32_t destination_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
 
+void SPI2_TX_DMA_Init(uint32_t source_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
+void SPI2_RX_DMA_Init(uint32_t destination_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
 
+void ADC1_DMA_Init(uint32_t source_address, uint16_t buffer_size, uint8_t priority, bool Circular_buffer);
 
 
 
