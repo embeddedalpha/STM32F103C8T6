83e7b106adba57bdfaee53ae43fbfaf112663682
Axion Engineering <kunalsalvi63@gmail.com>
2020-09-23 15:46:52 +0530

    Update USART.h

--- Firmware/UART/USART.h
+++ Firmware/UART/USART.h
@@ -3,22 +3,73 @@
 
 
 #include "stm32f10x.h"
+#include <stdbool.h>
+#include "board.h"
 
 
 
+/**********************************************************************************************************************
+ * @brief: This function sets up basic functions of UART.
+ * @parameters: uart         ->       USART1
+ *                                    USART2
+ *                                    USART3
+ *              baudrate     ->       2400
+ *                                    9600
+ *                                    19200
+ *                                    57600
+ *                                    115200
+ *                                    230400
+ *                                    460800
+ *                                    921600
+ *                                    2250000
+ *                                    450000
+ *              frame_length ->   0:  1 Start bit, 8 Data bits, n Stop bit
+ *                                1:  1 Start bit, 9 Data bits, n Stop bit
+ *              stop_bits    ->   0:  1 Stop bit
+ *                                1:  0.5 Stop bit
+ *                                2:  2 Stop bits
+ *                                3:  1.5 Stop bit
+ *              DMA_TX       ->   0:  DMA mode is enabled for transmission
+ *                                1:  DMA mode is disabled for transmission
+ *              DMA_RX       ->   0:  DMA mode is enabled for reception
+ *                                1:  DMA mode is disabled for reception
+ **********************************************************************************************************************/
+
+void UART_Setup(USART_TypeDef *uart,uint8_t baudrate, bool frame_length, uint8_t stop_bits, bool DMA_TX, bool DMA_RX);
+
+
+/**********************************************************************************************************************
+*@brief: This function sets up various interrupts for USART/UART block
+*@parameters: uart  ->  USART1
+*                       USART2
+*                       USART3
+*             parity_interrupt                      ->  0: Disable
+*                                                       1: Enable
+*             transmitter_register_empty_interrupt  ->  1: Enable
+*                                                       0: Disable
+*             transmitter_complete_interrupt        ->  1: Enable
+*                                                       0: Disable
+*             receiver_register_empty_interrupt     ->  1: Enable
+*                                                       0: Disable
+*             idle_interrupt                        ->  1: Enable
+*                                                       0: Disable
+*             CTS_interrupt                         ->  1: Enable
+*                                                       0: Disable
+*             Error_interrupt                       ->  1: Enable
+*                                                       0: Disable
+***********************************************************************************************************************/
 
 
 
-
-
-
-
-
-
-
-
-
-
+void USART_Interrupt_Setup(USART_TypeDef *uart,
+						  bool parity_interrupt,
+						  bool transmitter_register_empty_interrupt,
+						  bool transmitter_complete_interrupt,
+						  bool receiver_register_empty_interrupt,
+						  bool idle_interrupt,
+						  bool CTS_interrupt,
+						  bool Error_interrupt
+						  );
 
 
 #endif
