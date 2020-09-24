78ea1858eff9d1a0acdf8125653464e0364874e4
Axion Engineering <kunalsalvi63@gmail.com>
2020-09-24 12:09:40 +0530

    Update USART.h

--- Firmware/UART/USART.h
+++ Firmware/UART/USART.h
@@ -109,6 +109,40 @@ uint8_t UART_Get_Data(USART_TypeDef *uart);
 void UART_Send_Data(USART_TypeDef *uart,uint8_t data);
 
 
+/***********************************************************************************************************************/
+//                                                         USART
+/***********************************************************************************************************************/
+
+
+/**********************************************************************************************************************
+ * @brief: This function sets up basic functions of USART (with clock)
+ * @parameters: uart         ->       USART1
+ *                                    USART2
+ *                                    USART3
+ *              baudrate     ->       2400
+ *                                    9600
+ *                                    19200
+ *                                    57600
+ *                                    115200
+ *                                    230400
+ *                                    460800
+ *                                    921600
+ *                                    2250000
+ *                                    450000
+ *              frame_length ->   0:  1 Start bit, 8 Data bits, n Stop bit
+ *                                1:  1 Start bit, 9 Data bits, n Stop bit
+ *              stop_bits    ->   0:  1 Stop bit
+ *                                1:  0.5 Stop bit
+ *                                2:  2 Stop bits
+ *                                3:  1.5 Stop bit
+ *              DMA_TX       ->   0:  DMA mode is enabled for transmission
+ *                                1:  DMA mode is disabled for transmission
+ *              DMA_RX       ->   0:  DMA mode is enabled for reception
+ *                                1:  DMA mode is disabled for reception
+ **********************************************************************************************************************/
+
+
+void USART_Setup(USART_TypeDef *uart,uint8_t baudrate, bool frame_length, uint8_t stop_bits, bool DMA_TX, bool DMA_RX);
 
 
 #endif
