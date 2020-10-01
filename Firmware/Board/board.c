931090904b1889cb4ab6f693698538223cd2c266
Axion Engineering <kunalsalvi63@gmail.com>
2020-10-01 21:40:32 +0530

    Update board.c

--- Firmware/Board/board.c
+++ Firmware/Board/board.c
@@ -31,11 +31,11 @@ void Delay_us(TIM_TypeDef *timer,float usSeconds)
 	timer->PSC = 1;
 	if(timer == TIM1)
 	{
-	timer -> ARR = (usSeconds*TIMER_CLOCK_ADVANCED)/2; 
+	timer -> ARR = (usSeconds*TIMER_CLOCK_ADVANCED)/2;
 	}
 	else
 	{
-	timer -> ARR = (usSeconds*TIMER_CLOCK_GENERAL)/2; 
+	timer -> ARR = (usSeconds*TIMER_CLOCK_GENERAL)/2;
 	}
 	timer->CR1 |= TIM_CR1_URS;
 	timer->CR1 |= TIM_CR1_CEN;
@@ -51,11 +51,11 @@ void Delay_ms(TIM_TypeDef *timer,float msSeconds)
 	timer->SR = 0;
 	if(timer == TIM1){
 	timer->PSC = 72;
-	timer -> ARR = (msSeconds*TIMER_CLOCK_ADVANCED)/2; 
+	timer -> ARR = (msSeconds*TIMER_CLOCK_ADVANCED)/2;
 	}
 	else{
 	timer->PSC = 36;
-	timer -> ARR = (msSeconds*TIMER_CLOCK_GENERAL)/2; 
+	timer -> ARR = (msSeconds*TIMER_CLOCK_GENERAL)/2;
 	}
 	timer->CR1 |= TIM_CR1_URS;
 	timer->CR1 |= TIM_CR1_CEN;
@@ -68,45 +68,20 @@ void Delay_s(TIM_TypeDef *timer,float Seconds){
 
 	timer->SR = 0;
 	timer->PSC = 65535;
-	timer -> ARR = (Seconds*TIMER_CLOCK_ADVANCED)/2; 
+	timer -> ARR = (Seconds*TIMER_CLOCK_ADVANCED)/2;
 	timer->CR1 |= TIM_CR1_URS;
 	timer->CR1 |= TIM_CR1_CEN;
 	while(!(timer->SR & TIM_SR_UIF)){} //WAIT TIIL UIF BIT IS SET
 }
 
-//************************************************** APPEND ARRAY **************************************************
 
-void Append_Arrays(int data_1[], int data_1_length, int data_2[], int data_2_length)
-{
-int i,j;
-for(i = 0; i< data_1_length; i++)
-{
-buffer[i] = data_1[i];	
-}
-for(j = 0;  j < data_2_length; j++)
-{
-buffer[data_1_length + j] = data_2[j];	
-}	
-}
-
-//************************************************** Init Buffer **************************************************
-
-void Init_Buffer(void)
-{
-int i = 0;
-for(i = 0; i< 100; i++)
-{
-buffer[i] = -1;
-}
-}
-//
 
 //************************************************** SEND 16 BIT PARALLEL DATA **************************************************
 
 void Send_16Bit_Data(uint16_t data)
 {
-uint8_t lowerBYTE = 0x00FF & data;
-uint8_t higherBYTE = 0xFF00 & data;
+int lowerBYTE = 0x00FF & data;
+int higherBYTE = 0xFF00 & data;
 GPIOA->ODR = lowerBYTE;
 GPIOB->ODR = higherBYTE;
 }
@@ -135,7 +110,8 @@ void Paralle_Transmission_Init(void)
 
 }
 
-//************************************************** Setup for GPIO **************************************************
+
+//************************************************** Setup GPIO MODE **************************************************
 
 
 uint32_t GPIO_Config(uint8_t io_number, uint8_t type)
@@ -148,3 +124,4 @@ uint32_t GPIO_Config(uint8_t io_number, uint8_t type)
 		return (type << (4*(io_number-8)));
 	}
 }
+
