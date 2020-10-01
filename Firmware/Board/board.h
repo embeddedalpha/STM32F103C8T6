13b41d0d50750ffc323e4a75a1575fef6c179626
Axion Engineering <kunalsalvi63@gmail.com>
2020-10-01 21:40:50 +0530

    Update board.h

--- Firmware/Board/board.h
+++ Firmware/Board/board.h
@@ -15,22 +15,19 @@
 #define FLOATING_INPUT         0b0100
 #define PULL_DOWN_INPUT        0b1000
 #define PULL_UP_INPUT          0b1000
- 
-int buffer[100];
+
+
 
 void Delay_Init(TIM_TypeDef *timer);
 void Delay_us(TIM_TypeDef *timer, float usSeconds);
 void Delay_ms(TIM_TypeDef *timer, float msSeconds);
 void Delay_s (TIM_TypeDef *timer, float Seconds);
 
-void Append_Arrays(int data_1[], int data_1_length, int data_2[], int data_2_length);
-void Init_Buffer(void);
 
 void Send_16Bit_Data(uint16_t data);
 void Send_8Bit_Data(GPIO_TypeDef *GPIO ,uint8_t data);
 void Paralle_Transmission_Init(void);
 
 uint32_t GPIO_Config(uint8_t io_number, uint8_t type);
-
 
 #endif
