83f8c8e66f59330999bf4eaa059daf7ae1ff89bb
Blackshield Engineering <kunalsalvi63@gmail.com>
2020-11-05 19:52:11 +0530

    Update USART.h

--- Firmware/UART/USART.h
+++ Firmware/UART/USART.h
@@ -11,10 +11,12 @@
 //                                                         UART
 /***********************************************************************************************************************/
 
-
 /**********************************************************************************************************************
- * @brief: This function sets up basic functions of UART1 (without clock)
+ * @brief: This function sets up basic functions of UART (without clock)
  * @parameters:
+ *              USART        ->       USART1
+ *                                    USART2
+ *                                    USART3
  *              baudrate     ->       2400
  *                                    9600
  *                                    19200
@@ -37,65 +39,8 @@
  *                                1:  DMA mode is disabled for reception
  **********************************************************************************************************************/
 
+void UART_Setup(USART_TypeDef *USART,uint8_t baudrate, uint8_t frame_length, uint8_t stop_bits, uint8_t DMA_TX, uint8_t DMA_RX);
 
-void UART1_Init(uint8_t baudrate, bool frame_length, uint8_t stop_bits, bool DMA_TX, bool DMA_RX);
-
-/**********************************************************************************************************************
- * @brief: This function sets up basic functions of UART2 (without clock)
- * @parameters:
- *              baudrate     ->       2400
- *                                    9600
- *                                    19200
- *                                    57600
- *                                    115200
- *                                    230400
- *                                    460800
- *                                    921600
- *                                    2250000
- *                                    450000
- *              frame_length ->   0:  1 Start bit, 8 Data bits, n Stop bit
- *                                1:  1 Start bit, 9 Data bits, n Stop bit
- *              stop_bits    ->   0:  1 Stop bit
- *                                1:  0.5 Stop bit
- *                                2:  2 Stop bits
- *                                3:  1.5 Stop bit
- *              DMA_TX       ->   0:  DMA mode is enabled for transmission
- *                                1:  DMA mode is disabled for transmission
- *              DMA_RX       ->   0:  DMA mode is enabled for reception
- *                                1:  DMA mode is disabled for reception
- **********************************************************************************************************************/
-
-
-void UART2_Init(uint8_t baudrate, bool frame_length, uint8_t stop_bits, bool DMA_TX, bool DMA_RX);
-
-
-/**********************************************************************************************************************
- * @brief: This function sets up basic functions of UART3 (without clock)
- * @parameters:
- *              baudrate     ->       2400
- *                                    9600
- *                                    19200
- *                                    57600
- *                                    115200
- *                                    230400
- *                                    460800
- *                                    921600
- *                                    2250000
- *                                    450000
- *              frame_length ->   0:  1 Start bit, 8 Data bits, n Stop bit
- *                                1:  1 Start bit, 9 Data bits, n Stop bit
- *              stop_bits    ->   0:  1 Stop bit
- *                                1:  0.5 Stop bit
- *                                2:  2 Stop bits
- *                                3:  1.5 Stop bit
- *              DMA_TX       ->   0:  DMA mode is enabled for transmission
- *                                1:  DMA mode is disabled for transmission
- *              DMA_RX       ->   0:  DMA mode is enabled for reception
- *                                1:  DMA mode is disabled for reception
- **********************************************************************************************************************/
-
-
-void UART3_Init(uint8_t baudrate, bool frame_length, uint8_t stop_bits, bool DMA_TX, bool DMA_RX);
 
 
 
@@ -120,8 +65,6 @@ void UART3_Init(uint8_t baudrate, bool frame_length, uint8_t stop_bits, bool DMA
 *             Error_interrupt                       ->  1: Enable
 *                                                       0: Disable
 ***********************************************************************************************************************/
-
-
 
 void USART_Interrupt_Setup(USART_TypeDef *uart,
 						  bool parity_interrupt,
@@ -175,10 +118,12 @@ int UART_Send_Data(USART_TypeDef *uart,uint8_t data);
 //                                                         USART
 /***********************************************************************************************************************/
 
-
 /**********************************************************************************************************************
  * @brief: This function sets up basic functions of USART1 (with clock)
  * @parameters:
+ *              USART        ->       USART1
+ *                                    USART2
+ *                                    USART3
  *              baudrate     ->       2400
  *                                    9600
  *                                    19200
@@ -206,73 +151,8 @@ int UART_Send_Data(USART_TypeDef *uart,uint8_t data);
  **********************************************************************************************************************/
 
 
-void USART1_Setup(uint8_t baudrate, bool frame_length, uint8_t stop_bits,bool CPOL,bool CPHA, bool DMA_TX, bool DMA_RX);
+void USART_Setup(USART_TypeDef *USART,uint8_t baudrate, bool frame_length, uint8_t stop_bits,bool CPOL,bool CPHA, bool DMA_TX, bool DMA_RX);
 
-
-/**********************************************************************************************************************
- * @brief: This function sets up basic functions of USART2 (with clock)
- * @parameters:
- *              baudrate     ->       2400
- *                                    9600
- *                                    19200
- *                                    57600
- *                                    115200
- *                                    230400
- *                                    460800
- *                                    921600
- *                                    2250000
- *                                    450000
- *              frame_length ->   0:  1 Start bit, 8 Data bits, n Stop bit
- *                                1:  1 Start bit, 9 Data bits, n Stop bit
- *              stop_bits    ->   0:  1 Stop bit
- *                                1:  0.5 Stop bit
- *                                2:  2 Stop bits
- *                                3:  1.5 Stop bit
- *              CPOL              0:  Steady low value on CK pin outside transmission window
- *                                1:  Steady high value on CK pin outside transmission window
- *              CPHA              0:  The first clock transition is the first data capture edge
- *                                1:  The second clock transition is the first data capture edge
- *              DMA_TX       ->   0:  DMA mode is enabled for transmission
- *                                1:  DMA mode is disabled for transmission
- *              DMA_RX       ->   0:  DMA mode is enabled for reception
- *                                1:  DMA mode is disabled for reception
- **********************************************************************************************************************/
-
-
-void USART2_Setup(uint8_t baudrate, bool frame_length, uint8_t stop_bits,bool CPOL,bool CPHA, bool DMA_TX, bool DMA_RX);
-
-
-/**********************************************************************************************************************
- * @brief: This function sets up basic functions of USART3 (with clock)
- * @parameters:
- *              baudrate     ->       2400
- *                                    9600
- *                                    19200
- *                                    57600
- *                                    115200
- *                                    230400
- *                                    460800
- *                                    921600
- *                                    2250000
- *                                    450000
- *              frame_length ->   0:  1 Start bit, 8 Data bits, n Stop bit
- *                                1:  1 Start bit, 9 Data bits, n Stop bit
- *              stop_bits    ->   0:  1 Stop bit
- *                                1:  0.5 Stop bit
- *                                2:  2 Stop bits
- *                                3:  1.5 Stop bit
- *              CPOL              0:  Steady low value on CK pin outside transmission window
- *                                1:  Steady high value on CK pin outside transmission window
- *              CPHA              0:  The first clock transition is the first data capture edge
- *                                1:  The second clock transition is the first data capture edge
- *              DMA_TX       ->   0:  DMA mode is enabled for transmission
- *                                1:  DMA mode is disabled for transmission
- *              DMA_RX       ->   0:  DMA mode is enabled for reception
- *                                1:  DMA mode is disabled for reception
- **********************************************************************************************************************/
-
-
-void USART3_Setup(uint8_t baudrate, bool frame_length, uint8_t stop_bits,bool CPOL,bool CPHA, bool DMA_TX, bool DMA_RX);
 
 
 /***********************************************************************************************************************
