a6b46405f7801d4006775e11a9f7eb33af71438e
Blackshield Engineering <kunalsalvi63@gmail.com>
2020-11-15 19:52:17 +0530

    Update SDIO.h

--- Firmware/SDIO/SDIO.h
+++ Firmware/SDIO/SDIO.h
@@ -3,18 +3,22 @@
 
 
 #include "stm32f10x.h"
-#include "GPIO.h"
+#include "board.h"
 
 
 
-void SDCard_1_Bit_Init(void);
-void SDCard_4_Bit_Init(void);
+void SDIO_1_Bit_Init(uint32_t data_length);
+void SDIO_4_Bit_Init(uint32_t data_length);
 
-void SDCard_Power_ON(void);
-void SDCard_Power_OFF(void);
+void SDIO_Power_ON(void);
+void SDIO_Power_OFF(void);
 
-
-
+void SDIO_Send_Command(uint32_t command);
+uint8_t SDIO_Read_CMD_Response_(void);
+uint32_t SDIO_Read_Response_1(void);
+uint32_t SDIO_Read_Response_2(void);
+uint32_t SDIO_Read_Response_3(void);
+uint32_t SDIO_Read_Response_4(void);
 
 
 
