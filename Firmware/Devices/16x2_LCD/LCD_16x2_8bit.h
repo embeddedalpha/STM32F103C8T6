bdcddc55d2ab01b119af8792fbe749815bcb4cc7
Blackshield Engineering <kunalsalvi63@gmail.com>
2020-12-26 13:27:10 +0530

    Update LCD_16x2_8bit.h

--- Firmware/Devices/16x2_LCD/LCD_16x2_8bit.h
+++ Firmware/Devices/16x2_LCD/LCD_16x2_8bit.h
@@ -1,1 +1,27 @@
+#ifndef       _LCD_16_2_
+#define       _LCD_16_2_
 
+
+
+
+void LCD_Init(void);
+void delay();
+void lcd_cmd(unsigned char c);
+ void lcd_data(unsigned char c);
+void LCD_SEND_DATA(char *c);
+void LCD_new_line();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
