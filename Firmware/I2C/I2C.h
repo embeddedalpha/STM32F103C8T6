e49a51b3d255bf77c3a6b2c2f9a3931bbc26dd71
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-01-01 20:19:36 +0530

    Update I2C.h

--- Firmware/I2C/I2C.h
+++ Firmware/I2C/I2C.h
@@ -1,18 +1,16 @@
 /**********************************************************************************************************************************
 @file:    I2C.H
 @author:  Kunal Salvi
-@version: V3.05.0
-@date:    27/3/2020
+@version: V5.0.0
+@date:    1/1/2021
 @brief:   I2C Header File.
           This file contains the prototype of functions in I2C.c file.
           By calling various functions, the user can configure
           the I2C Hardware Peripheral in any of the permitted modes.
-
 @attention: This file aims at providing the user an abstraction layer to the I2C peripheral
             and is tested on STM32f103C8T6. This file is free for use to any person or corporation.
             In case of failure of code/product due to third party tampering, the author will not be
             responsible. This code is published under MIT License.
-
 **********************************************************************************************************************************/
 
 #ifndef _I2C_H_
@@ -21,81 +19,78 @@
 
 #include "stm32f10x.h"
 #include "board.h"
-#define i2c_device_write 0
-#define i2c_device_read  1
 
-I2C_TypeDef *I2C;
+
+#define I2C_SM_Mode	1
+#define I2C_FS_Mode	2
 
 /***************************************** Initialization Mode *****************************************************************
 //@brief: The function sets up the I2C controller in Master mode.
-//@param: none
+//@param: I2C_TypeDef -> 	I2C1
+ * 		          I2C2
+ *        mode ->		I2C_SM_Mode
+ * 		  	I2C_FM_Mode
 //@return: none
 ********************************************************************************************************************************/
 
-void I2C_Master_Init(void);
+void I2C_Master_Init(I2C_TypeDef *I2C,uint8_t mode);
 
 
-/***************************************** Generate Start Condition *****************************************************************
-//@brief: The function generates a start condition.
-//@param: none
+
+/***************************************** Master Start *****************************************************************
+//@brief: The function sets up the I2C controller in Master mode.
+//@param: I2C_TypeDef -> 	I2C1
+// 			I2C2
 //@return: none
 ********************************************************************************************************************************/
 
-void I2C_Master_Start(void);
+void I2C_Master_Start(I2C_TypeDef *I2C);
 
 
 
-/***************************************** Read Address *****************************************************************
-//@brief: The function sends device address as read command.
-//@param:
-//        data          device address
+/***************************************** Master Send Address *****************************************************************
+//@brief: The function sets up the I2C controller in Master mode.
+//@param: I2C_TypeDef -> 	I2C1
+// 			I2C2
+//        data  ->     ~
 //@return: none
 ********************************************************************************************************************************/
 
-void I2C_Master_Address_Read(uint8_t data);
+void I2C_Master_Address_Send(I2C_TypeDef *I2C, uint8_t data);
 
 
 
-/***************************************** Write Address *****************************************************************
-//@brief: The function sends device address as write command.
-//@param:
-//        data          device address
+/***************************************** Master Send Data *****************************************************************
+//@brief: The function sets up the I2C controller in Master mode.
+//@param: I2C_TypeDef -> 	I2C1
+// 			I2C2
+//        data	->     ~
 //@return: none
 ********************************************************************************************************************************/
 
-void I2C_Master_Address_Write(uint8_t data);
+void I2C_Master_Send_Data(I2C_TypeDef *I2C, uint8_t data);
 
 
 
+/***************************************** Master Receive Data *****************************************************************
+//@brief: The function sets up the I2C controller in Master mode.
+//@param: I2C_TypeDef -> 	I2C1
+// 			I2C2
+//@return: RX_Data
+********************************************************************************************************************************/
 
-/***************************************** Send Data *****************************************************************
-//@brief: The function sends data onto the I2C bus.
-//@param:
-//        data          data to be sent
+uint8_t I2C_Master_Rx_Data(I2C_TypeDef *I2C);
+
+
+
+/***************************************** Master Stop *****************************************************************
+//@brief: The function sets up the I2C controller in Master mode.
+//@param: I2C_TypeDef -> 	I2C1
+// 			I2C2
 //@return: none
 ********************************************************************************************************************************/
 
-void I2C_Master_Send_Data( uint8_t data);
-
-
-/***************************************** Read Data *****************************************************************
-//@brief: The function receives data from the I2C bus.
-//@param: none
-//@return: data from the device
-********************************************************************************************************************************/
-
-int I2C_Master_Rx_Data(void);
-
-
-
-/***************************************** Stops Transfer  *****************************************************************
-//@brief: The function terminates all transfers.
-//@param: none
-//@return: none
-********************************************************************************************************************************/
-
-void I2C_Master_Stop(void);
-
+void I2C_Master_Stop(I2C_TypeDef *I2C);
 
 
 
