8f6019450906a42a64ede5346ebeddaa66312bd6
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-02-27 14:08:48 +0530

    Update CRC.c

--- Firmware/CRC/CRC.c
+++ Firmware/CRC/CRC.c
@@ -1,1 +1,25 @@
+/*
+ * CRC.c
+ *
+ *  Created on: Feb 27, 2021
+ *      Author: Kunal
+ */
+
+
+#include "CRC.h"
+
+void reset_CRC(void)
+{
+	CRC -> CR |= CRC_CR_RESET;	//RESET CRC
+}
+
+uint32_t get_CRC(uint32_t in_CRC)
+{
+	uint32_t out_CRC;
+	CRC ->DR = in_CRC;	//STORE INCOMING DATA IN CRC DATA REGISTER
+	NOP_Delay_CPU_Ticks(4);
+	asm("NOP");
+	out_CRC = CRC -> DR;
+    return out_CRC;
+}
 
