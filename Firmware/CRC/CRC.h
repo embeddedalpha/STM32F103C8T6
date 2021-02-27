f83f53bdbefe7e0a3560d52e7aa8f6b5455d2212
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-02-27 14:08:13 +0530

    Update CRC.h

--- Firmware/CRC/CRC.h
+++ Firmware/CRC/CRC.h
@@ -1,1 +1,17 @@
+/*
+ * CRC.h
+ *
+ *  Created on: Feb 27, 2021
+ *      Author: Kunal
+ */
+
+#ifndef CRC_CRC_H_
+#define CRC_CRC_H_
+
+#include "stm32f103xb.h"
+
+void reset_CRC(void);
+uint32_t get_CRC(uint32_t in_CRC);
+
+#endif /* CRC_CRC_H_ */
 
