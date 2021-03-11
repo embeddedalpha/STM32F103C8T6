f35012c9d44791d87cab8ff155820c49b9715bf7
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-03-11 13:11:52 +0530

    Update SPI.h

--- Firmware/SPI/v2/SPI.h
+++ Firmware/SPI/v2/SPI.h
@@ -35,4 +35,6 @@ void SPI_Master_Init(SPI_Master_Config SPI);
 
 void SPI_Master_TX(SPI_Master_Config SPI, uint16_t data);
 
+uint16_t SPI_Master_RX(SPI_Master_Config SPI);
+
 #endif /* SPI_SPI_H_ */
