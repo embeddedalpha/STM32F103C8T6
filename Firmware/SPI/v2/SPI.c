fee8448f959e81f3ab0fa13b057bac759268ad41
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-03-11 12:49:03 +0530

    Update SPI.c

--- Firmware/SPI/v2/SPI.c
+++ Firmware/SPI/v2/SPI.c
@@ -1,1 +1,43 @@
 
+/*
+ * SPI.c
+ *
+ *  Created on: Feb 27, 2021
+ *      Author: Kunal
+ */
+
+
+#include "SPI.h"
+
+
+void SPI_Master_Init(SPI_Master_Config SPI)
+{
+	SPI.SPI->CR1 |= (
+					SPI.crc << 13 |
+					SPI.frame_format << 11 |
+					SPI.lsb_or_msb << 7 |
+					SPI.clock_speed << 3 |
+					SPI.clock_polarity << 1 |
+					SPI.clock_phase
+					);
+	SPI.SPI->CR2 |= (
+					SPI.txDMA << 1 |
+					SPI.rxDMA
+					);
+	if(SPI.interrupts)
+	{
+		SPI.SPI -> CR2 |= (SPI.err_int << 5 |
+						   SPI.tx_int << 7 |
+						   SPI.rxDMA << 6) ;
+	}
+
+	SPI.SPI -> CR1 |= SPI_CR1_MSTR | SPI_CR1_SPE;
+}
+
+
+
+void SPI_Master_TX(SPI_Master_Config SPI, uint16_t data)
+{
+	SPI.SPI->DR = data;
+	while(!(SPI.SPI -> SR & SPI_SR_TXE));
+}
