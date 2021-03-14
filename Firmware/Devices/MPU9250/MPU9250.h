62a34195e359294bb90a875a4d01ce4ab6b4eb5c
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-03-14 23:36:49 +0530

    Update MPU9250.h

--- Firmware/Devices/MPU9250/MPU9250.h
+++ Firmware/Devices/MPU9250/MPU9250.h
@@ -1,1 +1,20 @@
+/*
+ * MPU9250.h
+ *
+ *  Created on: 06-Mar-2021
+ *      Author: Kunal
+ */
+
+#ifndef MPU9250_MPU9250_H_
+#define MPU9250_MPU9250_H_
+
+#include "main.h"
+#include "SPI/SPI.h"
+
+
+
+void MPU9250_Init(SPI_Config MPU9250);
+
+
+#endif /* MPU9250_MPU9250_H_ */
 
