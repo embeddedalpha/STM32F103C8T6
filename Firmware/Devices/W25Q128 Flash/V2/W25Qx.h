767a8478d59ba7fc490ac8f94bc73b4e9d2a68ab
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-04-25 19:34:15 +0530

    Update and rename W25QxxFLASH.h to W25Qx.h

---
+++ Firmware/Devices/W25Q128 Flash/V2/W25Qx.h
@@ -0,0 +1,38 @@
+/*
+ * W25Qx.h
+ *
+ *  Created on: 25-Apr-2021
+ *      Author: Kunal
+ */
+
+#ifndef W25QX_W25QX_H_
+#define W25QX_W25QX_H_
+
+#include "main.h"
+#include "GPIO/GPIO.h"
+#include "SPI/SPI.h"
+
+struct SPI_Config W25Q;
+
+#define Status_Reg_Protection_0          7
+#define Sector_Protect                   6
+#define Top_Bottom_Write_Protect         5
+#define Block_Protect_Bit2               4
+#define Block_Protect_Bit1               3
+#define Block_Protect_Bit0               2
+#define Write_Enable_Latch               1
+#define Busy_Bit                         0
+
+void W25Qx_Init(SPI_TypeDef *SPI);
+void W25Qx_Read_Block(uint32_t address, uint8_t data[], uint32_t length);
+uint8_t W25Qx_Read_Byte(uint32_t address);
+void W25Qx_Write_Block(uint32_t address, uint8_t *data, uint32_t length);
+void W25Qx_Write_Byte(uint32_t address, uint8_t byte);
+void W25Qx_Write_Enable(void);
+void W25Qx_Write_Disable(void);
+void W25Qx_Select(void);
+void W25Qx_Deselect(void);
+uint8_t W25Qx_Read_Status_Register_1(void);
+uint8_t W25Qx_Read_Status_Register_2(void);
+
+#endif /* W25QX_W25QX_H_ */
