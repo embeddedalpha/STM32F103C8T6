aaeb44c840af09e6add19497a2e5380e130d5ddf
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-04-26 19:31:22 +0530

    Update MPU6050_I2C.c

--- Firmware/Devices/MPU6050/MPU6050_I2C.c
+++ Firmware/Devices/MPU6050/MPU6050_I2C.c
@@ -25,24 +25,24 @@ void MPU6050_Init(I2C_TypeDef *I2C)
 	I2C_Master_Send_Data(I2C, 0x00);
 	I2C_Master_Stop(I2C);
 
-
 }
 
-short * MPU6050_Get_Data(I2C_TypeDef *I2C)
+short *MPU6050_Get_Raw_Data(I2C_TypeDef *I2C)
 {
 	short data[7];
 	I2C_Master_Send_Address(I2C, 0x68, read);
 	I2C_Master_Send_Data(I2C, 0x3B);
 	//Accelerometer Data
-	data[0] = ((I2C_Master_Receive_Data(I2C) << 8) | I2C_Master_Receive_Data(I2C)/16384.0);
-	data[1] = ((I2C_Master_Receive_Data(I2C) << 8) | I2C_Master_Receive_Data(I2C)/16384.0);
-	data[2] = ((I2C_Master_Receive_Data(I2C) << 8) | I2C_Master_Receive_Data(I2C)/16384.0);
+	data[0] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/16384.0);
+	data[1] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/16384.0);
+	data[2] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/16384.0);
 	//Temperature Data
-	data[3] = ((I2C_Master_Receive_Data(I2C) << 8) | I2C_Master_Receive_Data(I2C)/340.0) + 36.53;
+	data[3] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/340.0) + 36.53;
 	//Gyroscope Data
-	data[4] = ((I2C_Master_Receive_Data(I2C) << 8) | I2C_Master_Receive_Data(I2C)/16.4);
-	data[5] = ((I2C_Master_Receive_Data(I2C) << 8) | I2C_Master_Receive_Data(I2C)/16.4);
-	data[6] = ((I2C_Master_Receive_Data(I2C) << 8) | I2C_Master_Receive_Data(I2C)/16.4);
+	data[4] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/16.4);
+	data[5] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/16.4);
+	data[6] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/16.4);
 
-
+	return data;
 }
+
