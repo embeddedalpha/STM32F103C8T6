9e36ab35ab34a6ab74b860412e3f2aff59714642
Kunal Salvi <kunalsalvi63@gmail.com>
2021-04-28 00:00:01 +0530

    Update MPU9250_I2C.h

--- Firmware/Devices/MPU9250/MPU9250_I2C.h
+++ Firmware/Devices/MPU9250/MPU9250_I2C.h
@@ -2,7 +2,7 @@
  * MPU9250.h
  *
  *  Created on: 27-04-2021
- *      Author: Kunal Salvi
+ *      Author: Kunal
  */
 
 #ifndef MPU9250_MPU9250_H_
@@ -10,6 +10,7 @@
 
 #include "main.h"
 #include "I2C/I2C.h"
+#include "MPU9250_REG.h"
 
 #define MPU9250_Address	0x68
 //#define MPU9250_Address	0x69    Un-comment to use any of one to use
@@ -19,5 +20,9 @@ void MPU9250_Write_byte(I2C_TypeDef *I2C, uint8_t reg_address, uint8_t data);
 void MPU9250_Write_Multiple_Bytes(I2C_TypeDef *I2C, uint8_t reg_address, uint8_t *data, uint16_t length);
 uint8_t MPU9250_Read_Byte(I2C_TypeDef *I2C, uint8_t reg_address);
 void MPU9250_Read_Multiple_Bytes(I2C_TypeDef *I2C, uint8_t reg_address,uint8_t *data, uint16_t length);
+void MPU9250_Read_Accl_Data(I2C_TypeDef *I2C, int16_t *array);
+void MPU9250_Read_Gyro_Data(I2C_TypeDef *I2C, int16_t *array);
+
+
 
 #endif /* MPU9250_MPU9250_H_ */
