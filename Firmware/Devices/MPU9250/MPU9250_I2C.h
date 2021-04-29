346a23176dbfefe42001f36a077600fdbcb332a6
Kunal Salvi <kunalsalvi63@gmail.com>
2021-04-29 20:35:05 +0530

    Update MPU9250_I2C.h

--- Firmware/Devices/MPU9250/MPU9250_I2C.h
+++ Firmware/Devices/MPU9250/MPU9250_I2C.h
@@ -1,7 +1,7 @@
 /*
  * MPU9250.h
  *
- *  Created on: 27-04-2021
+ *  Created on: 29-04-2021
  *      Author: Kunal
  */
 
@@ -16,12 +16,20 @@
 //#define MPU9250_Address	0x69    Un-comment to use any of one to use
 
 void MPU9250_Init(I2C_TypeDef *I2C);
-void MPU9250_Write_byte(I2C_TypeDef *I2C, uint8_t reg_address, uint8_t data);
-void MPU9250_Write_Multiple_Bytes(I2C_TypeDef *I2C, uint8_t reg_address, uint8_t *data, uint16_t length);
-uint8_t MPU9250_Read_Byte(I2C_TypeDef *I2C, uint8_t reg_address);
-void MPU9250_Read_Multiple_Bytes(I2C_TypeDef *I2C, uint8_t reg_address,uint8_t *data, uint16_t length);
+
+void MPU9250_Write_byte(I2C_TypeDef *I2C,uint8_t device_address, uint8_t reg_address, uint8_t data);
+
+void MPU9250_Write_Multiple_Bytes(I2C_TypeDef *I2C,uint8_t device_address, uint8_t reg_address, uint8_t *data, uint16_t length);
+
+uint8_t MPU9250_Read_Byte(I2C_TypeDef *I2C,uint8_t device_address, uint8_t reg_address);
+
+void MPU9250_Read_Multiple_Bytes(I2C_TypeDef *I2C,uint8_t device_address, uint8_t reg_address,uint8_t *data, uint16_t length);
+
 void MPU9250_Read_Accl_Data(I2C_TypeDef *I2C, int16_t *array);
+
 void MPU9250_Read_Gyro_Data(I2C_TypeDef *I2C, int16_t *array);
+
+void MPU9250_Read_Magn_Data(I2C_TypeDef *I2C, int16_t *array);
 
 
 
