e631680bfffcfcddc6e5e45cd70d17118a7043f3
Kunal Salvi <kunalsalvi63@gmail.com>
2021-04-30 23:42:41 +0530

    Update Debug.h

--- Firmware/Debug/Debug.h
+++ Firmware/Debug/Debug.h
@@ -7,15 +7,55 @@
 #include "string.h"
 #include "USART.h"
 #include "stdio.h"
+#include <stdint.h>
 
 
 #define DEBUG_UART  USART1
-#define delay				for(int i = 0; i<= 5000000; i++)
+
+
 
 void InitConsole(void);
 static void printConsole(char *msg, ...);
 int Console_Dump(const char *s);
 char * Console_Return(char *s);
+
+__STATIC_INLINE uint32_t Delay_Config(void)
+{
+
+	SysTick->CTRL = 0;
+	SysTick->LOAD = 0x00FFFFFF;
+	SysTick->VAL = 0;
+	SysTick->CTRL |= 5;
+	return (0UL);                                                     /* Function successful */
+}
+
+__STATIC_INLINE uint32_t Delay_us(unsigned long us)
+{
+
+	SysTick->LOAD = (uint32_t)(us * (1/1000000) * SystemCoreClock);
+	SysTick->VAL = 0;
+	while((SysTick->CTRL & 0x00010000) == 0);
+	return (0UL);                                                     /* Function successful */
+}
+
+__STATIC_INLINE uint32_t Delay_ms(unsigned long ms)
+{
+	SysTick->LOAD = (uint32_t)(ms * (1/1000) * SystemCoreClock);
+	SysTick->VAL = 0;
+	while((SysTick->CTRL & 0x00010000) == 0);
+	return (0UL);                                                     /* Function successful */
+}
+
+
+__STATIC_INLINE uint32_t Delay_s(unsigned long s)
+{
+	s = s * 1000;
+	for (; s>0; s--)
+	{
+		Delay_ms(1);
+	}
+	return (0UL);
+}
 
 
 #endif
