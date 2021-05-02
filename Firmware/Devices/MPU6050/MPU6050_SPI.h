edefd83b814704729bc966fe1ab86ed30cb8e8e4
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-02 20:10:11 +0530

    Update MPU6050_SPI.h

--- Firmware/Devices/MPU6050/MPU6050_SPI.h
+++ Firmware/Devices/MPU6050/MPU6050_SPI.h
@@ -1,1 +1,23 @@
+/*
+ * MPU6050_SPI.h
+ *
+ *  Created on: 01-May-2021
+ *      Author: Kunal
+ */
+
+#ifndef MPU6050_MPU6050_SPI_H_
+#define MPU6050_MPU6050_SPI_H_
+
+#include "main.h"
+#include "GPIO/GPIO.h"
+#include "SPI/SPI.h"
+
+struct SPI_Config MPU6050;
+
+
+void MPU6050_Init(SPI_Config *SPI);
+
+short * MPU6050_Get_Raw_Data(void);
+
+#endif /* MPU6050_MPU6050_SPI_H_ */
 
