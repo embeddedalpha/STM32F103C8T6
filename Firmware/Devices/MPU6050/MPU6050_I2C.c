d1bec66d0abbd25352b0de081d3aff3e85a35a5e
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-03 21:58:48 +0530

    Update MPU6050_I2C.c

--- Firmware/Devices/MPU6050/MPU6050_I2C.c
+++ Firmware/Devices/MPU6050/MPU6050_I2C.c
@@ -1,7 +1,7 @@
 /*
  * MPU6050.c
  *
- *  Created on: 26-Apr-2021
+ *  Created on: 25-Apr-2021
  *      Author: Kunal
  */
 
@@ -12,24 +12,36 @@ void MPU6050_Init(I2C_TypeDef *I2C)
 {
 	I2C_Init(I2C, Fast_Mode);
 	I2C_Master_Start(I2C);
+
 	I2C_Master_Send_Address(I2C, 0x68, write);
-	I2C_Master_Send_Data(I2C, 0x19);
+
+	I2C_Master_Send_Data(I2C, MPU6050_RA_PWR_MGMT_1);
+	I2C_Master_Send_Data(I2C, 0x00);
+
+	I2C_Master_Send_Data(I2C, MPU6050_RA_SMPLRT_DIV);
 	I2C_Master_Send_Data(I2C, 0x07);
-	I2C_Master_Send_Data(I2C, 0x6B);
+
+	I2C_Master_Send_Data(I2C, MPU6050_RA_CONFIG);
 	I2C_Master_Send_Data(I2C, 0x00);
-	I2C_Master_Send_Data(I2C, 0x1A);
-	I2C_Master_Send_Data(I2C, 0x00);
-	I2C_Master_Send_Data(I2C, 0x1B);
+
+	I2C_Master_Send_Data(I2C, MPU6050_RA_GYRO_CONFIG);
 	I2C_Master_Send_Data(I2C, 0x18);
-	I2C_Master_Send_Data(I2C, 0x6B);
+	\
+	I2C_Master_Send_Data(I2C, MPU6050_RA_PWR_MGMT_1);
 	I2C_Master_Send_Data(I2C, 0x00);
+
+
+
+
+
 	I2C_Master_Stop(I2C);
+
+
 
 }
 
-short *MPU6050_Get_Raw_Data(I2C_TypeDef *I2C)
+void MPU6050_Get_Raw_Data(I2C_TypeDef *I2C, short *data)
 {
-	short data[7];
 	I2C_Master_Send_Address(I2C, 0x68, read);
 	I2C_Master_Send_Data(I2C, 0x3B);
 	//Accelerometer Data
@@ -43,6 +55,6 @@ short *MPU6050_Get_Raw_Data(I2C_TypeDef *I2C)
 	data[5] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/16.4);
 	data[6] = (short)((I2C_Master_Receive_Data(I2C) < 8) || I2C_Master_Receive_Data(I2C)/16.4);
 
-	return data;
+
 }
 
