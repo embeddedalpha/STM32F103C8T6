6f0bbc7ff98b3f01e909d3365af56218d9c596a1
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-15 16:05:41 +0530

    Update IDP_WatchDog.h

--- Firmware/Independent WatchDog/IDP_WatchDog.h
+++ Firmware/Independent WatchDog/IDP_WatchDog.h
@@ -1,1 +1,21 @@
+/*
+ * IDP_WatchDog.h
+ *
+ *  Created on: 15-May-2021
+ *      Author: Kunal
+ */
+
+#ifndef WATCHDOG_IDP_WATCHDOG_H_
+#define WATCHDOG_IDP_WATCHDOG_H_
+
+#include "main.h"
+
+void IDP_WatchDog_Init(void);
+void IDP_WatchDog_Start(void);
+void IDP_WatchDog_Reload(void);
+void IDP_WatchDog_Stop(void);
+
+
+
+#endif /* WATCHDOG_IDP_WATCHDOG_H_ */
 
