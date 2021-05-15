72496b35c15fbcf043620126b761007c0415647d
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-15 16:05:22 +0530

    Update IDP_WatchDog.c

--- Firmware/Independent WatchDog/IDP_WatchDog.c
+++ Firmware/Independent WatchDog/IDP_WatchDog.c
@@ -1,1 +1,33 @@
+/*
+ * IDP_WatchDog.c
+ *
+ *  Created on: 15-May-2021
+ *      Author: Kunal
+ */
+
+#include "IDP_WatchDog.h"
+
+
+void IDP_WatchDog_Init(void)
+{
+	//IWDG RUNS ON 40KHz LSI OCS, TIME PERIOD 25uS
+	RCC ->CR |= RCC_CSR_IWDGRSTF;
+}
+
+void IDP_WatchDog_Start(void)
+{
+	IWDG ->KR = 0xCCCC;
+
+}
+
+void IDP_WatchDog_Reload(void)
+{
+	IWDG ->KR = 0xAAAA; //Without reload the next reset will happen after 1.09225 s
+}
+
+void IDP_WatchDog_Stop(void)
+{
+
+}
+
 
