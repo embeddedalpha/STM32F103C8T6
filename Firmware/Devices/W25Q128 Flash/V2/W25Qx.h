fa47f1a949d6625dc939aeb6c0fdd513ab3ac7b6
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-24 20:38:08 +0530

    Update W25Qx.h

--- Firmware/Devices/W25Q128 Flash/V2/W25Qx.h
+++ Firmware/Devices/W25Q128 Flash/V2/W25Qx.h
@@ -1,7 +1,7 @@
 /*
  * W25Qx.h
  *
- *  Created on: 25-Apr-2021
+ *  Created on: 24-May-2021
  *      Author: Kunal
  */
 
@@ -24,19 +24,18 @@ struct SPI_Config W25Q;
 #define Busy_Bit                         0
 
 void W25Qx_Init(SPI_TypeDef *SPI);
-void W25Qx_Read_Block(uint32_t address, uint8_t data[], uint32_t length);
+uint8_t *W25Qx_Read_Block(uint32_t address, uint32_t length);
+
 uint8_t W25Qx_Read_Byte(uint32_t address);
 void W25Qx_Write_Block(uint32_t address, uint8_t *data, uint32_t length);
 void W25Qx_Write_Byte(uint32_t address, uint8_t byte);
 void W25Qx_Write_Enable(void);
 void W25Qx_Write_Disable(void);
-void W25Qx_Select(void);
-void W25Qx_Deselect(void);
 uint8_t W25Qx_Read_Status_Register_1(void);
 uint8_t W25Qx_Read_Status_Register_2(void);
-void W25Qx_Block_Erase_64KB(void);
-void W25Qx_Block_Erase_32KB(void);
-void W25Qx_Block_Erase_4KB(void);
+void W25Qx_Block_Erase_64KB(uint32_t address);
+void W25Qx_Block_Erase_32KB(uint32_t address);
+void W25Qx_Block_Erase_4KB(uint32_t address);
 void W25Qx_Chip_Erase(void);
 void W25Qx_Power_Down(void);
 void W25Qx_Memory_Protect(void);
