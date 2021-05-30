e8b997894ba17ec37debb742fd65b8093dc2759b
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-30 23:00:17 +0530

    Update I2C.c

--- Firmware/I2C/I2C.c
+++ Firmware/I2C/I2C.c
@@ -1,96 +1,41 @@
 /*
- * I2C.c
+ * I2C.h
  *
  *  Created on: 23-Apr-2021
+ *  Updated on: 30-May-2021
  *      Author: Kunal
  */
 
+#ifndef I2C_I2C_H_
+#define I2C_I2C_H_
 
-#include "I2C.h"
+#include "main.h"
+#include "GPIO/GPIO.h"
 
+#define Fast_Mode	1
+#define Normal_Mode	2
+#define read 0
+#define write 1
 
-
-void I2C_Init(I2C_TypeDef *I2C, uint8_t mode)
+typedef struct I2C_Config
 {
-	if(I2C == I2C1)
-	{
-		RCC -> APB1ENR |= RCC_APB1ENR_I2C1EN;
-		GPIO_Pin_Setup(GPIOB, 6, ALT_OPEN_DRAIN_OUTPUT);
-		GPIO_Pin_Setup(GPIOB, 7, ALT_OPEN_DRAIN_OUTPUT);
-	}
-	else
-	{
-		RCC -> APB1ENR |= RCC_APB1ENR_I2C2EN;
-		GPIO_Pin_Setup(GPIOB, 10, ALT_OPEN_DRAIN_OUTPUT);
-		GPIO_Pin_Setup(GPIOB, 11, ALT_OPEN_DRAIN_OUTPUT);
-	}
-	if(mode)
-	{
-		I2C -> CR2 |= (uint16_t)30;
-		I2C -> CCR |= I2C_CCR_FS | I2C_CCR_DUTY ;
-		I2C -> CCR |= 3;
-		I2C -> TRISE = 10;
-		I2C -> CR1 |= I2C_CR1_PE;
-	}
-	else
-	{
-		I2C -> CR2 |= (uint16_t)30;
-		I2C -> CCR = 42;
-		I2C -> TRISE = 30;
-		I2C -> CR1 |= I2C_CR1_PE;
-	}
-}
+	I2C_TypeDef *I2C;
+	bool mode;
 
+}I2C_Config;
 
-void I2C_Master_Start(I2C_TypeDef *I2C)
-{
-	I2C->CR1 |= I2C_CR1_START;
-	while(!(I2C->SR1 & I2C_SR1_SB)){}
-}
+void I2C_Master_Init(I2C_Config I2C);
 
-void I2C_Master_Send_Address(I2C_TypeDef *I2C, char address,char RW)
-{
-	int temp;
-	I2C->DR = address | RW;
-	while(!(I2C->SR1 & I2C_SR1_ADDR));
-	while(I2C -> SR1 & I2C_SR1_ADDR)
-	{
-		temp = I2C -> SR1;
-		temp = I2C -> SR2;
-		if(!(I2C->SR1 & I2C_SR1_ADDR))
-		{
-			break;
-		}
-	}
-}
+void I2C_Master_Start(I2C_Config I2C);
 
-void I2C_Master_Send_Data(I2C_TypeDef *I2C, char data)
-{
+void I2C_Master_Send_Address(I2C_Config I2C, char address,char RW);
 
-	while(!(I2C->SR1 & I2C_SR1_TXE));
-	I2C -> DR = data;
-	while(!(I2C->SR1 & I2C_SR1_TXE));
-}
+void I2C_Master_Send_Data(I2C_Config I2C, char data);
 
-char I2C_Master_Receive_Data(I2C_TypeDef *I2C)
-{
-	char temp = 0;
-	while(!(I2C->SR1 & I2C_SR1_RXNE));
-	temp = I2C -> DR;
-	I2C -> CR1 |= I2C_CR1_ACK;
-	return temp;
-}
+char I2C_Master_Receive_Data(I2C_Config I2C);
 
-void I2C_Master_Stop(I2C_TypeDef *I2C)
-{
-	while(!(I2C->SR1 & I2C_SR1_TXE));
-	while(!(I2C->SR1 & I2C_SR1_BTF));
-	I2C -> CR1 |= I2C_CR1_STOP;
+void I2C_Master_Stop(I2C_Config I2C);
 
-}
+void I2C_Master_Send_NACK(I2C_Config I2C);
 
-void I2C_Master_Send_NACK(I2C_TypeDef *I2C)
-{
-	I2C -> CR1 &= ~I2C_CR1_ACK;
-}
-
+#endif /* I2C_I2C_H_ */
