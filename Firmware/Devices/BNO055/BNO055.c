7bdcb01065ecba50784c513e0a846de73634f1f1
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-30 23:54:01 +0530

    Update BNO055.c

--- Firmware/Devices/BNO055/BNO055.c
+++ Firmware/Devices/BNO055/BNO055.c
@@ -1,218 +1,95 @@
+/*
+ * BNO055.c
+ *
+ *  Created on: 30-May-2021
+ *      Author: Kunal
+ */
+
+
 #include "BNO055.h"
 
 
-
-
-void BNO055_Init()
+void BNO055_Init(I2C_TypeDef *I2C)
 {
-I2C_Master_Init();
+	Delay_Config();
+	BNO055.I2C = I2C;
+	BNO055.mode = Fast_Mode;
+	I2C_Master_Init(BNO055);
+	BNO055_Write_Single(OPR_MODE, 0x0C);
+	Delay_ms(20);
+
 }
 
-
-void BNO055_Read(uint8_t reg, uint8_t bytes)
+void BNO055_Read_Buffer(uint8_t reg, uint8_t *data, int len)
 {
-	I2C_Master_Start();
-	I2C_Master_Address_Write(BNO055_Address);
-	I2C_Master_Start();
-	for(int i = 0; i < bytes; i++)
+	int i=0;
+	I2C_Master_Start(BNO055);
+	I2C_Master_Send_Address(BNO055, 0x29, write);
+	I2C_Master_Send_Data(BNO055, reg);
+	I2C_Master_Start(BNO055);
+	I2C_Master_Send_Address(BNO055, 0x29, read);
+	for(i = 0;i < len; i++)
 	{
-		BNO055_Buffer[i] = I2C_Master_Rx_Data();
-		I2C_Gen_Ack();
+		data[i] = I2C_Master_Receive_Data(BNO055);
 	}
-	I2C_Master_Stop();
+	I2C_Master_Send_NACK(BNO055);
+	I2C_Master_Stop(BNO055);
+
 }
 
-
-
-void BNO055_Power_Mode(uint8_t mode){}
-
-
-//////////////////////////////////////////////////////////////////////////////////////
-//                                          QUATERNION ANGLE
-//////////////////////////////////////////////////////////////////////////////////////
-
-void BNO055_GET_Quaternion_DATA(void)
+void BNO055_Write_Buffer(unit8_t reg, uint8_t *data, int len)
 {
-	uint16_t QUA_X, QUA_Y, QUA_Z, QUA_W;
-	for(int i = 0 ; i< 10; i++)
-	{
-		BNO055_Buffer[i] = 0;
-	}
-
-	BNO055_Read(QUA_DATA_W_LSB,8);
-
-	QUA_W = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
-	QUA_X = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
-	QUA_Y = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
-	QUA_Z = (BNO055_Buffer[7] << 8) | BNO055_Buffer[6];
-
-	BNO055_QUATERNION_Buffer[0] = (float)(QUA_W / 16384.0f);
-	BNO055_QUATERNION_Buffer[1] = (float)(QUA_X / 16384.0f);
-	BNO055_QUATERNION_Buffer[2] = (float)(QUA_Y / 16384.0f);
-	BNO055_QUATERNION_Buffer[3] = (float)(QUA_Z / 16384.0f);
+	int i;
+	I2C_Master_Start(BNO055);
+	I2C_Master_Send_Address(BNO055, 0x29, write);
+	for(i = 0; i < len; i++)
+	I2C_Master_Send_Data(BNO055, data[i]);
+	I2C_Master_Stop(BNO055);
 }
 
-
-//////////////////////////////////////////////////////////////////////////////////////
-//                                          EULER ANGLE
-//////////////////////////////////////////////////////////////////////////////////////
-
-void BNO055_GET_Euler_DATA(void)
+void BNO055_Write_Single(unit8_t reg, unit8_t data)
 {
-	uint16_t RAW, PITCH, ROLL;
-	for(int i = 0 ; i< 10; i++)
-	{
-		BNO055_Buffer[i] = 0;
-	}
-
-	BNO055_Read(EUL_Heading_LSB,6);
-
-	RAW = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
-	PITCH = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
-	ROLL = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
-
-
-	BNO055_EULER_ANGLE_Buffer[0] = (float)(RAW / 16.0f);
-	BNO055_EULER_ANGLE_Buffer[1] = (float)(PITCH / 16.0f);
-	BNO055_EULER_ANGLE_Buffer[2] = (float)(ROLL / 16.0f);
-
+	I2C_Master_Start(BNO055);
+	I2C_Master_Send_Address(BNO055, 0x29, write);
+	I2C_Master_Send_Data(BNO055, data);
+	I2C_Master_Stop(BNO055);
 }
 
-//////////////////////////////////////////////////////////////////////////////////////
-//                                          GRAVITY VECTOR
-//////////////////////////////////////////////////////////////////////////////////////
-
-void BNO055_GET_Gravity_Vector_DATA(void)
+unit8_t BNO055_Read_Single(unit8_t reg)
 {
-	uint16_t GRV_X, GRV_Y, GRV_Z;
-	for(int i = 0 ; i< 10; i++)
-	{
-		BNO055_Buffer[i] = 0;
-	}
+	uint8_t temp;
+	I2C_Master_Start(BNO055);
+	I2C_Master_Send_Address(BNO055, 0x29, write);
+	I2C_Master_Send_Data(BNO055, reg);
+	I2C_Master_Start(BNO055);
+	I2C_Master_Send_Address(BNO055, 0x29, read);
+	temp =I2C_Master_Receive_Data(BNO055);
+	I2C_Master_Send_NACK(BNO055);
+	I2C_Master_Stop(BNO055);
 
-	BNO055_Read(GRV_Data_X_LSB,6);
-
-	GRV_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
-	GRV_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
-	GRV_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
-
-
-	BNO055_GRAVITY_VECTOR_Buffer[0] = (float)(GRV_X / 100.0f);
-	BNO055_GRAVITY_VECTOR_Buffer[1] = (float)(GRV_Y / 100.0f);
-	BNO055_GRAVITY_VECTOR_Buffer[2] = (float)(GRV_Z / 100.0f);
-
+	return temp;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////
-//                                          LINEAR ACCELERATION
-//////////////////////////////////////////////////////////////////////////////////////
-
-void BNO055_GET_Linear_Acceleration_DATA(void)
+void BNO055_Get_Euler_Angles(short *eu)
 {
-	uint16_t LIN_X, LIN_Y, LIN_Z;
-	for(int i = 0 ; i< 10; i++)
+	short i = 0;
+	uint8_t a[6];
+	BNO055_Read_Buffer(EUL_Heading_LSB, a, 6);
+	for(i = 0; i<3; i++)
 	{
-		BNO055_Buffer[i] = 0;
+		eu[i] = (short)((a[i*2+1] << 8) | a[i*2])/16;
 	}
-
-	BNO055_Read(GRV_Data_X_LSB,6);
-
-	LIN_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
-	LIN_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
-	LIN_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
-
-
-	BNO055_LINEAR_ACCL_Buffer[0] = (float)(LIN_X / 100.0f);
-	BNO055_LINEAR_ACCL_Buffer[1] = (float)(LIN_Y / 100.0f);
-	BNO055_LINEAR_ACCL_Buffer[2] = (float)(LIN_Z / 100.0f);
-
 }
 
-//////////////////////////////////////////////////////////////////////////////////////
-//                                          COMPASS DATA
-//////////////////////////////////////////////////////////////////////////////////////
-
-void BNO055_GET_Compass_DATA(void)
+void BNO055_Get_Quaternion_Angles(short *qu)
 {
-	uint16_t LIN_X, LIN_Y, LIN_Z;
-	for(int i = 0 ; i< 10; i++)
+	short i = 0;
+	unit8_t a[8];
+	BNO055_Read_Buffer(QUA_Data_W_LSB, a, 8);
+	for(i = 0; i < 4; i++)
 	{
-		BNO055_Buffer[i] = 0;
+		qu[i] = (short)((a[i*2+1] << 8) | a[i*2])/16384;
 	}
-
-	BNO055_Read(GRV_Data_X_LSB,6);
-
-	LIN_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
-	LIN_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
-	LIN_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
-
-
-	BNO055_LINEAR_ACCL_Buffer[0] = (float)(LIN_X / 100.0f);
-	BNO055_LINEAR_ACCL_Buffer[1] = (float)(LIN_Y / 100.0f);
-	BNO055_LINEAR_ACCL_Buffer[2] = (float)(LIN_Z / 100.0f);
-
 }
 
-//////////////////////////////////////////////////////////////////////////////////////
-//                                          RAW  DATA
-//////////////////////////////////////////////////////////////////////////////////////
-
-void BNO055_GET_GYRO_DATA(void)
-{
-	uint16_t GYRO_X, GYRO_Y, GYRO_Z;
-	for(int i = 0 ; i< 10; i++)
-	{
-		BNO055_Buffer[i] = 0;
-	}
-
-	BNO055_Read(GYRO_DATA_X_LSB,6);
-
-	GYRO_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
-	GYRO_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
-	GYRO_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
-
-	BNO055_GYRO_DATA_Buffer[0] = (float)(GYRO_X / 16.0f);
-	BNO055_GYRO_DATA_Buffer[1] = (float)(GYRO_Y / 16.0f);
-	BNO055_GYRO_DATA_Buffer[2] = (float)(GYRO_Z / 16.0f);
-}
-
-
-void BNO055_GET_ACCL_DATA(void)
-{
-	uint16_t ACCL_X, ACCL_Y, ACCL_Z;
-	for(int i = 0 ; i< 10; i++)
-	{
-		BNO055_Buffer[i] = 0;
-	}
-
-	BNO055_Read(ACC_DATA_X_LSB,6);
-
-	ACCL_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
-	ACCL_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
-	ACCL_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
-
-	BNO055_ACCL_DATA_Buffer[0] = (float)(ACCL_X / 0.01f);
-	BNO055_ACCL_DATA_Buffer[1] = (float)(ACCL_Y / 0.01f);
-	BNO055_ACCL_DATA_Buffer[2] = (float)(ACCL_Z / 0.01f);
-}
-
-
-void BNO055_GET_MAG_DATA(void)
-{
-	uint16_t MAG_X, MAG_Y, MAG_Z;
-	for(int i = 0 ; i< 10; i++)
-	{
-		BNO055_Buffer[i] = 0;
-	}
-
-	BNO055_Read(MAG_DATA_X_LSB,6);
-
-	MAG_X = (BNO055_Buffer[1] << 8) | BNO055_Buffer[0];
-	MAG_Y = (BNO055_Buffer[3] << 8) | BNO055_Buffer[2];
-	MAG_Z = (BNO055_Buffer[5] << 8) | BNO055_Buffer[4];
-
-	BNO055_MAG_DATA_Buffer[0] = (float)(MAG_X / 16.0f);
-	BNO055_MAG_DATA_Buffer[1] = (float)(MAG_Y / 16.0f);
-	BNO055_MAG_DATA_Buffer[2] = (float)(MAG_Z / 16.0f);
-}
 
