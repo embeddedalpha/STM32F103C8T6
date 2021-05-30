fb4820eaf51146271296fd18818872d34bd15f04
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-30 23:54:29 +0530

    Update BNO055.h

--- Firmware/Devices/BNO055/BNO055.h
+++ Firmware/Devices/BNO055/BNO055.h
@@ -1,49 +1,21 @@
-#ifndef _BNO055_H_
-#define _BNO055_H_
+/*
+ * BNO055.h
+ *
+ *  Created on: 30-May-2021
+ *      Author: Kunal
+ */
 
+#ifndef BNO055_BNO055_H_
+#define BNO055_BNO055_H_
+
+#include "main.h"
+#include "I2C/I2C.h"
 #include "BNO055_REG1.h"
 #include "BNO055_REG2.h"
-#include "i2c.h"
 
-#define BNO055_Address	0x28
+struct I2C_Config BNO055;
 
-#define BNO055_Normal_Mode 0x00
-#define BNO055_Low_Normal_Mode 0x01
-#define BNo055_Suspend_Mode 0x02
+void BNO055_Init(I2C_TypeDef *I2C);
 
 
-
-uint8_t BNO055_Buffer[10];
-
-float BNO055_QUATERNION_Buffer[4];
-float BNO055_GRAVITY_VECTOR_Buffer[3];
-float BNO055_LINEAR_ACCL_Buffer[3];
-float BNO055_EULER_ANGLE_Buffer[3];
-float BNO055_COMPASS_DATA_Buffer[3];
-float BNO055_IMU_DATA_Buffer[3];
-float BNO055_M4G_DATA_Buffer[3];
-float BNO055_NDOF_DATA_Buffer[3];
-
-float BNO055_GYRO_DATA_Buffer[3];
-float BNO055_ACCL_DATA_Buffer[3];
-float BNO055_MAG_DATA_Buffer[3];
-
-
-void BNO055_Init();
-void BNO055_Write(uint8_t reg, uint8_t data);
-void BNO055_Read(uint8_t reg, uint8_t bytes);
-void BNO055_Power_Mode(uint8_t mode);
-
-
-void BNO055_GET_Quaternion_DATA(void);
-void BNO055_GET_Euler_DATA(void);
-void BNO055_GET_Gravity_Vector_DATA(void);
-void BNO055_GET_Linear_Acceleration_DATA(void);
-void BNO055_GET_Compass_DATA(void);
-void BNO055_GET_GYRO_DATA(void);
-void BNO055_GET_ACCL_DATA(void);
-void BNO055_GET_MAG_DATA(void);
-
-
-
-#endif
+#endif /* BNO055_BNO055_H_ */
