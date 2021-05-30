211652065d9694ea7600b238a35e40c99781ff87
Kunal Salvi <kunalsalvi63@gmail.com>
2021-05-31 00:04:20 +0530

    Update BNO055.h

--- Firmware/Devices/BNO055/BNO055.h
+++ Firmware/Devices/BNO055/BNO055.h
@@ -2,6 +2,7 @@
  * BNO055.h
  *
  *  Created on: 30-May-2021
+ *  Updated on: 31-May-2021
  *      Author: Kunal
  */
 
@@ -17,5 +18,19 @@ struct I2C_Config BNO055;
 
 void BNO055_Init(I2C_TypeDef *I2C);
 
+void BNO055_Read_Buffer(uint8_t reg, uint8_t *data, int len);
 
+void BNO055_Write_Buffer(unit8_t reg, uint8_t *data, int len);
+
+void BNO055_Write_Single(unit8_t reg, unit8_t data);
+
+unit8_t BNO055_Read_Single(unit8_t reg);
+
+void BNO055_Get_Euler_Angles(short *eu);
+
+void BNO055_Get_Quaternion_Angles(short *qu);
+
+void BNO055_Get_Linear_Accl(short *la);
+
+void BNO055_Get_Gravity_Vector(short *gv);
 #endif /* BNO055_BNO055_H_ */
