c9d12961bde9f75e0331544f48f3f50da77ce266
Kunal Salvi <kunalsalvi63@gmail.com>
2021-06-01 23:06:42 +0530

    Update MPU6050_I2C.h

--- Firmware/Devices/MPU6050/MPU6050_I2C.h
+++ Firmware/Devices/MPU6050/MPU6050_I2C.h
@@ -1,8 +1,8 @@
 /*
  * MPU6050.h
  *
- *  Created on: 26-Apr-2021
- *      Author: Kunal
+ *  Created on: 25-Apr-2021
+ *      Author: Kunal Salvi
  */
 
 #ifndef MPU6050_MPU6050_I2C_H_
@@ -11,13 +11,28 @@
 #include "main.h"
 #include "GPIO/GPIO.h"
 #include "I2C/I2C.h"
+#include "MPU6050_Reg.h"
 
+struct I2C_Config MPU6050;
+#define MPU6050_Dev_Add	0x78
 
-void MPU6050_Init(I2C_TypeDef *I2C);
-
-short *MPU6050_Get_Raw_Data(I2C_TypeDef *I2C);
-
-
+void MPU6050_I2C_Init(I2C_TypeDef *I2C);
+void MPU6050_I2C_Write_Single(uint8_t reg, uint8_t data);
+void MPU6050_I2C_Write_Burst(uint8_t reg, unit8_t *data, int len);
+unit8_t MPU6050_I2C_Read_Single(uint8_t reg);
+void MPU6050_I2C_Read_Burst(uint8_t reg, unit8_t *data, int len);
+void MPU6050_I2C_Get_Raw_Data(uint16_t *accl_x,
+		                  uint16_t *accl_y,
+						  uint16_t *accl_z,
+						  uint16_t *gyro_x,
+						  uint16_t *gyro_y,
+						  uint16_t *gyro_z);
+void MPU6050_I2C_Get_Filtered_Data(uint16_t *accl_x,
+                                   uint16_t *accl_y,
+		                           uint16_t *accl_z,
+		                           uint16_t *gyro_x,
+		                           uint16_t *gyro_y,
+		                           uint16_t *gyro_z);
 
 
 #endif /* MPU6050_MPU6050_I2C_H_ */
