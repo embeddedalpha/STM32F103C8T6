47ee1857f4441690605c2d6ddd5ab3c950e708bd
Kunal Salvi <kunalsalvi63@gmail.com>
2021-06-03 21:59:48 +0530

    Update MPU9250_I2C.h

--- Firmware/Devices/MPU9250/MPU9250_I2C.h
+++ Firmware/Devices/MPU9250/MPU9250_I2C.h
@@ -2,6 +2,7 @@
  * MPU9250.c
  *
  *  Created on: 30-04-2021
+ *  Updated on: 03-06-2021
  *      Author: Kunal Salvi
  */
 
@@ -12,7 +13,10 @@
 #include "I2C/I2C.h"
 #include "MPU9250_REG.h"
 
+struct I2C_Config MPU9250;
+
 #define MPU9250_Address	0x68
+
 //#define MPU9250_Address	0x69    Un-comment to use any of one to use
 
 void MPU9250_Init(I2C_TypeDef *I2C);
@@ -25,16 +29,12 @@ uint8_t MPU9250_Read_Byte(uint8_t device_address, uint8_t reg_address);
 
 void MPU9250_Read_Multiple_Bytes(uint8_t device_address, uint8_t reg_address,uint8_t *data, uint16_t length);
 
-void MPU9250_Read_Accl_Data(I2C_TypeDef *I2C, int16_t *array);
+void MPU9250_Read_Accl_Data(short *array);
 
-void MPU9250_Read_Gyro_Data(I2C_TypeDef *I2C, int16_t *array);
+void MPU9250_Read_Gyro_Data(short *array);
 
-void MPU9250_Read_Magn_Data(I2C_TypeDef *I2C, int16_t *array);
+void MPU9250_Read_Magn_Data(short *array);
 
-void MPU9250_Calibrate_Accl(I2C_TypeDef *I2C, uint16_t *array);
 
-void MPU9250_Calibrate_Gyro(I2C_TypeDef *I2C, uint16_t *array);
-
-void MPU9250_Calibrate_Magn(I2C_TypeDef *I2C, uint16_t *array);
 
 #endif /* MPU9250_MPU9250_H_ */
