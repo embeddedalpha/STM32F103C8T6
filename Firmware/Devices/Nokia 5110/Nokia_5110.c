585f122b13d308e892a100e031cc567ecee4cde5
Kunal Salvi <kunalsalvi63@gmail.com>
2021-06-21 23:03:57 +0530

    Update Nokia_5110.c

--- Firmware/Devices/Nokia 5110/Nokia_5110.c
+++ Firmware/Devices/Nokia 5110/Nokia_5110.c
@@ -5,6 +5,14 @@
  *      Author: Kunal
  */
 
+// 1	RST	PA8
+// 2	CE	PA4
+// 3	DC	PA9
+// 4	DIN	PA7
+// 5	CLK	PA5
+// 6	VCC ~
+// 7	BL	~
+// 8	GND	~
 
 #include "Nokia_5110.h"
 
@@ -22,16 +30,24 @@ void N5110_Init(SPI_TypeDef *SPI)
 }
 
 void N5110_Reset_High(void)
-{}
+{
+	GPIOA->BSRR |= GPIO_BSRR_BS8;
+}
 
 void N5110_Reset_Low(void)
-{}
+{
+	GPIO->BRR |= GPIO_BRR_BR8;
+}
 
 void N5110_DC_High(void)
-{}
+{
+	GPIOA->BSRR |= GPIO_BSRR_BS9;
+}
 
 void N5110_DC_Low(void)
-{}
+{
+	GPIOA->BRR |= GPIO_BRR_BR9;
+}
 
 void N5110_Send_Command(uint8_t Command_Byte, uint8_t Data_Byte)
 {}
@@ -50,4 +66,3 @@ void N5110_Draw_Point(uint8_t x, uint8_t y)
 
 void N5110_Printf(uint8_t x,uint8_t y, char *s)
 {}
-
