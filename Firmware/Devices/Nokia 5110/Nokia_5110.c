1a809f5982dbca4b487d414966444e48e92b3b1d
Kunal Salvi <kunalsalvi63@gmail.com>
2021-06-22 22:40:57 +0530

    Update Nokia_5110.c

--- Firmware/Devices/Nokia 5110/Nokia_5110.c
+++ Firmware/Devices/Nokia 5110/Nokia_5110.c
@@ -19,6 +19,7 @@
 
 void N5110_Init(SPI_TypeDef *SPI)
 {
+	Delay_Config();
 	N5110.SPI = SPI;
 	N5110.clock_phase = 1;
 	N5110.clock_polarity = 1;
@@ -26,6 +27,17 @@ void N5110_Init(SPI_TypeDef *SPI)
 	N5110.frame_format = 0;
 	N5110.lsb_or_msb = 0;
 	SPI_Master_Init(N5110);
+
+	N5110_Reset_High();
+	Delay_ms(10);
+	N5110_Reset_Low();
+	Delay_ms(10);
+	N5110_Reset_High();
+
+	N5110_Send_Command((1 << 0) | (1 << 5)); //Select Extended Instruction Set
+	N5110_Send_Command(0xC0); //Set Vop to 16x
+	N5110_Send_Command(1 << 5); //Select Normal Instruction Set
+	N5110_Send_Command((1 << 3) | (1 << 2)); //Set Display mode as Normal
 
 }
 
@@ -49,20 +61,66 @@ void N5110_DC_Low(void)
 	GPIOA->BRR |= GPIO_BRR_BR9;
 }
 
-void N5110_Send_Command(uint8_t Command_Byte, uint8_t Data_Byte)
-{}
+void N5110_Send_Command(uint8_t Command_Byte)
+{
+	SPI_Master_SS_Select(N5110);
+	N5110_DC_Low();
+	SPI_Master_TX(N5110, Command_Byte);
+	N5110_DC_High();
+	SPI_Master_SS_Deselect(N5110);
+
+}
 
 void N5110_Send_Data(unint8_t Data)
-{}
+{
+	N5110_DC_High();
+	SPI_Master_TX(N5110, Data);
+	N5110_DC_Low();
+}
+
+void N5110_Send_Buffer(uint8_t *data, int length)
+{
+	int i;
+	N5110_DC_High();
+
+	for(i=0;i<length;i++)SPI_Master_TX(N5110, Data);
+
+	N5110_DC_Low();
+}
 
 void N5110_Invert_Screen(void)
-{}
+{
+	N5110_Send_Command((1 << 3) | (1 << 2) | (1 << 0));
+}
 
-void N5100_Set_Pointer(uint8_t x, uint8_t y)
-{}
+void N5110_Set_Pointer(uint8_t x, uint8_t y)
+{
+	N5110_Send_Command((1 << 6) | (y));
+	N5110_Send_Command((1 << 7) | (x));
+
+}
 
 void N5110_Draw_Point(uint8_t x, uint8_t y)
-{}
+{
+	N5110_Set_Pointer(x, y);
+	N5110_Send_Data(0x01);
+}
 
 void N5110_Printf(uint8_t x,uint8_t y, char *s)
-{}
+{
+	N5110_Set_Pointer(x,y);
+	while(*s)
+	{
+		for(i = 0;i < 5; i++)
+		{
+			N5110_Send_Data(ASCII[s-0x20][i]);
+		}
+	}
+}
+
+void N5110_Screen_Clear()
+{
+	for (i = 0; i < (LCD_X * LCD_Y/8); i++) {
+		N5110_Send_Data(0);
+	}
+}
